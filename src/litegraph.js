4e0670ffb1a49fec8eff09d0b03480eb30095561
comfyanonymous <comfyanonymous@protonmail.com>
2023-04-10 02:42:35 -0400

    litegraph dev forgot to remove the title height from the collision if it's hidden

--- src/litegraph.js
+++ src/litegraph.js
@@ -1654,7 +1654,8 @@
 		var nRet = null;
         for (var i = nodes_list.length - 1; i >= 0; i--) {
             var n = nodes_list[i];
-            if (n.isPointInside(x, y, margin)) {
+            var skip_title = n.constructor.title_mode == LiteGraph.NO_TITLE;
+            if (n.isPointInside(x, y, margin, skip_title)) {
                 // check for lesser interest nodes (TODO check for overlapping, use the top)
 				/*if (typeof n == "LGraphGroup"){
 					nRet = n;
