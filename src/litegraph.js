a525552030dd4c9d3329ec9b729d7c6061ce12e6
comfyanonymous <comfyanonymous@protonmail.com>
2023-08-31 02:24:39 -0400

    Fix litegraph button being black on light theme.

--- src/litegraph.js
+++ src/litegraph.js
@@ -9794,6 +9794,7 @@ LGraphNode.prototype.executeAction = function(action)
 
             switch (w.type) {
                 case "button":
+                    ctx.fillStyle = background_color;
                     if (w.clicked) {
                         ctx.fillStyle = "#AAA";
                         w.clicked = false;
