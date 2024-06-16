f997a7858d0268b430ceace9ab08a9ae1b24dec6
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-06-15 22:02:00 -0400

    Update jestconfig

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 /** @type {import('jest').Config} */
 export default {
+	preset: "ts-jest",
 	testEnvironment: "jsdom",
+	globals: {
+		"ts-jest": {
+			tsconfig: "tsconfig.json"
+		}
+	},
+	transform: {
+		"^.+\\.(ts|tsx)$": "ts-jest"
+	},
 	setupFiles: ["./tests-ui/globalSetup.js"],
 	setupFilesAfterEnv: ["./tests-ui/afterSetup.js"],
 	clearMocks: true,
