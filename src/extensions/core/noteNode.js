86c8db8c8f28036c836673bbce61013eb8662ac9
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-06-18 12:34:18 -0400

    Bundle all core extensions

--- src/extensions/core/noteNode.js
+++ src/extensions/core/noteNode.js
@@ -1,5 +1,5 @@
-import {app} from "../../scripts/app.js";
-import {ComfyWidgets} from "../../scripts/widgets.js";
+import {app} from "../../scripts/app";
+import {ComfyWidgets} from "../../scripts/widgets";
 // Node that add notes to your project
 
 app.registerExtension({
--- src/extensions/core/undoRedo.js
+++ src/extensions/core/undoRedo.js
@@ -1,5 +1,5 @@
-import { app } from "../../scripts/app.js";
-import { api } from "../../scripts/api.js"
+import { app } from "../../scripts/app";
+import { api } from "../../scripts/api";
 
 const MAX_HISTORY = 50;
 
