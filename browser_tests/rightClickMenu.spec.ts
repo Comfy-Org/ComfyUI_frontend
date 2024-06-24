8149bce6de43d174f8c05539c0325475a550c441
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-06-24 16:46:04 -0400

    Add group test

--- browser_tests/rightClickMenu.spec.ts
+++ browser_tests/rightClickMenu.spec.ts
@@ -15,6 +15,14 @@ test.describe('Canvas Right Click Menu', () => {
         await comfyPage.nextFrame();
         await expect(comfyPage.canvas).toHaveScreenshot('add-node-node-added.png');
     });
+
+    test('Can add group', async ({ comfyPage }) => {
+        await comfyPage.rightClickCanvas();
+        await expect(comfyPage.canvas).toHaveScreenshot('right-click-menu.png');
+        await comfyPage.page.getByText('Add Group', { exact: true }).click();
+        await comfyPage.nextFrame();
+        await expect(comfyPage.canvas).toHaveScreenshot('add-group-group-added.png');
+    });
 });
 
 test.describe('Node Right Click Menu', () => {
