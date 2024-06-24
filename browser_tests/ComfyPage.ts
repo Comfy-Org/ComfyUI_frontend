f6fc7e7e4bde6221a4aa300e49b0dc2edc271b89
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-06-24 16:26:43 -0400

    Move fixture

--- browser_tests/ComfyPage.ts
+++ browser_tests/ComfyPage.ts
@@ -1,4 +1,5 @@
 import type { Page, Locator } from '@playwright/test';
+import { test as base } from '@playwright/test';
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -92,6 +93,8 @@ export class ComfyPage {
     await this.page.mouse.down();
     await this.page.mouse.move(427, 98);
     await this.page.mouse.up();
+    // Move out the way to avoid highlight of menu item.
+    await this.page.mouse.move(10, 10);
     await this.nextFrame();
   }
 
@@ -134,3 +137,32 @@ export class ComfyPage {
     await this.nextFrame();
   }
 }
+
+export const comfyPageFixture = base.extend<{ comfyPage: ComfyPage }>({
+  comfyPage: async ({ page }, use) => {
+    const comfyPage = new ComfyPage(page);
+    await comfyPage.goto();
+    // Unify font for consistent screenshots.
+    await page.addStyleTag({
+      url: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+    });
+    await page.addStyleTag({
+      url: "https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+    });
+    await page.addStyleTag({
+      content: `
+      * {
+				font-family: 'Roboto Mono', 'Noto Color Emoji';
+			}`
+    });
+
+    await page.waitForFunction(() => document.fonts.ready);
+    await page.waitForFunction(() => window['app'] != undefined);
+    await page.evaluate(() => { window['app']['canvas'].show_info = false; });
+    await comfyPage.nextFrame();
+    // Reset view to force re-rendering of canvas. So that info fields like fps
+    // become hidden.
+    await comfyPage.resetView();
+    await use(comfyPage);
+  },
+});
--- browser_tests/interaction.spec.ts
+++ browser_tests/interaction.spec.ts
@@ -1,34 +1,5 @@
-import { test as base, expect } from '@playwright/test';
-import { ComfyPage } from './ComfyPage';
-
-const test = base.extend<{ comfyPage: ComfyPage }>({
-  comfyPage: async ({ page }, use) => {
-    const comfyPage = new ComfyPage(page);
-    await comfyPage.goto();
-    // Unify font for consistent screenshots.
-    await page.addStyleTag({
-      url: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-    });
-    await page.addStyleTag({
-      url: "https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-    });
-    await page.addStyleTag({
-      content: `
-      * {
-				font-family: 'Roboto Mono', 'Noto Color Emoji';
-			}`
-    });
-
-    await page.waitForFunction(() => document.fonts.ready);
-    await page.waitForFunction(() => window['app'] != undefined);
-    await page.evaluate(() => { window['app']['canvas'].show_info = false; });
-    await comfyPage.nextFrame();
-    // Reset view to force re-rendering of canvas. So that info fields like fps
-    // become hidden.
-    await comfyPage.resetView();
-    await use(comfyPage);
-  },
-});
+import { expect } from '@playwright/test';
+import { comfyPageFixture as test } from './ComfyPage';
 
 test.describe('Node Interaction', () => {
   test('Can enter prompt', async ({ comfyPage }) => {
