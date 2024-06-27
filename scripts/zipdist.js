6d7003ee5eb7318286f0bb375a7a68a6b818c967
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-06-27 15:24:00 -0400

    Zip dist on build

---
+++ scripts/zipdist.js
@@ -0,0 +1,9 @@
+import zipdir from 'zip-dir';
+
+zipdir('./dist', { saveTo: './dist.zip' }, function (err, buffer) {
+  if (err) {
+    console.error('Error zipping "dist" directory:', err);
+  } else {
+    console.log('Successfully zipped "dist" directory.');
+  }
+});
