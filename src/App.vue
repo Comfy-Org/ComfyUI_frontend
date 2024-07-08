e6d77830fb00009ae491e3032172b6c2445a6aea
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-07-08 15:14:47 -0400

    Change theme

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,40 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, provide, ref } from "vue";
+import { onMounted, onUnmounted, provide, ref } from "vue";
 import NodeSearchboxPopover from "@/components/NodeSearchBoxPopover.vue";
 import ProgressSpinner from "primevue/progressspinner";
 import { api } from "@/scripts/api";
 import { NodeSearchService } from "./services/nodeSearchService";
+import { ColorPaletteLoadedEvent } from "./types/colorPalette";
 
 const isLoading = ref(true);
 const nodeSearchService = ref<NodeSearchService>();
+
+const updateTheme = (e: ColorPaletteLoadedEvent) => {
+  const DARK_THEME_CLASS = "dark-theme";
+  const isDarkTheme = e.detail.id !== "light";
+
+  if (isDarkTheme) {
+    document.body.classList.add(DARK_THEME_CLASS);
+  } else {
+    document.body.classList.remove(DARK_THEME_CLASS);
+  }
+};
 
 onMounted(async () => {
   const nodeDefs = Object.values(await api.getNodeDefs());
   nodeSearchService.value = new NodeSearchService(nodeDefs);
   isLoading.value = false;
+
+  document.addEventListener("comfy:setting:color-palette-loaded", updateTheme);
+});
+
+onUnmounted(() => {
+  document.removeEventListener(
+    "comfy:setting:color-palette-loaded",
+    updateTheme
+  );
 });
 
 provide("nodeSearchService", nodeSearchService);
--- src/extensions/core/colorPalette.ts
+++ src/extensions/core/colorPalette.ts
@@ -1,6 +1,6 @@
 import { app } from "../../scripts/app";
 import { $el } from "../../scripts/ui";
-import type { ColorPalettes } from "@/types/colorPalette";
+import type { ColorPalettes, Palette } from "@/types/colorPalette";
 import { LGraphCanvas, LiteGraph } from "@comfyorg/litegraph";
 
 // Manage color palettes
@@ -626,7 +626,7 @@ app.registerExtension({
       await loadColorPalette(getColorPalette());
     };
 
-    const loadColorPalette = async (colorPalette) => {
+    const loadColorPalette = async (colorPalette: Palette) => {
       colorPalette = await completeColorPalette(colorPalette);
       if (colorPalette.colors) {
         // Sets the colors of node slots and links
@@ -671,6 +671,12 @@ app.registerExtension({
         }
         app.canvas.draw(true, true);
       }
+
+      document.dispatchEvent(
+        new CustomEvent("comfy:setting:color-palette-loaded", {
+          detail: colorPalette,
+        })
+      );
     };
 
     const getColorPalette = (colorPaletteId?) => {
