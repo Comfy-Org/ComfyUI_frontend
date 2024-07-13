93a0c1012f1ed407fe7eee81a4afde5641b12660
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-07-13 09:25:58 -0400

    Add system node to serach box db (#128)

--- src/services/nodeSearchService.ts
+++ src/services/nodeSearchService.ts
@@ -3,6 +3,42 @@ import { getNodeSource } from "@/types/nodeSource";
 import Fuse, { IFuseOptions, FuseSearchOptions } from "fuse.js";
 import _ from "lodash";
 
+export const SYSTEM_NODE_DEFS: ComfyNodeDef[] = [
+  {
+    name: "PrimitiveNode",
+    display_name: "Primitive",
+    category: "utils",
+    input: { required: {}, optional: {} },
+    output: ["*"],
+    output_name: ["connect to widget input"],
+    output_is_list: [false],
+    python_module: "nodes",
+    description: "Primitive values like numbers, strings, and booleans.",
+  },
+  {
+    name: "Reroute",
+    display_name: "Reroute",
+    category: "utils",
+    input: { required: { "": ["*"] }, optional: {} },
+    output: ["*"],
+    output_name: [""],
+    output_is_list: [false],
+    python_module: "nodes",
+    description: "Reroute the connection to another node.",
+  },
+  {
+    name: "Note",
+    display_name: "Note",
+    category: "utils",
+    input: { required: {}, optional: {} },
+    output: [],
+    output_name: [],
+    output_is_list: [],
+    python_module: "nodes",
+    description: "Node that add notes to your project",
+  },
+];
+
 export class FuseSearch<T> {
   private fuse: Fuse<T>;
   public readonly data: T[];
