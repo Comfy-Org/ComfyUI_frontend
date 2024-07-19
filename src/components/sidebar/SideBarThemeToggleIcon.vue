17a489f12fe9a5a026476f0719b2fbc8415817ee
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-07-19 12:08:27 -0400

    Add theme toggle logic

--- src/components/sidebar/SideBarThemeToggleIcon.vue
+++ src/components/sidebar/SideBarThemeToggleIcon.vue
@@ -3,12 +3,18 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
 import SideBarIcon from "./SideBarIcon.vue";
+import { app } from "@/scripts/app";
 
 const isDarkMode = ref(false);
 const icon = computed(() => (isDarkMode.value ? "pi pi-sun" : "pi pi-moon"));
+const themeId = computed(() => (isDarkMode.value ? "dark" : "light"));
 const toggleTheme = () => {
   isDarkMode.value = !isDarkMode.value;
 };
+
+watch(themeId, (newThemeId) => {
+  app.ui.settings.setSettingValue("Comfy.ColorPalette", newThemeId);
+});
 </script>
