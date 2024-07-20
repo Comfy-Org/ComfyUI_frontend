00b75cead07411797377103676cd2c755cae9d7d
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-07-20 16:15:23 -0400

    Move settings button from top menu to side bar

---
+++ src/components/sidebar/SideBarSettingsToggleIcon.vue
@@ -0,0 +1,12 @@
+<template>
+  <SideBarIcon icon="pi pi-cog" @click="showSetting" />
+</template>
+
+<script setup lang="ts">
+import { app } from "@/scripts/app";
+import SideBarIcon from "./SideBarIcon.vue";
+
+const showSetting = () => {
+  app.ui.settings.show();
+};
+</script>
