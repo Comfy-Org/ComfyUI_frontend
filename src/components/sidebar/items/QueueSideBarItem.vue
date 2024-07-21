608e195986ccf75713a30e4ac895d95c5d0b5071
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-07-20 23:17:13 -0400

    computed

--- src/components/sidebar/items/QueueSideBarItem.vue
+++ src/components/sidebar/items/QueueSideBarItem.vue
@@ -17,11 +17,11 @@ import Accordion from "primevue/accordion";
 import AccordionHeader from "primevue/accordionheader";
 import AccordionPanel from "primevue/accordionpanel";
 import AccordionContent from "primevue/accordioncontent";
-import { onMounted } from "vue";
+import { computed, onMounted } from "vue";
 import { api } from "@/scripts/api";
 
 const queueStore = useQueueStore();
-const tasks = queueStore.tasks;
+const tasks = computed(() => queueStore.tasks);
 
 onMounted(() => {
   api.addEventListener("status", () => {
