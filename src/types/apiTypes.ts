1a13328975904d088322f6a58252afc8717a0b87
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-07-31 09:30:19 -0400

    nit

--- src/types/apiTypes.ts
+++ src/types/apiTypes.ts
@@ -198,7 +198,7 @@ const zFloatInputSpec = inputSpec([
     min: z.number().optional(),
     max: z.number().optional(),
     step: z.number().optional(),
-    round: z.number().optional(),
+    round: z.boolean().optional(),
     default: z.number().optional()
   })
 ])
