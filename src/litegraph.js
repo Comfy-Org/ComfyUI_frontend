d2ef32fcb51c7858b46436f3dbf2e68da30e1013
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-08-06 10:56:57 -0400

    Fix Vite build (#55)
    * Fix build output
    
    * 0.7.37
    
    * Fix type export
    
    * 0.7.38
    
    * Adjust build
    
    * 0.7.39
    
    * Add css to package declaration
    
    * 0.7.40
    
    * ESM style named export
    
    * 0.7.41
    
    * Fix style export for unit test
    
    * 0.7.42

--- src/litegraph.js
+++ src/litegraph.js
@@ -1,4 +1,6 @@
+const globalExport = {};
 
+(function (globalThis) {
     // *************************************************************
     //   LiteGraph CLASS                                     *******
     // *************************************************************
@@ -14513,14 +14515,13 @@ LGraphNode.prototype.executeAction = function(action)
                 window.setTimeout(callback, 1000 / 60);
             };
     }
+})(globalExport)
 
-if (typeof exports != "undefined") {
-    exports.LiteGraph = globalThis.LiteGraph;
-    exports.LGraph = globalThis.LGraph;
-    exports.LLink = globalThis.LLink;
-    exports.LGraphNode = globalThis.LGraphNode;
-    exports.LGraphGroup = globalThis.LGraphGroup;
-    exports.DragAndScale = globalThis.DragAndScale;
-    exports.LGraphCanvas = globalThis.LGraphCanvas;
-    exports.ContextMenu = globalThis.ContextMenu;
-}
+export const LiteGraph = globalExport.LiteGraph;
+export const LGraph = globalExport.LGraph;
+export const LLink = globalExport.LLink;
+export const LGraphNode = globalExport.LGraphNode;
+export const LGraphGroup = globalExport.LGraphGroup;
+export const DragAndScale = globalExport.DragAndScale;
+export const LGraphCanvas = globalExport.LGraphCanvas;
+export const ContextMenu = globalExport.ContextMenu;
--- vite.config.mts
+++ vite.config.mts
@@ -6,16 +6,10 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/litegraph.js'),
       name: 'litegraph.js',
-      // TODO: Below workaround ensures output matches pre-vite format.  Should be removed.
-      fileName: (moduleFormat, entryAlias) => 'src/litegraph.js',
-      formats: ['iife']
+      fileName: (format) => `litegraph.${format}.js`,
+      formats: ['es', 'umd']
     },
     minify: false,
     sourcemap: true,
-    rollupOptions: {
-      // Disabling tree-shaking
-      // Prevent vite remove unused exports
-      treeshake: false
-    }
   },
 })
