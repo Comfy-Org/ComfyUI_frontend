17aa44d9f6f576871284bed538c1462d6c926006
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-08-18 10:23:03 -0400

    Activate new settings dialog from default UI (#506)

--- src/services/dialogService.ts
+++ src/services/dialogService.ts
@@ -1,6 +1,10 @@
+// This module is mocked in tests-ui/
+// Import vue components here to avoid tests-ui/ reporting errors
+// about importing primevue components.
 import { useDialogStore } from '@/stores/dialogStore'
 import LoadWorkflowWarning from '@/components/dialog/content/LoadWorkflowWarning.vue'
-import { markRaw } from 'vue'
+import SettingDialogContent from '@/components/dialog/content/SettingDialogContent.vue'
+import SettingDialogHeader from '@/components/dialog/header/SettingDialogHeader.vue'
 
 export function showLoadWorkflowWarning(props: {
   missingNodeTypes: any[]
@@ -9,7 +13,14 @@ export function showLoadWorkflowWarning(props: {
 }) {
   const dialogStore = useDialogStore()
   dialogStore.showDialog({
-    component: markRaw(LoadWorkflowWarning),
+    component: LoadWorkflowWarning,
     props
+  })
+}
+
+export function showSettingsDialog() {
+  useDialogStore().showDialog({
+    headerComponent: SettingDialogHeader,
+    component: SettingDialogContent
   })
 }
