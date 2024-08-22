1e62f876ef8d1a5d4389bba349aa14c1ebb7031b
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-08-22 13:51:02 -0400

    Single dialog

--- src/components/dialog/content/ExecutionErrorDialogContent.vue
+++ src/components/dialog/content/ExecutionErrorDialogContent.vue
@@ -3,34 +3,20 @@
     icon="pi pi-exclamation-circle"
     :title="error.node_type"
     :message="error.exception_message"
-    :buttonLabel="$t('showReport')"
-    @action="openReportDialog"
   />
+  <ErrorReport :error="error" />
 </template>
 
 <script setup lang="ts">
 import { ExecutionErrorWsMessage } from '@/types/apiTypes'
 import NoResultsPlaceholder from '@/components/common/NoResultsPlaceholder.vue'
-import { useDialogStore } from '@/stores/dialogStore'
-import ErrorReportDialogContent from './ErrorReportDialogContent.vue'
-import { api } from '@/scripts/api'
+import ErrorReport from './error/ErrorReport.vue'
 
 const props = defineProps<{
   error: ExecutionErrorWsMessage
 }>()
 
 const error = props.error
-const dialogStore = useDialogStore()
-
-const openReportDialog = async () => {
-  dialogStore.showDialog({
-    component: ErrorReportDialogContent,
-    props: {
-      error: error,
-      systemStats: await api.getSystemStats()
-    }
-  })
-}
 </script>
 
 <style scoped>
--- src/components/dialog/content/error/ErrorReport.vue
+++ src/components/dialog/content/error/ErrorReport.vue
@@ -1,53 +1,69 @@
 <template>
-  <ScrollPanel style="width: 100%; height: 70vh">
-    <pre>{{ reportContent }}</pre>
-  </ScrollPanel>
-  <Button
-    label="Copy to Clipboard"
-    icon="pi pi-copy"
-    @click="copyReportToClipboard"
-  />
-  <Button
-    label="Open GitHub Issue"
-    icon="pi pi-external-link"
-    @click="openGitHubIssue"
-    class="p-button-secondary"
-  />
-  <FindIssueButton
-    :errorMessage="props.error.exception_message"
-    repoOwner="comfyanonymous"
-    repoName="ComfyUI"
-  />
+  <div class="error-report">
+    <Button
+      v-show="!reportOpen"
+      :label="$t('showReport')"
+      @click="showReport"
+      text
+    />
+    <template v-if="reportOpen">
+      <Divider />
+      <ScrollPanel style="width: 100%; max-height: 70vh">
+        <pre>{{ reportContent }}</pre>
+      </ScrollPanel>
+      <Divider />
+    </template>
+
+    <div class="action-container">
+      <FindIssueButton
+        :errorMessage="props.error.exception_message"
+        repoOwner="comfyanonymous"
+        repoName="ComfyUI"
+      />
+      <Button
+        label="Open GitHub Issue"
+        icon="pi pi-external-link"
+        @click="openGitHubIssue"
+        class="p-button-secondary"
+      />
+      <Button
+        label="Copy to Clipboard"
+        icon="pi pi-copy"
+        @click="copyReportToClipboard"
+      />
+    </div>
+  </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { useClipboard } from '@vueuse/core'
 import { useToast } from 'primevue/usetoast'
 import Button from 'primevue/button'
+import Divider from 'primevue/divider'
 import ScrollPanel from 'primevue/scrollpanel'
-import FindIssueButton from './error/FindIssueButton.vue'
+import FindIssueButton from './FindIssueButton.vue'
+import type { ExecutionErrorWsMessage, SystemStats } from '@/types/apiTypes'
+import { api } from '@/scripts/api'
 
-const props = defineProps({
-  error: {
-    type: Object,
-    required: true
-  },
-  systemStats: {
-    type: Object,
-    required: true
-  }
-})
+const props = defineProps<{
+  error: ExecutionErrorWsMessage
+}>()
 
 const reportContent = ref('')
+const reportOpen = ref(false)
+const showReport = () => {
+  reportOpen.value = true
+}
+
 const toast = useToast()
 const { copy, isSupported } = useClipboard()
 
-onMounted(() => {
-  generateReport()
+onMounted(async () => {
+  generateReport(await api.getSystemStats())
 })
 
-const generateReport = () => {
+const generateReport = (systemStats: SystemStats) => {
   reportContent.value = `
 # ComfyUI Error Report
 ## Error Details
@@ -59,11 +75,11 @@ const generateReport = () => {
 ${props.error.traceback.join('\n')}
 \`\`\`
 ## System Information
-- **OS:** ${props.systemStats.system.os}
-- **Python Version:** ${props.systemStats.system.python_version}
-- **Embedded Python:** ${props.systemStats.system.embedded_python}
+- **OS:** ${systemStats.system.os}
+- **Python Version:** ${systemStats.system.python_version}
+- **Embedded Python:** ${systemStats.system.embedded_python}
 ## Devices
-${props.systemStats.devices
+${systemStats.devices
   .map(
     (device) => `
 - **Name:** ${device.name}
@@ -117,3 +133,17 @@ const openGitHubIssue = () => {
   window.open(url, '_blank')
 }
 </script>
+
+<style scoped>
+.error-report {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.action-container {
+  display: flex;
+  gap: 1rem;
+  justify-content: flex-end;
+}
+</style>
