e942da058c218a1c7bb932573f831a5990a2bf04
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-08-29 14:44:09 -0400

    Add component test (Vitest) (#681)
    * Add component test (Vitest)
    
    * Fix compile error

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./vitest.setup.ts'],
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    coverage: {
+      reporter: ['text', 'json', 'html']
+    }
+  }
+})
