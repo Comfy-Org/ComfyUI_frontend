c55ac7aba09d62503224ee84c9b85f0134360513
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-09-03 11:55:50 -0400

    Searchbox revamp

--- src/types/searchBoxTypes.ts
+++ src/types/searchBoxTypes.ts
@@ -3,3 +3,9 @@ export enum LinkReleaseTriggerMode {
   HOLD_SHIFT = 'hold shift',
   NOT_HOLD_SHIFT = 'NOT hold shift'
 }
+
+export enum LinkReleaseTriggerAction {
+  SHOW_CONTEXT_MENU = 'context menu',
+  SHOW_SEARCH_BOX = 'search box',
+  NO_ACTION = 'no action'
+}
