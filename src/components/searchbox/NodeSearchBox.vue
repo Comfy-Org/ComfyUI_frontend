7d25d976d158e5e0946121d3f9aba87357aa7cad
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-09-15 17:15:19 +0900

    Extract search option as a Vue component (#838)

--- src/components/searchbox/NodeSearchBox.vue
+++ src/components/searchbox/NodeSearchBox.vue
@@ -43,38 +43,7 @@
       :optionLabel="'display_name'"
     >
       <template v-slot:option="{ option }">
-        <div class="option-container">
-          <div class="option-display-name">
-            <div>
-              <span
-                v-html="highlightQuery(option.display_name, currentQuery)"
-              ></span>
-              <span>&nbsp;</span>
-              <Tag v-if="showIdName" severity="secondary">
-                <span v-html="highlightQuery(option.name, currentQuery)"></span>
-              </Tag>
-            </div>
-            <div v-if="showCategory" class="option-category">
-              {{ option.category.replaceAll('/', ' > ') }}
-            </div>
-          </div>
-          <div class="option-badges">
-            <Tag
-              v-if="option.experimental"
-              :value="$t('experimental')"
-              severity="primary"
-            />
-            <Tag
-              v-if="option.deprecated"
-              :value="$t('deprecated')"
-              severity="danger"
-            />
-            <NodeSourceChip
-              v-if="option.python_module !== undefined"
-              :python_module="option.python_module"
-            />
-          </div>
-        </div>
+        <NodeSearchItem :nodeDef="option" :currentQuery="currentQuery" />
       </template>
       <!-- FilterAndValue -->
       <template v-slot:chip="{ value }">
@@ -92,11 +61,10 @@
 <script setup lang="ts">
 import { computed, onMounted, ref } from 'vue'
 import AutoCompletePlus from '@/components/primevueOverride/AutoCompletePlus.vue'
-import Tag from 'primevue/tag'
 import Dialog from 'primevue/dialog'
 import Button from 'primevue/button'
 import NodeSearchFilter from '@/components/searchbox/NodeSearchFilter.vue'
-import NodeSourceChip from '@/components/node/NodeSourceChip.vue'
+import NodeSearchItem from '@/components/searchbox/NodeSearchItem.vue'
 import { type FilterAndValue } from '@/services/nodeSearchService'
 import NodePreview from '@/components/node/NodePreview.vue'
 import { ComfyNodeDefImpl, useNodeDefStore } from '@/stores/nodeDefStore'
@@ -109,12 +77,6 @@ const { t } = useI18n()
 
 const enableNodePreview = computed(() =>
   settingStore.get('Comfy.NodeSearchBoxImpl.NodePreview')
-)
-const showCategory = computed(() =>
-  settingStore.get('Comfy.NodeSearchBoxImpl.ShowCategory')
-)
-const showIdName = computed(() =>
-  settingStore.get('Comfy.NodeSearchBoxImpl.ShowIdName')
 )
 
 const props = withDefaults(
@@ -143,12 +105,6 @@ const search = (query: string) => {
       limit: props.searchLimit
     })
   ]
-}
-
-const highlightQuery = (text: string, query: string) => {
-  if (!query) return text
-  const regex = new RegExp(`(${query})`, 'gi')
-  return text.replace(regex, '<span class="highlight">$1</span>')
 }
 
 const emit = defineEmits(['addFilter', 'removeFilter', 'addNode'])
@@ -200,29 +156,6 @@ const setHoverSuggestion = (index: number) => {
 
 .comfy-vue-node-search-box {
   @apply z-10 flex-grow;
-}
-
-.option-container {
-  @apply flex justify-between items-center px-2 py-0 cursor-pointer overflow-hidden w-full;
-}
-
-.option-display-name {
-  @apply font-semibold flex flex-col;
-}
-
-.option-category {
-  @apply font-light text-sm text-gray-400 overflow-hidden text-ellipsis;
-  /* Keeps the text on a single line by default */
-  white-space: nowrap;
-}
-
-:deep(.highlight) {
-  background-color: var(--p-primary-color);
-  color: var(--p-primary-contrast-color);
-  font-weight: bold;
-  border-radius: 0.25rem;
-  padding: 0rem 0.125rem;
-  margin: -0.125rem 0.125rem;
 }
 
 ._filter-button {
