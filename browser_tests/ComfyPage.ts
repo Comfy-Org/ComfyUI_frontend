3e941e975f9b3cd79dcafe3fdb49d94842a286e2
pythongosssss <125205205+pythongosssss@users.noreply.github.com>
2024-09-16 03:28:04 +0100

    Floating menu option (#726)
    * Add floating menu
    
    * Fix
    
    * Updates
    
    * Add auto-queue change test
    
    * Fix

--- browser_tests/ComfyPage.ts
+++ browser_tests/ComfyPage.ts
@@ -1,6 +1,7 @@
 import type { Page, Locator } from '@playwright/test'
 import { test as base } from '@playwright/test'
 import { expect } from '@playwright/test'
+import { ComfyAppMenu } from './helpers/appMenu'
 import dotenv from 'dotenv'
 dotenv.config()
 import * as fs from 'fs'
@@ -228,6 +229,7 @@ export class ComfyPage {
   // Components
   public readonly searchBox: ComfyNodeSearchBox
   public readonly menu: ComfyMenu
+  public readonly appMenu: ComfyAppMenu
 
   constructor(public readonly page: Page) {
     this.url = process.env.PLAYWRIGHT_TEST_URL || 'http://localhost:8188'
@@ -238,6 +240,7 @@ export class ComfyPage {
     this.workflowUploadInput = page.locator('#comfy-file-input')
     this.searchBox = new ComfyNodeSearchBox(page)
     this.menu = new ComfyMenu(page)
+    this.appMenu = new ComfyAppMenu(page)
   }
 
   async getGraphNodesCount(): Promise<number> {
