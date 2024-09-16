eef2cdaf4233bc03fea211c1faca1c8323fa4d78
pythongosssss <125205205+pythongosssss@users.noreply.github.com>
2024-09-16 03:28:04 +0100

    Floating menu option (#726)
    * Add floating menu
    
    * Fix
    
    * Updates
    
    * Add auto-queue change test
    
    * Fix

---
+++ src/services/autoQueueService.ts
@@ -0,0 +1,41 @@
+import {
+  useQueueSettingsStore,
+  useQueuePendingTaskCountStore
+} from '@/stores/queueStore'
+import { app } from '@/scripts/app'
+import { api } from '@/scripts/api'
+
+export function setupAutoQueueHandler() {
+  const queueCountStore = useQueuePendingTaskCountStore()
+  const queueSettingsStore = useQueueSettingsStore()
+
+  let graphHasChanged = false
+  let internalCount = 0 // Use an internal counter here so it is instantly updated when re-queuing
+  api.addEventListener('graphChanged', () => {
+    if (queueSettingsStore.mode === 'change') {
+      if (internalCount) {
+        graphHasChanged = true
+      } else {
+        graphHasChanged = false
+        app.queuePrompt(0, queueSettingsStore.batchCount)
+        internalCount++
+      }
+    }
+  })
+
+  queueCountStore.$subscribe(
+    () => {
+      internalCount = queueCountStore.count
+      if (!internalCount && !app.lastExecutionError) {
+        if (
+          queueSettingsStore.mode === 'instant' ||
+          (queueSettingsStore.mode === 'change' && graphHasChanged)
+        ) {
+          graphHasChanged = false
+          app.queuePrompt(0, queueSettingsStore.batchCount)
+        }
+      }
+    },
+    { detached: true }
+  )
+}
