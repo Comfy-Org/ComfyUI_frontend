48fe14e2635aaa27bdf2de4d0a01bdcbf84f2523
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-09-17 11:11:52 +0900

    [Beta Menu] Show active workflow name on browser tab title (#857)

--- src/components/BrowserTabTitle.vue
+++ src/components/BrowserTabTitle.vue
@@ -6,6 +6,8 @@
 
 <script setup lang="ts">
 import { useExecutionStore } from '@/stores/executionStore'
+import { useSettingStore } from '@/stores/settingStore'
+import { useWorkflowStore } from '@/stores/workflowStore'
 import { useTitle } from '@vueuse/core'
 import { computed } from 'vue'
 
@@ -13,6 +15,19 @@ const executionStore = useExecutionStore()
 const executionText = computed(() =>
   executionStore.isIdle ? '' : `[${executionStore.executionProgress}%]`
 )
-const title = computed(() => executionText.value + 'ComfyUI')
+
+const settingStore = useSettingStore()
+const betaMenuEnabled = computed(
+  () => settingStore.get('Comfy.UseNewMenu') !== 'Disabled'
+)
+
+const workflowStore = useWorkflowStore()
+const workflowNameText = computed(
+  () =>
+    (betaMenuEnabled.value ? workflowStore.activeWorkflow?.name : undefined) ??
+    'ComfyUI'
+)
+
+const title = computed(() => executionText.value + workflowNameText.value)
 useTitle(title)
 </script>
