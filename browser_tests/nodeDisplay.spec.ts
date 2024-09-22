9e5f095604bada6492e15fe593a1091540f49a63
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-09-22 11:35:06 +0900

    Add playwright tests

---
+++ browser_tests/nodeDisplay.spec.ts
@@ -0,0 +1,21 @@
+import { expect } from '@playwright/test'
+import { comfyPageFixture as test } from './ComfyPage'
+
+// If an input is optional by node definition, it should be shown as
+// a hollow circle no matter what shape it was defined in the workflow JSON.
+test.describe('Optional input', () => {
+  test('No shape specified', async ({ comfyPage }) => {
+    await comfyPage.loadWorkflow('optional_input_no_shape')
+    await expect(comfyPage.canvas).toHaveScreenshot('optional_input.png')
+  })
+
+  test('Wrong shape specified', async ({ comfyPage }) => {
+    await comfyPage.loadWorkflow('optional_input_wrong_shape')
+    await expect(comfyPage.canvas).toHaveScreenshot('optional_input.png')
+  })
+
+  test('Correct shape specified', async ({ comfyPage }) => {
+    await comfyPage.loadWorkflow('optional_input_correct_shape')
+    await expect(comfyPage.canvas).toHaveScreenshot('optional_input.png')
+  })
+})
