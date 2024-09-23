cfb29d3ba6a59ebcc4b1dba94cd4dd67fe60e7c0
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-09-23 10:17:01 +0900

    Revert "Revert "Fix routing and layout issue (#923)" (#930)"
    This reverts commit 1e2dfea173299302c60b482d9fbd979caaaad6ed.

--- src/components/dialog/UnloadWindowConfirmDialog.vue
+++ src/components/dialog/UnloadWindowConfirmDialog.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
-    <!-- This component does not render anything visible. It is used to confirm
-    the user wants to close the window, and if they do, it will call the
-    beforeunload event. -->
+    <!--
+    UnloadWindowConfirmDialog: This component does not render
+    anything visible. It is used to confirm the user wants to
+    close the window, and if they do, it will call the
+    beforeunload event.
+    -->
   </div>
 </template>
 
 <script setup lang="ts">
 import { useSettingStore } from '@/stores/settingStore'
-import { onMounted, onUnmounted } from 'vue'
+import { onMounted, onBeforeUnmount } from 'vue'
 
 const settingStore = useSettingStore()
+
 const handleBeforeUnload = (event: BeforeUnloadEvent) => {
   if (settingStore.get('Comfy.Window.UnloadConfirmation')) {
     event.preventDefault()
@@ -23,7 +27,7 @@ onMounted(() => {
   window.addEventListener('beforeunload', handleBeforeUnload)
 })
 
-onUnmounted(() => {
+onBeforeUnmount(() => {
   window.removeEventListener('beforeunload', handleBeforeUnload)
 })
 </script>
