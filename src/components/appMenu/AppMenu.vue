69ab005abc5e003308484af8cf38851856cf1bee
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-09-24 17:47:09 +0900

    Rework split button

--- src/components/appMenu/AppMenu.vue
+++ src/components/appMenu/AppMenu.vue
@@ -19,7 +19,7 @@
       </ButtonGroup>
       <div class="separator"></div>
 
-      <Popover ref="queuePopover" data-testid="queue-options">
+      <!-- <Popover ref="queuePopover" data-testid="queue-options">
         <div class="queue-options">
           <p class="batch-count">
             <FloatLabel v-tooltip="$t('menu.batchCountTooltip')">
@@ -58,39 +58,44 @@
             </template>
           </p>
         </div>
-      </Popover>
-      <SplitButton
-        v-tooltip.bottom="$t('menu.queueWorkflow')"
-        :label="$t('menu.generate')"
-        :icon="`pi pi-${icon}`"
-        severity="primary"
-        @click="queuePrompt"
-        :model="[]"
-        :pt="{
-          pcDropdown: ({ instance }) => {
-            instance.onDropdownButtonClick = function (e: Event) {
-              e.preventDefault()
-              queuePopover.toggle(e)
-            }
-          }
-        }"
-        data-testid="queue-button"
-      >
-      </SplitButton>
-      <Button
-        v-tooltip.bottom="$t('menu.interrupt')"
-        icon="pi pi-times"
-        severity="danger"
-        :disabled="!executingPrompt"
-        @click="() => commandStore.getCommand('Comfy.Interrupt')()"
-      >
-      </Button>
+      </Popover> -->
+      <ButtonGroup>
+        <SplitButton
+          v-tooltip.bottom="$t('menu.queueWorkflow')"
+          :label="activeQueueModeMenuItem.label"
+          :icon="activeQueueModeMenuItem.icon"
+          severity="primary"
+          @click="queuePrompt"
+          :model="queueModeMenuItems"
+          data-testid="queue-button"
+          v-tooltip="activeQueueModeMenuItem.tooltip"
+          :pt="{}"
+        >
+          <template #item="{ item }">
+            <Button
+              :label="item.label"
+              :icon="item.icon"
+              severity="secondary"
+              text
+              v-tooltip="item.tooltip"
+            />
+          </template>
+        </SplitButton>
+        <Button
+          v-tooltip.bottom="$t('menu.interrupt')"
+          icon="pi pi-times"
+          severity="danger"
+          :disabled="!executingPrompt"
+          @click="() => commandStore.getCommand('Comfy.Interrupt')()"
+        >
+        </Button>
+      </ButtonGroup>
     </div>
   </Panel>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 import Panel from 'primevue/panel'
 import SplitButton from 'primevue/splitbutton'
 import Button from 'primevue/button'
@@ -102,6 +107,7 @@ import Slider from 'primevue/slider'
 import RadioButton from 'primevue/radiobutton'
 import ButtonGroup from 'primevue/buttongroup'
 import {
+  AutoQueueMode,
   useQueuePendingTaskCountStore,
   useQueueSettingsStore
 } from '@/stores/queueStore'
@@ -109,6 +115,8 @@ import { app } from '@/scripts/app'
 import { storeToRefs } from 'pinia'
 import { useSettingStore } from '@/stores/settingStore'
 import { useCommandStore } from '@/stores/commandStore'
+import { MenuItem } from 'primevue/menuitem'
+import { useI18n } from 'vue-i18n'
 
 const settingsStore = useSettingStore()
 const commandStore = useCommandStore()
@@ -119,19 +127,45 @@ const visible = computed(
   () => settingsStore.get('Comfy.UseNewMenu') === 'Floating'
 )
 
-const queuePopover = ref(null)
-const queueModes = ['disabled', 'instant', 'change']
-
-const icon = computed(() => {
-  switch (queueMode.value) {
-    case 'instant':
-      return 'forward'
-    case 'change':
-      return 'step-forward-alt'
-    default:
-      return 'play'
+const { t } = useI18n()
+const queueModeMenuItemLookup: Record<AutoQueueMode, MenuItem> = {
+  disabled: {
+    key: 'disabled',
+    label: 'Queue',
+    icon: 'pi pi-play',
+    tooltip: t('menu.disabledTooltip'),
+    command: () => {
+      queueMode.value = 'disabled'
+    }
+  },
+  instant: {
+    key: 'instant',
+    label: 'Queue (Instant)',
+    icon: 'pi pi-forward',
+    tooltip: t('menu.instantTooltip'),
+    command: () => {
+      queueMode.value = 'instant'
+    }
+  },
+  change: {
+    key: 'change',
+    label: 'Queue (Change)',
+    icon: 'pi pi-step-forward-alt',
+    tooltip: t('menu.changeTooltip'),
+    command: () => {
+      queueMode.value = 'change'
+    }
   }
-})
+}
+
+const activeQueueModeMenuItem = computed(
+  () => queueModeMenuItemLookup[queueMode.value]
+)
+const queueModeMenuItems = computed(() =>
+  Object.values(queueModeMenuItemLookup).filter(
+    (item) => item.key !== queueMode.value
+  )
+)
 
 const executingPrompt = computed(() => !!queueCountStore.count.value)
 
