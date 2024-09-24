0d28c108d2fad93d10f57040c14c954ed4aa396f
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-09-24 09:51:57 +0900

    Add topbar dropdown menu (#937)
    * Add basic menu
    
    * Add workflows/edit to menu bar
    
    * Add command store
    
    * Fix z-index
    
    * Fix beta menu setting switch
    
    * nit
    
    * Drop to center
    
    * Fix command invocation

---
+++ src/components/topbar/TopMenubar.vue
@@ -0,0 +1,58 @@
+<template>
+  <teleport to=".comfyui-body-top">
+    <div class="top-menubar comfyui-menu" v-if="betaMenuEnabled">
+      <h1 class="comfyui-logo mx-2">ComfyUI</h1>
+      <Menubar
+        :model="items"
+        class="border-none p-0 bg-transparent"
+        :pt="{
+          rootList: 'gap-0'
+        }"
+      />
+    </div>
+  </teleport>
+</template>
+
+<script setup lang="ts">
+import Menubar from 'primevue/menubar'
+import { useCoreMenuItemStore } from '@/stores/coreMenuItemStore'
+import { computed } from 'vue'
+import { useSettingStore } from '@/stores/settingStore'
+
+const settingStore = useSettingStore()
+const betaMenuEnabled = computed(
+  () => settingStore.get('Comfy.UseNewMenu') !== 'Disabled'
+)
+const coreMenuItemsStore = useCoreMenuItemStore()
+const items = coreMenuItemsStore.menuItems
+</script>
+
+<style scoped>
+.comfyui-menu {
+  width: 100vw;
+  background: var(--comfy-menu-bg);
+  color: var(--fg-color);
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 0.8em;
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  z-index: 1000;
+  order: 0;
+  grid-column: 1/-1;
+  overflow: auto;
+  max-height: 90vh;
+}
+
+.comfyui-logo {
+  font-size: 1.2em;
+  user-select: none;
+  cursor: default;
+}
+</style>
+
+<style>
+.top-menubar .p-menubar-item-link svg {
+  display: none;
+}
+</style>
