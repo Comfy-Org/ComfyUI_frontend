f55dfbed8133eaca34b8b4c32f2f863c9a5fdf97
huchenlei <huchenlei@proton.me>
2024-10-04 14:25:51 -0400

    style dialog

--- src/App.vue
+++ src/App.vue
@@ -23,3 +23,10 @@ onMounted(() => {
   console.log('ComfyUI Front-end version:', config.app_version)
 })
 </script>
+
+<style>
+/* Globally remove bottom padding from dialog header. More compact dialog. */
+.p-dialog-header {
+  @apply pb-0;
+}
+</style>
--- src/components/dialog/content/PromptDialogContent.vue
+++ src/components/dialog/content/PromptDialogContent.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="prompt-dialog-content">
-    <p>{{ message }}</p>
-    <InputText v-model="inputValue" @keyup.enter="onConfirm" />
-    <div class="button-container">
-      <Button @click="onConfirm">{{ $t('confirm') }}</Button>
-    </div>
+  <div class="prompt-dialog-content flex flex-col gap-2 pt-8">
+    <FloatLabel>
+      <InputText v-model="inputValue" @keyup.enter="onConfirm" />
+      <label>{{ message }}</label>
+    </FloatLabel>
+    <Button @click="onConfirm">{{ $t('confirm') }}</Button>
   </div>
 </template>
 
 <script setup lang="ts">
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
+import FloatLabel from 'primevue/floatlabel'
 import { ref } from 'vue'
 import { useDialogStore } from '@/stores/dialogStore'
 
