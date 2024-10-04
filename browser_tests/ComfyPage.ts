c8dfb662bc8cc74794e56bbdde12c98569f3a291
huchenlei <huchenlei@proton.me>
2024-10-04 16:05:53 -0400

    Add playwright test

--- browser_tests/ComfyPage.ts
+++ browser_tests/ComfyPage.ts
@@ -485,6 +485,36 @@ export class ComfyPage {
     return `./browser_tests/assets/${fileName}`
   }
 
+  async executeCommand(commandId: string) {
+    await this.page.evaluate((id: string) => {
+      return window['app'].extensionManager.command.execute(id)
+    }, commandId)
+  }
+
+  async registerCommand(
+    commandId: string,
+    command: (() => void) | (() => Promise<void>)
+  ) {
+    await this.page.evaluate(
+      ({ commandId, commandStr }) => {
+        const app = window['app']
+        const randomSuffix = Math.random().toString(36).substring(2, 8)
+        const extensionName = `TestExtension_${randomSuffix}`
+
+        app.registerExtension({
+          name: extensionName,
+          commands: [
+            {
+              id: commandId,
+              function: eval(commandStr)
+            }
+          ]
+        })
+      },
+      { commandId, commandStr: command.toString() }
+    )
+  }
+
   async registerKeybinding(keyCombo: KeyCombo, command: () => void) {
     await this.page.evaluate(
       ({ keyCombo, commandStr }) => {
