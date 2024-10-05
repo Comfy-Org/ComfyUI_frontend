9c42847b767796540e6865a9d1b7a47e0d29bf79
huchenlei <huchenlei@proton.me>
2024-10-05 16:32:21 -0400

    Add eslint-plugin-unused-imports

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginVue from 'eslint-plugin-vue'
+import unusedImports from 'eslint-plugin-unused-imports'
 
 export default [
   {
@@ -34,6 +35,17 @@ export default [
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/prefer-as-const': 'off'
+    }
+  },
+  {
+    plugins: {
+      'unused-imports': unusedImports
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/prefer-as-const': 'off',
+      'unused-imports/no-unused-imports': 'error'
     }
   }
 ]
