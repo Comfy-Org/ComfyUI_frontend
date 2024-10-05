08237e375254d36f0a49b1b5437059ade6e14030
huchenlei <huchenlei@proton.me>
2024-10-05 10:28:25 -0400

    Update test expectations

--- browser_tests/browserTabTitle.spec.ts
+++ browser_tests/browserTabTitle.spec.ts
@@ -16,7 +16,7 @@ test.describe('Browser tab title', () => {
         return window['app'].workflowManager.activeWorkflow.name
       })
       // Note: unsaved workflow name is always prepended with "*".
-      expect(await comfyPage.page.title()).toBe(`*${workflowName}`)
+      expect(await comfyPage.page.title()).toBe(`*${workflowName} - ComfyUI`)
     })
 
     // Broken by https://github.com/Comfy-Org/ComfyUI_frontend/pull/893
@@ -28,15 +28,15 @@ test.describe('Browser tab title', () => {
         return window['app'].workflowManager.activeWorkflow.name
       })
       // Note: unsaved workflow name is always prepended with "*".
-      expect(await comfyPage.page.title()).toBe(`*${workflowName}`)
+      expect(await comfyPage.page.title()).toBe(`*${workflowName} - ComfyUI`)
 
       await comfyPage.menu.saveWorkflow('test')
-      expect(await comfyPage.page.title()).toBe('test')
+      expect(await comfyPage.page.title()).toBe('test - ComfyUI')
 
       const textBox = comfyPage.widgetTextBox
       await textBox.fill('Hello World')
       await comfyPage.clickEmptySpace()
-      expect(await comfyPage.page.title()).toBe(`*test`)
+      expect(await comfyPage.page.title()).toBe(`*test - ComfyUI`)
 
       // Delete the saved workflow for cleanup.
       await comfyPage.page.evaluate(async () => {
