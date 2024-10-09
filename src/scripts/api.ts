f94bdc358bb5053bd6763a91b07e24b1f3701481
Chenlei Hu <huchenlei@proton.me>
2024-10-09 16:02:27 -0400

    Disable node def validation by default (#1190)
    * Add setting
    
    * Make node def validation optional

--- src/scripts/api.ts
+++ src/scripts/api.ts
@@ -273,9 +273,15 @@ class ComfyApi extends EventTarget {
    * Loads node object definitions for the graph
    * @returns The node definitions
    */
-  async getNodeDefs(): Promise<Record<string, ComfyNodeDef>> {
+  async getNodeDefs({ validate = false }: { validate?: boolean } = {}): Promise<
+    Record<string, ComfyNodeDef>
+  > {
     const resp = await this.fetchApi('/object_info', { cache: 'no-store' })
     const objectInfoUnsafe = await resp.json()
+    if (!validate) {
+      return objectInfoUnsafe
+    }
+    // Validate node definitions against zod schema. (slow)
     const objectInfo: Record<string, ComfyNodeDef> = {}
     for (const key in objectInfoUnsafe) {
       const validatedDef = validateComfyNodeDef(
