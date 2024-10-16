ba7b923e05dea5e665fbcf1872d7fec7bce52d20
juju <piatekjulien@gmail.com>
2024-10-16 23:38:39 +0200

    fix typecheck error

--- src/extensions/core/uploadImage.ts
+++ src/extensions/core/uploadImage.ts
@@ -1,13 +1,12 @@
-// @ts-strict-ignore
 import { app } from '../../scripts/app'
-import { ComfyNodeDef } from '@/types/apiTypes'
+import { ComfyObjectInfo } from '@/types/comfy'
 
 // Adds an upload button to the nodes
 
 app.registerExtension({
   name: 'Comfy.UploadImage',
-  async beforeRegisterNodeDef(nodeType, nodeData: ComfyNodeDef, app) {
-    if (nodeData?.input?.required?.image?.[1]?.image_upload === true) {
+  async beforeRegisterNodeDef(nodeType, nodeData: ComfyObjectInfo, app) {
+    if (nodeData.input?.required?.image?.[1]?.image_upload === true) {
       nodeData.input.required.upload = ['IMAGEUPLOAD']
     }
   }
