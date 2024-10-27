b6d853a44a221477c77955fc5a4ea7fda4e958ab
huchenlei <huchenlei@proton.me>
2024-10-27 08:18:21 -0400

    Convert workspaceStateStore

--- src/stores/workspaceStateStore.ts
+++ src/stores/workspaceStateStore.ts
@@ -1,53 +1,65 @@
-import type { SidebarTabExtension, ToastManager } from '@/types/extensionTypes'
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import type { SidebarTabExtension, ToastManager } from '@/types/extensionTypes'
 import { useToastStore } from './toastStore'
 import { useQueueSettingsStore } from './queueStore'
 import { useCommandStore } from './commandStore'
 import { useSidebarTabStore } from './workspace/sidebarTabStore'
 import { useSettingStore } from './settingStore'
 
-interface WorkspaceState {
-  spinner: boolean
-  // Whether the shift key is down globally
-  shiftDown: boolean
-}
+export const useWorkspaceStore = defineStore('workspace', () => {
+  const spinner = ref(false)
+  const shiftDown = ref(false)
 
-export const useWorkspaceStore = defineStore('workspace', {
-  state: (): WorkspaceState => ({
-    spinner: false,
-    shiftDown: false
-  }),
-  getters: {
-    toast(): ToastManager {
-      return useToastStore()
-    },
-    queueSettings() {
-      return useQueueSettingsStore()
-    },
-    command() {
-      return {
-        execute: useCommandStore().execute
-      }
-    },
-    sidebarTab() {
-      return useSidebarTabStore()
-    },
-    setting() {
-      return {
-        get: useSettingStore().get,
-        set: useSettingStore().set
-      }
-    }
-  },
-  actions: {
-    registerSidebarTab(tab: SidebarTabExtension) {
-      this.sidebarTab.registerSidebarTab(tab)
-    },
-    unregisterSidebarTab(id: string) {
-      this.sidebarTab.unregisterSidebarTab(id)
-    },
-    getSidebarTabs(): SidebarTabExtension[] {
-      return this.sidebarTab.sidebarTabs
-    }
+  const toast = computed<ToastManager>(() => useToastStore())
+  const queueSettings = computed(() => useQueueSettingsStore())
+  const command = computed(() => ({
+    execute: useCommandStore().execute
+  }))
+  const sidebarTab = computed(() => useSidebarTabStore())
+  const setting = computed(() => ({
+    get: useSettingStore().get,
+    set: useSettingStore().set
+  }))
+
+  /**
+   * Registers a sidebar tab.
+   * @param tab The sidebar tab to register.
+   * @deprecated Use `sidebarTab.registerSidebarTab` instead.
+   */
+  function registerSidebarTab(tab: SidebarTabExtension) {
+    sidebarTab.value.registerSidebarTab(tab)
+  }
+
+  /**
+   * Unregisters a sidebar tab.
+   * @param id The id of the sidebar tab to unregister.
+   * @deprecated Use `sidebarTab.unregisterSidebarTab` instead.
+   */
+  function unregisterSidebarTab(id: string) {
+    sidebarTab.value.unregisterSidebarTab(id)
+  }
+
+  /**
+   * Gets all registered sidebar tabs.
+   * @returns All registered sidebar tabs.
+   * @deprecated Use `sidebarTab.sidebarTabs` instead.
+   */
+  function getSidebarTabs(): SidebarTabExtension[] {
+    return sidebarTab.value.sidebarTabs
+  }
+
+  return {
+    spinner,
+    shiftDown,
+    toast,
+    queueSettings,
+    command,
+    sidebarTab,
+    setting,
+
+    registerSidebarTab,
+    unregisterSidebarTab,
+    getSidebarTabs
   }
 })
