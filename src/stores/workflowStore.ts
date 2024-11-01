8cab3ec499a1e69976a8433d09ca04d7f0e01266
huchenlei <huchenlei@proton.me>
2024-11-01 15:46:34 -0400

    Add openworkflowPaths

--- src/stores/workflowStore.ts
+++ src/stores/workflowStore.ts
@@ -180,6 +180,7 @@ export const useWorkflowStore = defineStore('workflow', () => {
     }
 
     activeWorkflow.value = workflow
+    openWorkflowPaths.value.push(workflow.path)
   }
 
   const createTemporary = (path?: string) => {
