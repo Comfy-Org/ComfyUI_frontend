94f1ca4279beeec82c642994ff58189eff56649d
filtered <176114999+webfiltered@users.noreply.github.com>
2024-11-02 10:00:02 +1100

    Remove future test (#249)

--- test/LGraph.test.ts
+++ test/LGraph.test.ts
@@ -1,26 +1,45 @@
-import {
-  LGraph,
-  LiteGraph,
-} from "../dist/litegraph.es.js";
+import { LGraph, LGraphGroup, LGraphNode, LiteGraph } from "../src/litegraph"
+import { LiteGraphGlobal } from "../src/LiteGraphGlobal"
 
-describe("LegacyLGraph Compatibility Layer", () => {
-  test("LGraph can be instantiated", () => {
-    const graph = new LGraph({extra: "TestGraph"});
-    expect(graph).toBeInstanceOf(LGraph);
-    expect(graph.extra).toBe("TestGraph");
-  });
+function makeGraph() {
+  const LiteGraph = new LiteGraphGlobal()
+  LiteGraph.registerNodeType("TestNode", LGraphNode)
+  LiteGraph.registerNodeType("OtherNode", LGraphNode)
+  LiteGraph.registerNodeType("", LGraphNode)
+  return new LGraph()
+}
 
-  test("LGraph can be extended via prototype", () => {
-    const graph = new LGraph();
+describe("LGraph", () => {
+  it("can be instantiated", () => {
+    // @ts-ignore TODO: Remove once relative imports fix goes in.
+    const graph = new LGraph({ extra: "TestGraph" })
+    expect(graph).toBeInstanceOf(LGraph)
+    expect(graph.extra).toBe("TestGraph")
+  })
+})
+
+describe("Legacy LGraph Compatibility Layer", () => {
+  it("can be extended via prototype", () => {
+    const graph = new LGraph()
     // @ts-expect-error Should always be an error.
     LGraph.prototype.newMethod = function () {
-      return "New method added via prototype";
-    };
+      return "New method added via prototype"
+    }
     // @ts-expect-error Should always be an error.
-    expect(graph.newMethod()).toBe("New method added via prototype");
-  });
+    expect(graph.newMethod()).toBe("New method added via prototype")
+  })
 
-  test("LegacyLGraph is correctly assigned to LiteGraph", () => {
-    expect(LiteGraph.LGraph).toBe(LGraph);
-  });
-});
+  it("is correctly assigned to LiteGraph", () => {
+    expect(LiteGraph.LGraph).toBe(LGraph)
+  })
+})
+
+describe("LGraph Serialisation", () => {
+  it("should serialise", () => {
+    const graph = new LGraph()
+    graph.add(new LGraphNode("Test Node"))
+    graph.add(new LGraphGroup("Test Group"))
+    expect(graph.nodes.length).toBe(1)
+    expect(graph.groups.length).toBe(1)
+  })
+})
