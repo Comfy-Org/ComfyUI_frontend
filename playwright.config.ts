4c19e1ba3aec49b2db035bb368d59670daf8a095
filtered <176114999+webfiltered@users.noreply.github.com>
2024-11-05 12:33:50 +1100

    Speed up E2E tests using fully parallel (#1429)
    With flaky tests / async bugs all dealt with, fullyParallel can be restored.

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   testDir: './browser_tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
