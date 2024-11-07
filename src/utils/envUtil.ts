f74f4c05eb6dea6c565a8f8d5b2f60393a52009a
huchenlei <huchenlei@proton.me>
2024-11-07 15:56:57 -0500

    Add comfyui-electron-types dependency

--- src/utils/envUtil.ts
+++ src/utils/envUtil.ts
@@ -1,3 +1,9 @@
+import { ElectronAPI } from '@comfyorg/comfyui-electron-types'
+
 export function isElectron() {
   return 'electronAPI' in window && window['electronAPI'] !== undefined
+}
+
+export function electronAPI() {
+  return window['electronAPI'] as ElectronAPI
 }
