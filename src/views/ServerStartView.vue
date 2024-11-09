242f641a1d65110a865888a74cf18ac383a0dc37
huchenlei <huchenlei@proton.me>
2024-11-09 12:12:51 -0500

    WIP

--- src/views/ServerStartView.vue
+++ src/views/ServerStartView.vue
@@ -1,17 +1,57 @@
-<!-- This is a dummy page built for electron app only. -->
-<!-- Replace this page with the electron side logic on installation & log streaming. -->
 <template>
   <div
-    class="absolute inset-0 flex flex-col items-center justify-center h-screen bg-surface-0"
+    class="font-sans flex flex-col justify-center items-center h-screen m-0 text-neutral-300 bg-neutral-900"
   >
-    <ProgressSpinner class="w-16 h-16 mb-4" />
-    <div class="text-xl">{{ $t('loading') }}{{ counter }}</div>
+    <FirstTimeSetup
+      v-if="showSetup"
+      :initial-path="defaultInstallLocation"
+      @complete="showSetup = false"
+    />
+    <ProgressOverlay v-else :status="status" :logs="logs" />
   </div>
 </template>
 
 <script setup lang="ts">
-import ProgressSpinner from 'primevue/progressspinner'
-import { useInterval } from '@vueuse/core'
+import { ref, onMounted } from 'vue'
+import ProgressOverlay from './screens/ProgressOverlay.vue'
+import FirstTimeSetup from './screens/FirstTimeSetup.vue'
+import { ProgressStatus } from '@comfyorg/comfyui-electron-types'
+import { electronAPI as getElectronAPI } from '@/utils/envUtil'
 
-const counter = useInterval(1000)
+const electronAPI = getElectronAPI()
+
+const showSetup = ref<boolean | null>(null)
+const status = ref<ProgressStatus>(ProgressStatus.INITIAL_STATE)
+const logs = ref<string[]>([])
+const defaultInstallLocation = ref('')
+
+const updateProgress = ({ status: newStatus }: { status: ProgressStatus }) => {
+  status.value = newStatus
+  logs.value = [] // Clear logs when status changes
+}
+
+const addLogMessage = (message: string) => {
+  logs.value = [...logs.value, message]
+}
+
+onMounted(() => {
+  electronAPI.sendReady()
+
+  electronAPI.onShowSelectDirectory(() => {
+    showSetup.value = true
+  })
+
+  electronAPI.onFirstTimeSetupComplete(() => {
+    showSetup.value = false
+  })
+
+  electronAPI.onProgressUpdate(updateProgress)
+  electronAPI.onLogMessage((message: string) => {
+    addLogMessage(message)
+  })
+
+  electronAPI.onDefaultInstallLocation((location: string) => {
+    defaultInstallLocation.value = location
+  })
+})
 </script>
