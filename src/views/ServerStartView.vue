f1395e72f669d218ace61857099ecfa4c850d62e
huchenlei <huchenlei@proton.me>
2024-11-09 22:30:08 -0500

    nit

--- src/views/ServerStartView.vue
+++ src/views/ServerStartView.vue
@@ -1,22 +1,14 @@
 <template>
   <div
-    class="font-sans flex flex-col justify-center items-center h-screen m-0 text-neutral-300 bg-neutral-900"
+    class="font-sans flex flex-col justify-center items-center h-screen m-0 text-neutral-300 bg-neutral-900 dark-theme pointer-events-auto"
   >
-    <FirstTimeSetup
-      v-if="showSetup"
-      :initial-path="defaultInstallLocation"
-      @complete="showSetup = false"
-    />
-    <div v-else>
-      <h2 class="text-2xl font-bold">{{ ProgressMessages[status] }}</h2>
-      <LogTerminal :fetch-logs="fetchLogs" :fetch-interval="500" />
-    </div>
+    <h2 class="text-2xl font-bold">{{ ProgressMessages[status] }}</h2>
+    <LogTerminal :fetch-logs="fetchLogs" :fetch-interval="500" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import FirstTimeSetup from './screens/FirstTimeSetup.vue'
 import LogTerminal from '@/components/common/LogTerminal.vue'
 import {
   ProgressStatus,
@@ -26,10 +18,8 @@ import { electronAPI as getElectronAPI } from '@/utils/envUtil'
 
 const electronAPI = getElectronAPI()
 
-const showSetup = ref<boolean | null>(null)
 const status = ref<ProgressStatus>(ProgressStatus.INITIAL_STATE)
 const logs = ref<string[]>([])
-const defaultInstallLocation = ref('')
 
 const updateProgress = ({ status: newStatus }: { status: ProgressStatus }) => {
   status.value = newStatus
@@ -46,15 +36,6 @@ const fetchLogs = async () => {
 
 onMounted(() => {
   electronAPI.sendReady()
-
-  electronAPI.onShowSelectDirectory(() => {
-    showSetup.value = true
-  })
-
-  electronAPI.onFirstTimeSetupComplete(() => {
-    showSetup.value = false
-  })
-
   electronAPI.onProgressUpdate(updateProgress)
   electronAPI.onLogMessage((message: string) => {
     addLogMessage(message)
