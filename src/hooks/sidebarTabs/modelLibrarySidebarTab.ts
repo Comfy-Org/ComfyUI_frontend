41e3759568bba28fa188e64726be11ccaaa91c36
Oto Ciulis <oto.ciulis@gmail.com>
2024-11-15 07:51:23 -0800

    feat: Adding download count badge to sidebar

--- src/hooks/sidebarTabs/modelLibrarySidebarTab.ts
+++ src/hooks/sidebarTabs/modelLibrarySidebarTab.ts
@@ -2,15 +2,28 @@ import { markRaw } from 'vue'
 import { useI18n } from 'vue-i18n'
 import ModelLibrarySidebarTab from '@/components/sidebar/tabs/ModelLibrarySidebarTab.vue'
 import type { SidebarTabExtension } from '@/types/extensionTypes'
+import {
+  type ElectronDownload,
+  useElectronDownloadStore
+} from '@/stores/electronDownloadStore'
 
 export const useModelLibrarySidebarTab = (): SidebarTabExtension => {
   const { t } = useI18n()
+  const electronDownloadStore = useElectronDownloadStore()
+
   return {
     id: 'model-library',
     icon: 'pi pi-box',
     title: t('sideToolbar.modelLibrary'),
     tooltip: t('sideToolbar.modelLibrary'),
     component: markRaw(ModelLibrarySidebarTab),
-    type: 'vue'
+    type: 'vue',
+    iconBadge: () => {
+      if (electronDownloadStore.downloads.length > 0) {
+        return electronDownloadStore.downloads.length.toString()
+      }
+
+      return null
+    }
   }
 }
