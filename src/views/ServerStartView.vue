d944e8d7a4980ee37fda3f3706898b87d7a3dc37
pythongosssss <125205205+pythongosssss@users.noreply.github.com>
2024-11-18 11:17:33 +0000

    Use xterm for startup logs

--- src/views/ServerStartView.vue
+++ src/views/ServerStartView.vue
@@ -3,18 +3,19 @@
     class="font-sans flex flex-col justify-center items-center h-screen m-0 text-neutral-300 bg-neutral-900 dark-theme pointer-events-auto"
   >
     <h2 class="text-2xl font-bold">{{ ProgressMessages[status] }}</h2>
-    <LogTerminal :fetch-logs="fetchLogs" :fetch-interval="500" />
+    <BaseTerminal @created="terminalCreated" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import LogTerminal from '@/components/common/LogTerminal.vue'
+import { ref, onMounted, Ref } from 'vue'
+import BaseTerminal from '@/components/bottomPanel/tabs/terminal/BaseTerminal.vue'
 import {
   ProgressStatus,
   ProgressMessages
 } from '@comfyorg/comfyui-electron-types'
 import { electronAPI } from '@/utils/envUtil'
+import type { useTerminal } from '@/hooks/bottomPanelTabs/useTerminal'
 
 const electron = electronAPI()
 
@@ -26,19 +27,18 @@ const updateProgress = ({ status: newStatus }: { status: ProgressStatus }) => {
   logs.value = [] // Clear logs when status changes
 }
 
-const addLogMessage = (message: string) => {
-  logs.value = [...logs.value, message]
-}
-
-const fetchLogs = async () => {
-  return logs.value.join('\n')
+const terminalCreated = (
+  { terminal, useAutoSize }: ReturnType<typeof useTerminal>,
+  root: Ref<HTMLElement>
+) => {
+  useAutoSize(root, true, true)
+  electron.onLogMessage((message: string) => {
+    terminal.write(message)
+  })
 }
 
 onMounted(() => {
   electron.sendReady()
   electron.onProgressUpdate(updateProgress)
-  electron.onLogMessage((message: string) => {
-    addLogMessage(message)
-  })
 })
 </script>
