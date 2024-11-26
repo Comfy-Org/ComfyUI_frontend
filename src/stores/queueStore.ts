ad56780d07b4f95897a7021857a10418f011f119
huchenlei <huchenlei@proton.me>
2024-11-26 13:13:11 -0500

    Revert "Filter cached/canceled results (#1586)"
    This reverts commit 6fbf1248f4a9c8b159f954c45f7296ca2f1717dd.

--- src/stores/queueStore.ts
+++ src/stores/queueStore.ts
@@ -30,7 +30,6 @@ export class ResultItemImpl {
   filename: string
   subfolder: string
   type: string
-  cached: boolean
 
   nodeId: NodeId
   // 'audio' | 'images' | ...
@@ -44,7 +43,6 @@ export class ResultItemImpl {
     this.filename = obj.filename ?? ''
     this.subfolder = obj.subfolder ?? ''
     this.type = obj.type ?? ''
-    this.cached = obj.cached
 
     this.nodeId = obj.nodeId
     this.mediaType = obj.mediaType
@@ -151,13 +149,6 @@ export class TaskItemImpl {
     if (!this.outputs) {
       return []
     }
-
-    const cachedEntries = new Set(
-      this.status?.messages?.find((message) => {
-        return message[0] === 'execution_cached'
-      })?.[1].nodes
-    )
-
     return Object.entries(this.outputs).flatMap(([nodeId, nodeOutputs]) =>
       Object.entries(nodeOutputs).flatMap(([mediaType, items]) =>
         (items as ResultItem[]).map(
@@ -165,8 +156,7 @@ export class TaskItemImpl {
             new ResultItemImpl({
               ...item,
               nodeId,
-              mediaType,
-              cached: cachedEntries.has(nodeId)
+              mediaType
             })
         )
       )
--- src/types/apiTypes.ts
+++ src/types/apiTypes.ts
@@ -454,11 +454,6 @@ const zNodeBadgeMode = z.enum(
   Object.values(NodeBadgeMode) as [string, ...string[]]
 )
 
-const zQueueFilter = z.object({
-  hideCached: z.boolean(),
-  hideCanceled: z.boolean()
-})
-
 const zSettings = z.record(z.any()).and(
   z
     .object({
@@ -511,8 +506,6 @@ const zSettings = z.record(z.any()).and(
       'Comfy.Validation.Workflows': z.boolean(),
       'Comfy.Workflow.SortNodeIdOnSave': z.boolean(),
       'Comfy.Queue.ImageFit': z.enum(['contain', 'cover']),
-      'Comfy.Queue.ShowFlatList': z.boolean(),
-      'Comfy.Queue.Filter': zQueueFilter.passthrough(),
       'Comfy.Workflow.WorkflowTabsPosition': z.enum(['Sidebar', 'Topbar']),
       'Comfy.Node.DoubleClickTitleToEdit': z.boolean(),
       'Comfy.Window.UnloadConfirmation': z.boolean(),
