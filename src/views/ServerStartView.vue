7ca46bd25ca5caafecfed37d06b7c80e56a8ade5
huchenlei <huchenlei@proton.me>
2024-11-26 14:19:58 -0500

    [Electron] Add version number to error state

--- src/views/ServerStartView.vue
+++ src/views/ServerStartView.vue
@@ -2,9 +2,14 @@
   <div
     class="font-sans flex flex-col justify-center items-center h-screen m-0 text-neutral-300 bg-neutral-900 dark-theme pointer-events-auto"
   >
-    <h2 class="text-2xl font-bold">{{ t(`serverStart.process.${status}`) }}</h2>
+    <h2 class="text-2xl font-bold">
+      {{ t(`serverStart.process.${status}`) }}
+      <span v-if="status === ProgressStatus.ERROR">
+        v{{ electronVersion }}
+      </span>
+    </h2>
     <div
-      v-if="status == ProgressStatus.ERROR"
+      v-if="status === ProgressStatus.ERROR"
       class="flex items-center my-4 gap-2"
     >
       <Button
@@ -43,6 +48,7 @@ const electron = electronAPI()
 const { t } = useI18n()
 
 const status = ref<ProgressStatus>(ProgressStatus.INITIAL_STATE)
+const electronVersion = ref<string>('')
 let xterm: Terminal | undefined
 
 const updateProgress = ({ status: newStatus }: { status: ProgressStatus }) => {
@@ -72,9 +78,10 @@ const reportIssue = () => {
 }
 const openLogs = () => electron.openLogsFolder()
 
-onMounted(() => {
+onMounted(async () => {
   electron.sendReady()
   electron.onProgressUpdate(updateProgress)
+  electronVersion.value = await electron.getElectronVersion()
 })
 </script>
 
