146e790d473bef606b87694c85bc3229755be928
huchenlei <huchenlei@proton.me>
2024-11-27 15:31:12 -0500

    nit

--- src/extensions/core/dynamicPrompts.ts
+++ src/extensions/core/dynamicPrompts.ts
@@ -17,7 +17,8 @@ app.registerExtension({
     if (node.widgets) {
       // Locate dynamic prompt text widgets
       // Include any widgets with dynamicPrompts set to true, and customtext
-      const widgets = node.widgets.filter((n) => 'dynamicPrompts' in n)
+      // @ts-expect-error dynamicPrompts is not typed
+      const widgets = node.widgets.filter((n) => n.dynamicPrompts)
       for (const widget of widgets) {
         // Override the serialization of the value to resolve dynamic prompts for all widgets supporting it in this node
         // @ts-expect-error hacky override
