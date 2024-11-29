ffac95745799178605d5a6ba469f9f1c04007189
huchenlei <huchenlei@proton.me>
2024-11-28 22:26:37 -0500

    Add username to logout button tooltip

--- src/components/sidebar/SidebarLogoutIcon.vue
+++ src/components/sidebar/SidebarLogoutIcon.vue
@@ -1,17 +1,19 @@
 <template>
-  <SidebarIcon
-    icon="pi pi-sign-out"
-    :tooltip="$t('sideToolbar.logout')"
-    @click="logout"
-  />
+  <SidebarIcon icon="pi pi-sign-out" :tooltip="tooltip" @click="logout" />
 </template>
 
 <script setup lang="ts">
 import SidebarIcon from './SidebarIcon.vue'
 import { useUserStore } from '@/stores/userStore'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 
+const { t } = useI18n()
 const userStore = useUserStore()
 
+const tooltip = computed(
+  () => `${t('sideToolbar.logout')} (${userStore.currentUser?.username})`
+)
 const logout = () => {
   userStore.logout()
   window.location.reload()
