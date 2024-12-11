6675615437950c3f385562625d2eed6319f7f9b5
huchenlei <huchenlei@proton.me>
2024-12-11 11:32:24 -0800

    [i18n] Fix ko translation schema

--- src/i18n.ts
+++ src/i18n.ts
@@ -5,18 +5,20 @@ import ru from './locales/ru/main.json'
 import ja from './locales/ja/main.json'
 import ko from './locales/ko/main.json'
 
+const messages: Record<string, typeof en> = {
+  en,
+  zh,
+  ru,
+  ja,
+  ko
+}
+
 export const i18n = createI18n({
   // Must set `false`, as Vue I18n Legacy API is for Vue 2
   legacy: false,
   locale: navigator.language.split('-')[0] || 'en',
   fallbackLocale: 'en',
-  messages: {
-    en,
-    zh,
-    ru,
-    ja,
-    ko
-  },
+  messages,
   // Ignore warnings for locale options as each option is in its own language.
   // e.g. "English", "中文", "Русский", "日本語", "한국어"
   missingWarn: /^(?!settingsDialog\.Comfy_Locale\.options\.).+/,
