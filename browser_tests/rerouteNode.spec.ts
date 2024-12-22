9c8f4e52329be94d844b105a8ca4fc54800dec70
christian-byrne <abolkonsky.rem@gmail.com>
2024-12-21 23:15:08 -0700

    Add playwright test

---
+++ browser_tests/rerouteNode.spec.ts
@@ -0,0 +1,25 @@
+import { expect } from '@playwright/test'
+import { comfyPageFixture as test } from './fixtures/ComfyPage'
+
+test.describe('Reroute Node', () => {
+  const WORKFLOW_NAME = 'reroute_nodes.json'
+
+  test('loads from inserted workflow', async ({ comfyPage }) => {
+    await comfyPage.setupWorkflowsDirectory({
+      [WORKFLOW_NAME]: WORKFLOW_NAME
+    })
+    await comfyPage.setup()
+
+    await comfyPage.menu.workflowsTab.open()
+    await comfyPage.menu.workflowsTab
+      .getPersistedItem(WORKFLOW_NAME)
+      .click({ button: 'right' })
+
+    const insertButton = comfyPage.page.locator('.p-contextmenu-item-link', {
+      hasText: 'Insert'
+    })
+    await insertButton.click()
+
+    await expect(comfyPage.canvas).toHaveScreenshot('reroute_inserted.png')
+  })
+})
