26a4f678568623be67cf4ce5051108ab1f5ffc7f
bezo97 <bezozo97@gmail.com>
2024-12-23 22:41:02 +0100

    Setup new TemplateWorkflowsContent layout: - Show selection list for custom_nodes that contain workflow templates - Put item cards in carousel - Use getWorkflowTemplates() to fetch items

--- src/components/templates/TemplateWorkflowsContent.vue
+++ src/components/templates/TemplateWorkflowsContent.vue
@@ -1,56 +1,99 @@
 <template>
-  <div
-    class="flex flex-wrap content-around justify-around gap-4 mt-4"
-    data-testid="template-workflows-content"
-  >
-    <div
-      v-for="template in templates"
-      :key="template"
-      :data-testid="`template-workflow-${template}`"
+  <div class="flex">
+    <Listbox v-model="selectedTab" :options="tabs" optionLabel="title" />
+    <Carousel
+      class="carousel"
+      :value="selectedTab.templates"
+      :numVisible="4"
+      :numScroll="3"
+      :key="selectedTab.id"
     >
-      <Card>
-        <template #header>
-          <div
-            class="relative overflow-hidden rounded-lg cursor-pointer"
-            @click="loadWorkflow(template)"
-          >
-            <img
-              :src="`templates/${template}.jpg`"
-              class="w-64 h-64 rounded-lg object-cover"
-            />
-            <a>
+      <template #item="slotProps">
+        <Card>
+          <template #header>
+            <div class="flex center justify-center">
               <div
-                class="absolute top-0 left-0 w-64 h-64 overflow-hidden opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-opacity-50 bg-black flex items-center justify-center"
+                class="relative overflow-hidden rounded-lg cursor-pointer w-64 h-64"
+                @click="loadWorkflow(slotProps.data)"
               >
-                <i class="pi pi-play-circle"></i>
+                <img
+                  :src="`templates/${slotProps.data}.jpg`"
+                  class="w-64 h-64 rounded-lg object-cover thumbnail"
+                />
+                <a>
+                  <div
+                    class="absolute top-0 left-0 w-64 h-64 overflow-hidden opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-opacity-50 bg-black flex items-center justify-center"
+                  >
+                    <i class="pi pi-play-circle" style="color: white"></i>
+                  </div>
+                </a>
+                <ProgressSpinner
+                  v-if="loading === slotProps.data"
+                  class="absolute inset-0 z-1 w-3/12 h-full"
+                />
               </div>
-            </a>
-            <ProgressSpinner
-              v-if="loading === template"
-              class="absolute inset-0 z-1 w-3/12 h-full"
-            />
-          </div>
-        </template>
-        <template #subtitle>{{
-          $t(`templateWorkflows.template.${template}`)
-        }}</template>
-      </Card>
-    </div>
+            </div>
+          </template>
+          <template #subtitle>{{ slotProps.data }}</template>
+        </Card>
+      </template>
+    </Carousel>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useDialogStore } from '@/stores/dialogStore'
+import Carousel from 'primevue/carousel'
+import Listbox from 'primevue/listbox'
 import Card from 'primevue/card'
 import ProgressSpinner from 'primevue/progressspinner'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { app } from '@/scripts/app'
 import { api } from '@/scripts/api'
 import { useI18n } from 'vue-i18n'
+import toast from 'primevue/toast'
 const { t } = useI18n()
 
-const templates = ['default', 'image2image', 'upscale', 'flux_schnell']
 const loading = ref<string | null>(null)
+
+interface WorkflowTemplatesTab {
+  id: string
+  title: string
+  templates: string[]
+}
+
+//These default templates are provided by the frontend
+const comfyUITemplates = {
+  id: 'default',
+  title: 'ComfyUI',
+  templates: ['default', 'image2image', 'upscale', 'flux_schnell']
+}
+
+const tabs = ref<WorkflowTemplatesTab[]>([comfyUITemplates])
+
+const selectedTab = ref<WorkflowTemplatesTab>(comfyUITemplates)
+
+onMounted(async () => {
+  try {
+    const workflowTemplates = await api.getWorkflowTemplates()
+    tabs.value = [
+      comfyUITemplates,
+      ...Object.entries(workflowTemplates).map(([key, value]) => ({
+        id: key,
+        title: key,
+        templates: value
+      }))
+    ]
+  } catch (error) {
+    console.error('Error fetching workflow templates:', error)
+    toast.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: 'Failed to fetch workflow templates',
+      life: 5000
+    })
+  }
+})
 
 const loadWorkflow = async (id: string) => {
   loading.value = id
@@ -62,7 +105,7 @@ const loadWorkflow = async (id: string) => {
     json,
     true,
     true,
-    t(`templateWorkflows.template.${id}`)
+    t(`templateWorkflows.template.${id}`, id)
   )
 
   return false
@@ -77,5 +120,22 @@ const loadWorkflow = async (id: string) => {
 
 :deep(.p-card-subtitle) {
   text-align: center;
+}
+
+.carousel {
+  width: 1300px;
+}
+
+/* Fallback graphics for workflows that don't have an image. */
+img.thumbnail::before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: var(--comfy-menu-secondary-bg);
+  color: var(--fg-color);
+  content: '🗎';
+  text-align: center;
+  align-content: center;
+  font-size: 64px;
 }
 </style>
