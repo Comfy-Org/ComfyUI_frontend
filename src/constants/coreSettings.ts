4082bb5ca37f7cb017ba9b76f387682781397960
huchenlei <huchenlei@proton.me>
2024-12-24 23:01:24 -0500

    nit

--- src/constants/coreSettings.ts
+++ src/constants/coreSettings.ts
@@ -670,13 +670,13 @@ export const CORE_SETTINGS: SettingParams[] = [
     name: 'The active color palette id',
     type: 'hidden',
     defaultValue: 'dark',
-    versionModified: '1.6.6'
+    versionModified: '1.6.7'
   },
   {
     id: 'Comfy.CustomColorPalettes',
     name: 'Custom color palettes',
     type: 'hidden',
     defaultValue: {} as ColorPalettes,
-    versionModified: '1.6.6'
+    versionModified: '1.6.7'
   }
 ]
