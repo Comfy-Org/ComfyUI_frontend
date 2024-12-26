30513b253ae38e71a3f9868338488d92d39ee70e
huchenlei <huchenlei@proton.me>
2024-12-26 14:12:28 -0500

    [i18n] Add French translation

--- src/constants/coreSettings.ts
+++ src/constants/coreSettings.ts
@@ -268,7 +268,8 @@ export const CORE_SETTINGS: SettingParams[] = [
       { value: 'zh', text: '中文' },
       { value: 'ru', text: 'Русский' },
       { value: 'ja', text: '日本語' },
-      { value: 'ko', text: '한국어' }
+      { value: 'ko', text: '한국어' },
+      { value: 'fr', text: 'Français' }
     ],
     defaultValue: () => navigator.language.split('-')[0] || 'en'
   },
