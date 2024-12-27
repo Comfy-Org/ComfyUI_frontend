0b161c11953d8a4c755a4cda7f5e41a6c4303302
filtered <176114999+webfiltered@users.noreply.github.com>
2024-12-28 05:51:53 +1100

    Fix scaled font blurriness in context menus (#400)
    Requires that fonts scale in increments of 0.25.

--- src/ContextMenu.ts
+++ src/ContextMenu.ts
@@ -194,7 +194,7 @@ export class ContextMenu {
     root.style.left = left + "px"
     root.style.top = top + "px"
 
-    if (options.scale) root.style.transform = `scale(${options.scale})`
+    if (options.scale) root.style.transform = `scale(${Math.round(options.scale * 4) * 0.25})`
   }
 
   addItem(
