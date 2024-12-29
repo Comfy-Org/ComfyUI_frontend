2430d97feb8e83b3d9ccee9c178cf1fa117e3982
bezo97 <bezozo97@gmail.com>
2024-12-29 16:00:59 +0100

    show document icon when a workflow thumbnail is not available

--- src/components/templates/TemplateWorkflowCard.vue
+++ src/components/templates/TemplateWorkflowCard.vue
@@ -6,13 +6,18 @@
           class="relative overflow-hidden rounded-lg cursor-pointer w-64 h-64"
         >
           <img
+            v-if="!imageError"
             :src="
               props.moduleName === 'default'
                 ? `templates/${props.workflowName}.jpg`
                 : `api/workflow_templates/${props.moduleName}/${props.workflowName}.jpg`
             "
+            @error="imageError = true"
             class="w-64 h-64 rounded-lg object-cover thumbnail"
           />
+          <div v-else class="w-64 h-64 content-center text-center">
+            <i class="pi pi-file" style="font-size: 4rem"></i>
+          </div>
           <a>
             <div
               class="absolute top-0 left-0 w-64 h-64 overflow-hidden opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-opacity-50 bg-black flex items-center justify-center"
@@ -36,12 +41,15 @@
 <script setup lang="ts">
 import Card from 'primevue/card'
 import ProgressSpinner from 'primevue/progressspinner'
+import { ref } from 'vue'
 
 const props = defineProps<{
   moduleName: string
   workflowName: string
   loading: boolean
 }>()
+
+const imageError = ref(false)
 </script>
 
 <style lang="css" scoped>
