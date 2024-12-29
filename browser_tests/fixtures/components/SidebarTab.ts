0b1d43142946133d1ac54f872d90d3765e24e237
christian-byrne <abolkonsky.rem@gmail.com>
2024-12-29 13:32:58 -0700

    Add queue sidebar fixture

--- browser_tests/fixtures/components/SidebarTab.ts
+++ browser_tests/fixtures/components/SidebarTab.ts
@@ -147,3 +147,98 @@ export class WorkflowsSidebarTab extends SidebarTab {
     await this.page.waitForTimeout(300)
   }
 }
+
+export class QueueSidebarTab extends SidebarTab {
+  constructor(public readonly page: Page) {
+    super(page, 'queue')
+  }
+
+  get root() {
+    return this.page.locator('.sidebar-content-container', { hasText: 'Queue' })
+  }
+
+  get clearButton() {
+    return this.root.locator('.clear-all-button')
+  }
+
+  get scrollContainer() {
+    return this.root.locator('.scroll-container')
+  }
+
+  get placeholder() {
+    return this.root.locator('.no-results-placeholder')
+  }
+
+  get tasks() {
+    return this.scrollContainer.locator('[data-virtual-grid-item]')
+  }
+
+  get visibleTasks() {
+    return this.scrollContainer
+      .locator('[data-virtual-grid-item]')
+      .locator('visible=true')
+  }
+
+  async open() {
+    await super.open()
+    await this.root.waitFor({ state: 'visible' })
+  }
+
+  async close() {
+    if (!this.tabButton.isVisible()) {
+      return
+    }
+    await this.tabButton.click()
+    await this.root.waitFor({ state: 'hidden' })
+  }
+
+  async waitForMostRecentTask() {
+    return Promise.all([
+      this.tasks.first().waitFor({ state: 'visible' }),
+      this.tasks.first().waitFor({ state: 'attached' })
+    ])
+  }
+
+  async waitForLeastRecentTask() {
+    return Promise.all([
+      this.tasks.last().waitFor({ state: 'visible' }),
+      this.tasks.last().waitFor({ state: 'attached' })
+    ])
+  }
+
+  async waitForTasks() {
+    return Promise.all([
+      this.waitForMostRecentTask(),
+      this.waitForLeastRecentTask()
+    ])
+  }
+
+  async scrollTasksUp() {
+    await this.tasks.first().scrollIntoViewIfNeeded()
+    await this.waitForMostRecentTask()
+    await this.page.waitForTimeout(256)
+  }
+
+  async scrollTasksDown() {
+    await this.tasks.last().scrollIntoViewIfNeeded()
+    await this.waitForLeastRecentTask()
+    await this.page.waitForTimeout(256)
+  }
+
+  async setWidth(width: number) {
+    return this.page.evaluate((width) => {
+      localStorage.setItem('queue', JSON.stringify([width, 100 - width]))
+    }, width)
+  }
+
+  async clearTasks() {
+    // Allow delete-history requests
+    await this.page.unroute('**/api/history*')
+
+    await this.clearButton.click()
+    const confirmButton = this.page.getByLabel('Delete')
+    await confirmButton.waitFor({ state: 'visible' })
+    await confirmButton.click()
+    await this.placeholder.waitFor({ state: 'visible' })
+  }
+}
