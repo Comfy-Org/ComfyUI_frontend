5972a07cfe47b203f689f9d8d7b36814bd77be26
Chenlei Hu <huchenlei@proton.me>
2025-01-03 11:20:25 -0500

    1.6 patches (#2141)

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -49,7 +49,7 @@ import {
 } from './pnginfo'
 import { $el, ComfyUI } from './ui'
 import { ComfyAppMenu } from './ui/menu/index'
-import { getStorageValue } from './utils'
+import { clone, getStorageValue } from './utils'
 import { type ComfyWidgetConstructor, ComfyWidgets } from './widgets'
 
 export const ANIM_PREVIEW_WIDGET = '$$comfy_animation_preview'
@@ -1271,11 +1271,7 @@ export class ComfyApp {
       reset_invalid_values = true
     }
 
-    if (typeof structuredClone === 'undefined') {
-      graphData = JSON.parse(JSON.stringify(graphData))
-    } else {
-      graphData = structuredClone(graphData)
-    }
+    graphData = clone(graphData)
 
     if (useSettingStore().get('Comfy.Validation.Workflows')) {
       // TODO: Show validation error in a dialog.
