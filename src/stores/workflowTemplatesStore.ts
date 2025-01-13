238ac90fcc3f8fdae59b74f7f9c1db8faa94ce8e
christian-byrne <abolkonsky.rem@gmail.com>
2025-01-13 15:04:04 -0700

    Refactor workflow templates management and introduce core templates

--- src/stores/workflowTemplatesStore.ts
+++ src/stores/workflowTemplatesStore.ts
@@ -1,20 +1,33 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { computed } from 'vue'
 
+import { CORE_TEMPLATES } from '@/constants/coreTemplates'
 import { api } from '@/scripts/api'
+import type { WorkflowTemplates } from '@/types/workflowTemplateTypes'
 
 export const useWorkflowTemplatesStore = defineStore(
   'workflowTemplates',
   () => {
-    const items = ref<{
-      [customNodesName: string]: string[]
-    }>({})
+    const customTemplates = ref<{ [moduleName: string]: string[] }>({})
     const isLoaded = ref(false)
+    const defaultTemplate = CORE_TEMPLATES[0]
+
+    const templates = computed<WorkflowTemplates[]>(() => [
+      ...CORE_TEMPLATES,
+      ...Object.entries(customTemplates.value).map(
+        ([moduleName, templates]) => ({
+          moduleName,
+          title: moduleName,
+          templates
+        })
+      )
+    ])
 
     async function loadWorkflowTemplates() {
       try {
         if (!isLoaded.value) {
-          items.value = await api.getWorkflowTemplates()
+          customTemplates.value = await api.getWorkflowTemplates()
           isLoaded.value = true
         }
       } catch (error) {
@@ -23,8 +36,10 @@ export const useWorkflowTemplatesStore = defineStore(
     }
 
     return {
-      items,
+      templates,
+      defaultTemplate,
       isLoaded,
+
       loadWorkflowTemplates
     }
   }
---
+++ src/types/workflowTemplateTypes.ts
@@ -0,0 +1,7 @@
+export interface WorkflowTemplates {
+  moduleName: string
+  /** The title (display name) of the associated module */
+  title: string
+  /** The list of template workflow filenames for the associated module */
+  templates: string[]
+}
