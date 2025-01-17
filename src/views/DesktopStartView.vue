6988899ff536530381275503db3365f4b41db067
filtered <176114999+webfiltered@users.noreply.github.com>
2025-01-17 16:05:24 +1100

    Add desktop start screen

---
+++ src/views/DesktopStartView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="w-screen h-screen grid items-center justify-around text-neutral-300 bg-neutral-900 dark-theme pointer-events-auto overflow-y-auto"
+  >
+    <div class="max-w-screen-sm w-screen p-8">
+      <ProgressBar mode="indeterminate" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ProgressBar from 'primevue/progressbar'
+</script>
