c53e2b74dede9725d7a26e3319efa59f9dedf56b
huchenlei <huchenlei@proton.me>
2025-01-19 18:41:27 -0500

    Add g.feedback

--- src/components/dialog/content/FeedbackDialogContent.vue
+++ src/components/dialog/content/FeedbackDialogContent.vue
@@ -3,7 +3,7 @@
     <Card>
       <template #content>
         <div class="flex flex-col items-center">
-          <h3 class="text-4xl">{{ $t('menuLabels.Feedback') }}</h3>
+          <h3 class="text-4xl">{{ $t('g.feedback') }}</h3>
           <Rating
             v-model="rating"
             class="flex justify-center"
