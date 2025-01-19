8da5dcbc821fadd2b98636546c99aa10417adf93
filtered <176114999+webfiltered@users.noreply.github.com>
2025-01-20 06:30:16 +1100

    Fix TS types

--- src/types/desktop/maintenanceTypes.ts
+++ src/types/desktop/maintenanceTypes.ts
@@ -3,45 +3,45 @@ import type { MaybeRef } from 'vue'
 import type { VueSeverity } from '../primeVueTypes'
 
 interface MaintenanceTaskButton {
-  text?: string
+  readonly text?: string
   /** e.g. 'pi pi-external-link' */
-  icon?: string
+  readonly icon?: string
 }
 
 export interface MaintenanceTask {
   /** ID string used as i18n key */
-  id: string
+  readonly id: string
   /** Short name for status, e.g. Git */
-  name: string
-  description?: string
-  descriptionOk?: string
-  errorDescription?: string
-  detail?: string
+  readonly name: string
+  readonly description?: string
+  readonly descriptionOk?: string
+  readonly errorDescription?: string
+  readonly detail?: string
   /** URL to the image to show in card mode. */
-  headerImg?: string
-  button?: MaintenanceTaskButton
+  readonly headerImg?: string
+  readonly button?: MaintenanceTaskButton
   state?: 'warning' | 'error' | 'resolved' | 'OK' | 'skipped'
   /** `true` if the task has been resolved (was `error`, now `OK`). */
   resolved?: boolean
   loading?: MaybeRef<boolean>
   /** Whether to show a confirmation dialog before running the task. */
-  requireConfirm?: boolean
-  confirmText?: string
+  readonly requireConfirm?: boolean
+  readonly confirmText?: string
   /** Function to run when the button is clicked (or when confirmed, if required). */
   onClick?: (args?: unknown[]) => void | Promise<void>
   /** Called by onClick to run the actual task. */
-  execute: (args?: unknown[]) => boolean | Promise<boolean>
+  readonly execute: (args?: unknown[]) => boolean | Promise<boolean>
   /** Show the button with `severity="danger"` */
-  severity?: VueSeverity
+  readonly severity?: VueSeverity
   /** Whether this task should display the terminal window when run. */
-  usesTerminal?: boolean
+  readonly usesTerminal?: boolean
   /** If `true`, successful completion of this task will refresh install validation and automatically continue if successful. */
-  isInstallationFix?: boolean
+  readonly isInstallationFix?: boolean
 }
 
 export interface MaintenanceFilter {
-  icon: string
-  value: string
+  readonly icon: string
+  readonly value: string
   // value: 'All' | 'Errors'
-  tasks: MaintenanceTask[]
+  readonly tasks: MaintenanceTask[]
 }
