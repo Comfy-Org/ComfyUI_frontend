87fc7a2c5d61294f32d9e976c766fd746d6b2762
Chenlei Hu <huchenlei@proton.me>
2025-01-20 11:15:16 -0500

    [Cleanup] Remove explicit prettier plugin call (#2299)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -11,7 +11,7 @@ export default {
 
 function formatAndEslint(fileNames) {
   return [
-    `prettier --write ${fileNames.join(' ')} --plugin @trivago/prettier-plugin-sort-imports`,
+    `prettier --write ${fileNames.join(' ')}`,
     `eslint --fix ${fileNames.join(' ')}`
   ]
 }
