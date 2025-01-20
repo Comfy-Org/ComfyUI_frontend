08c3baf91ba73ad121819ce4e3abe435a28abb6d
filtered <176114999+webfiltered@users.noreply.github.com>
2025-01-21 09:38:25 +1100

    [Refactor] customRef to VueUse

--- src/utils/refUtil.ts
+++ src/utils/refUtil.ts
@@ -1,38 +1,24 @@
-import { customRef } from 'vue'
-
-type TimerId = Parameters<typeof clearTimeout>[0]
+import { useTimeout } from '@vueuse/core'
+import { computed, ref } from 'vue'
 
 /**
- * This is a standard boolean vue ref with one difference: when set to `true` it stays that way for at least {@link duration}.
- * If set to `false` before {@link duration} has passed, it uses a timer to delay the change.
+ * Vue boolean ref (writable computed) with one difference: when set to `true` it stays that way for at least {@link minDuration}.
+ * If set to `false` before {@link minDuration} has passed, it uses a timer to delay the change.
  * @param value The default value to set on this ref
- * @param duration The minimum time that this ref must be `true` for
+ * @param minDuration The minimum time that this ref must be `true` for
  * @returns A custom boolean vue ref with a minimum activation time
  */
-export function minDurationRef(value: boolean, duration = 250) {
-  let enabledAt = Date.now()
+export function useMinLoadingDurationRef(value: boolean, minDuration = 250) {
+  const current = ref(value)
 
-  let timeout: TimerId
+  const { ready, start } = useTimeout(minDuration, { controls: true })
 
-  return customRef((track, trigger) => ({
-    get() {
-      track()
-      return value
-    },
-    set(newValue) {
-      clearTimeout(timeout)
+  return computed({
+    get: () => current.value || !ready.value,
+    set: (newValue) => {
+      if (newValue && !current.value) start()
 
-      if (newValue) {
-        enabledAt = Date.now()
-        value = newValue
-        trigger()
-      } else {
-        const delay = Math.max(0, duration - (Date.now() - enabledAt))
-        timeout = setTimeout(() => {
-          value = newValue
-          trigger()
-        }, delay)
-      }
+      current.value = newValue
     }
-  }))
+  })
 }
