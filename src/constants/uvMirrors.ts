0702a56d0355a44f39f11aae9f5bb323c225a7ae
huchenlei <huchenlei@proton.me>
2025-01-24 11:47:15 -0500

    Update electron-types

---
+++ src/constants/uvMirrors.ts
@@ -0,0 +1,11 @@
+export const DEFAULT_UV_PYPY_INSTALL_MIRROR =
+  'https://downloads.python.org/pypy'
+
+export const DEFAULT_UV_PYTHON_INSTALL_MIRROR =
+  'https://github.com/astral-sh/python-build-standalone/releases/download'
+
+export const FALLBACK_UV_PYPY_INSTALL_MIRROR =
+  'https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple'
+
+export const FALLBACK_UV_PYTHON_INSTALL_MIRROR =
+  'https://ghfast.top/https://github.com/astral-sh/python-build-standalone/releases/download'
