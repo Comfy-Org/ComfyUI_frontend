adff23bb84e8bd92162b63639315d944c688623d
Chenlei Hu <huchenlei@proton.me>
2025-02-10 11:17:09 -0500

    Add ISerialisedNode.widgets_values documentation (#513)

--- src/types/serialisation.ts
+++ src/types/serialisation.ts
@@ -58,6 +58,12 @@ export interface ISerialisedNode {
   color?: string
   bgcolor?: string
   showAdvanced?: boolean
+  /**
+   * Note: Some custom nodes overrides the `widgets_values` property to an
+   * object that has `length` property and index access. It is not safe to call
+   * any array methods on it.
+   * See example in https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite/blob/8629188458dc6cb832f871ece3bd273507e8a766/web/js/VHS.core.js#L59-L84
+   */
   widgets_values?: TWidgetValue[]
 }
 
