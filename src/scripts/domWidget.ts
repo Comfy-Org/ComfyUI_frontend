89812ce7d0fe2fd02186288455e472d42297eee9
Chenlei Hu <huchenlei@proton.me>
2025-02-12 19:41:06 -0500

    [Cleanup] Remove unused hooks on DOMWidget (#2540)

--- src/scripts/domWidget.ts
+++ src/scripts/domWidget.ts
@@ -67,8 +67,6 @@ export interface DOMWidgetOptions<
   getMaxHeight?: () => number
   getHeight?: () => string | number
   onDraw?: (widget: DOMWidget<T, V>) => void
-  beforeResize?: (this: DOMWidget<T, V>, node: LGraphNode) => void
-  afterResize?: (this: DOMWidget<T, V>, node: LGraphNode) => void
 }
 
 function intersect(a: Rect, b: Rect): Vector4 | null {
@@ -490,10 +488,8 @@ LGraphNode.prototype.addDOMWidget = function <
     this[SIZE] = true
     const onResize = this.onResize
     this.onResize = function (size: Size) {
-      options.beforeResize?.call(widget, this)
       computeSize.call(this, size)
       onResize?.call(this, size)
-      options.afterResize?.call(widget, this)
     }
   }
 
