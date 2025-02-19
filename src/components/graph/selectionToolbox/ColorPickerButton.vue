186b9c9abba911fb6408b4f2c425f88ce165d3c5
huchenlei <huchenlei@proton.me>
2025-02-19 13:13:50 -0500

    Directly use selectbutton

--- src/components/graph/selectionToolbox/ColorPickerButton.vue
+++ src/components/graph/selectionToolbox/ColorPickerButton.vue
@@ -16,11 +16,21 @@
       v-if="showColorPicker"
       class="color-picker-container absolute -top-10 left-1/2"
     >
-      <ColorCustomizationSelector
-        :color-options="colorOptions"
-        v-model="color"
-        :allow-custom="false"
-      />
+      <SelectButton
+        v-model="selectedColorOption"
+        :options="colorOptions"
+        optionLabel="name"
+        dataKey="value"
+      >
+        <template #option="{ option }">
+          <i
+            class="pi pi-circle-fill"
+            :style="{
+              color: isLightTheme ? option.value.light : option.value.dark
+            }"
+          />
+        </template>
+      </SelectButton>
     </div>
   </div>
 </template>
@@ -28,57 +38,54 @@
 <script setup lang="ts">
 import { LGraphCanvas, LiteGraph } from '@comfyorg/litegraph'
 import Button from 'primevue/button'
+import SelectButton from 'primevue/selectbutton'
 import { computed, ref, watch } from 'vue'
 
-import ColorCustomizationSelector from '@/components/common/ColorCustomizationSelector.vue'
 import { useCanvasStore } from '@/stores/graphStore'
 import { useColorPaletteStore } from '@/stores/workspace/colorPaletteStore'
 import { adjustColor } from '@/utils/colorUtil'
 import { setItemColor } from '@/utils/litegraphUtil'
 
+const canvasStore = useCanvasStore()
+const colorPaletteStore = useColorPaletteStore()
+const isLightTheme = computed(
+  () => colorPaletteStore.completedActivePalette.light_theme
+)
+
 const showColorPicker = ref(false)
 
-const NO_COLOR_OPTION = {
-  name: 'No Color',
-  value: LiteGraph.NODE_DEFAULT_BGCOLOR
+type ColorOption = {
+  name: string
+  value: {
+    dark: string
+    light: string
+  }
 }
-const darkThemeColorOptions = [
+
+const NO_COLOR_OPTION: ColorOption = {
+  name: 'No Color',
+  value: {
+    dark: LiteGraph.NODE_DEFAULT_BGCOLOR,
+    light: adjustColor(LiteGraph.NODE_DEFAULT_BGCOLOR, { lightness: 0.5 })
+  }
+}
+const colorOptions: ColorOption[] = [
   NO_COLOR_OPTION,
   ...Object.entries(LGraphCanvas.node_colors).map(([name, color]) => ({
     name,
-    value: color.bgcolor
+    value: {
+      dark: color.bgcolor,
+      light: adjustColor(color.bgcolor, { lightness: 0.5 })
+    }
   }))
 ]
-const lightThemeColorOptions = darkThemeColorOptions.map((option) => ({
-  ...option,
-  value: adjustColor(option.value, { lightness: 0.5 })
-}))
 
-const colorPaletteStore = useColorPaletteStore()
-const colorOptions = computed<{ name: string; value: string }[]>(() =>
-  colorPaletteStore.completedActivePalette.light_theme
-    ? lightThemeColorOptions
-    : darkThemeColorOptions
-)
-const color = ref(colorOptions.value[0].value)
-watch(colorOptions, (newOptions, oldOptions) => {
-  const oldOption = oldOptions.find((option) => option.value === color.value)
-  const newValue = newOptions.find((option) => option.name === oldOption?.name)
-  if (newValue) {
-    color.value = newValue.value
-  }
-})
-watch(color, (colorValue) => {
-  const option = colorOptions.value.find(
-    (option) => option.value === colorValue
-  )
-  if (option) {
-    applyColor(option.name)
-  }
+const selectedColorOption = ref<ColorOption | null>(null)
+watch(selectedColorOption, (colorOption) => {
+  applyColor(colorOption?.name ?? NO_COLOR_OPTION.name)
   showColorPicker.value = false
 })
 
-const canvasStore = useCanvasStore()
 const applyColor = (colorName: string) => {
   const colorOption =
     colorName === NO_COLOR_OPTION.name
