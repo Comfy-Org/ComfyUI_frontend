37d4cc974b03886ad6d9c7d5864ceb1b37373361
bymyself <cbyrne@comfy.org>
2025-02-24 08:45:27 -0700

    Cleanup node's file input elements on node removal (#2703)
    Co-authored-by: huchenlei <huchenlei@proton.me>

--- src/composables/useNodeFileInput.ts
+++ src/composables/useNodeFileInput.ts
@@ -1,3 +1,7 @@
+import type { LGraphNode } from '@comfyorg/litegraph'
+
+import { useChainCallback } from './functional/useChainCallback'
+
 interface FileInputOptions {
   accept?: string
   allow_batch?: boolean
@@ -8,7 +12,7 @@ interface FileInputOptions {
 /**
  * Creates a file input for a node.
  */
-export function useNodeFileInput(options: FileInputOptions) {
+export function useNodeFileInput(node: LGraphNode, options: FileInputOptions) {
   const {
     accept,
     allow_batch = false,
@@ -16,30 +20,26 @@ export function useNodeFileInput(options: FileInputOptions) {
     onSelect
   } = options
 
-  const fileInput = document.createElement('input')
+  let fileInput: HTMLInputElement | null = document.createElement('input')
   fileInput.type = 'file'
   fileInput.accept = accept ?? '*'
   fileInput.multiple = allow_batch
-  fileInput.style.visibility = 'hidden'
 
   fileInput.onchange = () => {
-    if (fileInput.files?.length) {
+    if (fileInput?.files?.length) {
       const files = Array.from(fileInput.files).filter(fileFilter)
       if (files.length) onSelect(files)
     }
   }
 
-  document.body.append(fileInput)
-
-  /**
-   * Shows the system file picker dialog for selecting files.
-   */
-  function openFileSelection() {
-    fileInput.click()
-  }
+  node.onRemoved = useChainCallback(node.onRemoved, () => {
+    if (fileInput) {
+      fileInput.onchange = null
+      fileInput = null
+    }
+  })
 
   return {
-    fileInput,
-    openFileSelection
+    openFileSelection: () => fileInput?.click()
   }
 }
