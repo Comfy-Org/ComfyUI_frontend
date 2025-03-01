e80190fdcadfdf8979a28caf7152c3facae8a6f4
huchenlei <huchenlei@proton.me>
2025-03-01 16:40:55 -0500

    nit

--- tests-ui/tests/apiTypes.test.ts
+++ tests-ui/tests/apiTypes.test.ts
@@ -31,8 +31,6 @@ describe('validateNodeDef', () => {
   })
 
   describe.each([
-    [{ ckpt_name: 'foo' }, ['foo', {}]],
-    [{ ckpt_name: ['foo'] }, ['foo', {}]],
     [{ ckpt_name: ['foo', { default: 1 }] }, ['foo', { default: 1 }]],
     // Extra input spec should be preserved
     [{ ckpt_name: ['foo', { bar: 1 }] }, ['foo', { bar: 1 }]],
