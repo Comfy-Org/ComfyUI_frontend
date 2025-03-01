4cd1af883fc3bfde28e5ef5a0251d6a8c085876b
filtered <176114999+webfiltered@users.noreply.github.com>
2025-03-02 05:58:02 +1100

    [TS] Update type to reflect actual usage
    - Required to use https://github.com/Comfy-Org/litegraph.js/issues/664

--- src/types/litegraphTypes.ts
+++ src/types/litegraphTypes.ts
@@ -14,8 +14,8 @@ export class ConnectingLinkImpl implements ConnectingLink {
   constructor(
     public node: LGraphNode,
     public slot: number,
-    public input: INodeInputSlot | undefined,
-    public output: INodeOutputSlot | undefined,
+    public input: INodeInputSlot | null | undefined,
+    public output: INodeOutputSlot | null | undefined,
     public pos: Vector2,
     public afterRerouteId?: RerouteId
   ) {}
