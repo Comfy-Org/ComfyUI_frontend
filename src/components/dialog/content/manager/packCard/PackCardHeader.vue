a1d781b79b47a472a4428697ae5025a60519268a
bymyself <cbyrne@comfy.org>
2025-03-08 14:14:37 -0700

    break node pack name

--- src/components/dialog/content/manager/packCard/PackCardHeader.vue
+++ src/components/dialog/content/manager/packCard/PackCardHeader.vue
@@ -3,7 +3,10 @@
     <slot name="thumbnail">
       <PackIcon :node-pack="nodePack" width="24" height="24" />
     </slot>
-    <h2 class="text-2xl font-bold text-center mt-4 mb-2">
+    <h2
+      class="text-2xl font-bold text-center mt-4 mb-2"
+      style="word-break: break-all"
+    >
       <slot name="title">{{ nodePack?.name }}</slot>
     </h2>
     <div class="mt-2 mb-4 w-full max-w-xs flex justify-center">
