d3df6dcd4189fea7e88383a22c03db9b5bc3b926
bymyself <cbyrne@comfy.org>
2025-03-10 02:23:00 -0700

    add default value for metadata labels

--- src/components/dialog/content/manager/infoPanel/MetadataRow.vue
+++ src/components/dialog/content/manager/infoPanel/MetadataRow.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="flex py-1.5 text-xs">
-    <div class="w-1/3 text-color-secondary truncate pr-2 text-muted">
-      {{ label }}:
-    </div>
+    <div class="w-1/3 truncate pr-2 text-muted">{{ label }}:</div>
     <div class="w-2/3">
       <slot>{{ value }}</slot>
     </div>
@@ -10,7 +8,7 @@
 </template>
 
 <script setup lang="ts">
-const { value = 'N/A' } = defineProps<{
+const { value = 'N/A', label = 'N/A' } = defineProps<{
   label: string
   value?: string | number
 }>()
