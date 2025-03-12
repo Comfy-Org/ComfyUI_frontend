eaeeae4ded64a32483ea872db13de26f4502b8f9
bymyself <cbyrne@comfy.org>
2025-03-11 23:56:34 -0700

    whitelsit RealESRGAN_x4Plus

--- src/components/dialog/content/MissingModelsWarning.vue
+++ src/components/dialog/content/MissingModelsWarning.vue
@@ -52,7 +52,8 @@ const allowedSuffixes = ['.safetensors', '.sft']
 // Models that fail above conditions but are still allowed
 const whiteListedUrls = new Set([
   'https://huggingface.co/stabilityai/stable-zero123/resolve/main/stable_zero123.ckpt',
-  'https://huggingface.co/TencentARC/T2I-Adapter/resolve/main/models/t2iadapter_depth_sd14v1.pth?download=true'
+  'https://huggingface.co/TencentARC/T2I-Adapter/resolve/main/models/t2iadapter_depth_sd14v1.pth?download=true',
+  'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth'
 ])
 
 interface ModelInfo {
