b223e8e90b2762e977a989632937de31fc17fad0
bymyself <cbyrne@comfy.org>
2025-03-12 22:16:03 -0700

    bind button wrapper props to button

--- src/components/dialog/content/manager/button/PackActionButton.vue
+++ src/components/dialog/content/manager/button/PackActionButton.vue
@@ -2,17 +2,17 @@
   <Button
     outlined
     class="m-0 p-0 rounded-lg border-neutral-700"
-    :severity="severity"
     :class="{
       'w-full': fullWidth,
       'w-min-content': !fullWidth
     }"
     :disabled="isExecuted"
+    v-bind="$attrs"
     @click="handleClick"
   >
     <span class="py-2.5 px-3">
       <template v-if="isExecuted">
-        {{ managerStore.statusMessage }}
+        {{ loadingMessage ?? $t('g.loading') }}
       </template>
       <template v-else>
         {{ label }}
@@ -25,24 +25,24 @@
 import Button from 'primevue/button'
 import { ref } from 'vue'
 
-import { useComfyManagerStore } from '@/stores/comfyManagerStore'
-import { VueSeverity } from '@/types/primeVueTypes'
-
 const {
-  fullWidth = false,
   label,
-  severity = 'secondary' as VueSeverity
+  loadingMessage,
+  fullWidth = false
 } = defineProps<{
-  fullWidth?: boolean
   label: string
-  severity?: VueSeverity
+  loadingMessage?: string
+  fullWidth?: boolean
 }>()
 
 const emit = defineEmits<{
   action: []
 }>()
 
-const managerStore = useComfyManagerStore()
+defineOptions({
+  inheritAttrs: false
+})
+
 const isExecuted = ref(false)
 
 const handleClick = (): void => {
