09ef287898cc256f2c99c9a053a0797e0ea241e3
filtered <176114999+webfiltered@users.noreply.github.com>
2025-03-19 01:24:40 +1100

    Support existing workflows by using optional props

--- src/schemas/comfyWorkflowSchema.ts
+++ src/schemas/comfyWorkflowSchema.ts
@@ -239,8 +239,8 @@ const zExtra = z
 /** Schema version 0.4 */
 export const zComfyWorkflow = z
   .object({
-    id: z.string().uuid(),
-    revision: z.number(),
+    id: z.string().uuid().optional(),
+    revision: z.number().optional(),
     last_node_id: zNodeId,
     last_link_id: z.number(),
     nodes: z.array(zComfyNode),
@@ -257,8 +257,8 @@ export const zComfyWorkflow = z
 /** Schema version 1 */
 export const zComfyWorkflow1 = z
   .object({
-    id: z.string().uuid(),
-    revision: z.number(),
+    id: z.string().uuid().optional(),
+    revision: z.number().optional(),
     version: z.literal(1),
     config: zConfig.optional().nullable(),
     state: zGraphState,
