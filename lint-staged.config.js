3f2b4507fb4db3ed457035cbe7882921d9c281ab
Chenlei Hu <hcl@comfy.org>
2025-03-18 22:37:30 -0400

    Remove typescript-strict-plugin

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,8 +4,7 @@ export default {
   './**/*.{ts,tsx,vue,mts}': (stagedFiles) => [
     ...formatAndEslint(stagedFiles),
     'vue-tsc --noEmit',
-    'tsc --noEmit',
-    'tsc-strict'
+    'tsc --noEmit'
   ]
 }
 
