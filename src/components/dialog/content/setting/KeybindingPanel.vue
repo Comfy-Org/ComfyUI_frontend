e06a1495361d34c40adaacd237e2b4ef36d800c0
Benjamin Lu <templu1107@proton.me>
2025-04-12 20:09:45 -0400

    Add source column

--- src/components/dialog/content/setting/KeybindingPanel.vue
+++ src/components/dialog/content/setting/KeybindingPanel.vue
@@ -71,6 +71,11 @@
           <span v-else>-</span>
         </template>
       </Column>
+      <Column field="source" :header="$t('g.source')">
+        <template #body="slotProps">
+          <span>{{ slotProps.data.source || '-' }}</span>
+        </template>
+      </Column>
     </DataTable>
 
     <Dialog
@@ -160,6 +165,7 @@ interface ICommandData {
   id: string
   keybinding: KeybindingImpl | null
   label: string
+  source?: string
 }
 
 const commandsData = computed<ICommandData[]>(() => {
@@ -169,7 +175,8 @@ const commandsData = computed<ICommandData[]>(() => {
       `commands.${normalizeI18nKey(command.id)}.label`,
       command.label ?? ''
     ),
-    keybinding: keybindingStore.getKeybindingByCommandId(command.id)
+    keybinding: keybindingStore.getKeybindingByCommandId(command.id),
+    source: command.source
   }))
 })
 
