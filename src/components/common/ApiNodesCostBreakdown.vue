b2cf3b0abebfde462eba1bbd946c48bb4c02152d
Chenlei Hu <hcl@comfy.org>
2025-04-14 17:34:07 -0400

    better match design

--- src/components/common/ApiNodesCostBreakdown.vue
+++ src/components/common/ApiNodesCostBreakdown.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-col gap-2 h-full">
-    <div class="flex justify-between text-sm text-muted mb-2">
+  <div class="flex flex-col gap-3 h-full">
+    <div class="flex justify-between text-xs">
       <div>{{ t('apiNodesCostBreakdown.title') }}</div>
       <div>{{ t('apiNodesCostBreakdown.costPerRun') }}</div>
     </div>
@@ -9,26 +9,30 @@
         <div
           v-for="node in nodes"
           :key="node.name"
-          class="flex items-center justify-between p-3 rounded bg-[var(--p-content-border-color)]"
+          class="flex items-center justify-between px-3 py-2 rounded-md bg-[var(--p-content-border-color)]"
         >
-          <div class="flex items-center gap-3">
-            <span>{{ node.name }}</span>
+          <div class="flex items-center gap-2">
+            <span class="text-base font-medium leading-tight">{{
+              node.name
+            }}</span>
           </div>
           <div class="flex items-center gap-1">
             <Tag
               severity="secondary"
               icon="pi pi-dollar"
               rounded
-              class="text-yellow-500 p-1"
+              class="text-amber-400 p-1"
             />
-            <span>{{ node.cost.toFixed(costPrecision) }}</span>
+            <span class="text-base font-medium leading-tight">
+              {{ node.cost.toFixed(costPrecision) }}
+            </span>
           </div>
         </div>
       </div>
     </ScrollPanel>
     <template v-if="showTotal && nodes.length > 1">
-      <Divider />
-      <div class="flex justify-between items-center pt-2 border-t px-3">
+      <Divider class="my-2" />
+      <div class="flex justify-between items-center border-t px-3">
         <span class="text-sm">{{ t('apiNodesCostBreakdown.totalCost') }}</span>
         <div class="flex items-center gap-1">
           <Tag
