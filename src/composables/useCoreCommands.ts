3862d78fb3d45a2b1f080bde3caacd40a2e33e5f
bymyself <cbyrne@comfy.org>
2025-04-14 16:29:03 -0700

    improve command names

--- src/composables/useCoreCommands.ts
+++ src/composables/useCoreCommands.ts
@@ -676,6 +676,7 @@ export function useCoreCommands(): ComfyCommand[] {
               'Comfy.Manager.Menu.ToggleVisibility' // This command is registered by legacy manager FE extension
             )
           } catch (error) {
+            console.error('error', error)
             useToastStore().add({
               severity: 'error',
               summary: t('g.error'),
@@ -692,7 +693,7 @@ export function useCoreCommands(): ComfyCommand[] {
     {
       id: 'Comfy.Manager.ShowUpdateAvailablePacks',
       icon: 'pi pi-sync',
-      label: 'Check for Updates',
+      label: 'Check for Custom Node Updates',
       versionAdded: '1.17.0',
       function: () => {
         dialogService.showManagerDialog({
@@ -703,7 +704,7 @@ export function useCoreCommands(): ComfyCommand[] {
     {
       id: 'Comfy.Manager.ShowMissingPacks',
       icon: 'pi pi-exclamation-circle',
-      label: 'Install Missing',
+      label: 'Install Missing Custom Nodes',
       versionAdded: '1.17.0',
       function: () => {
         dialogService.showManagerDialog({
