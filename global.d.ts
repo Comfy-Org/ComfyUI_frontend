1ed7c7caa97a9707cb41422d056e1abec1e762e1
bymyself <cbyrne@comfy.org>
2025-04-17 19:27:30 -0700

    use env vars for firebase config

--- global.d.ts
+++ global.d.ts
@@ -3,6 +3,14 @@ declare const __SENTRY_ENABLED__: boolean
 declare const __SENTRY_DSN__: string
 declare const __ALGOLIA_APP_ID__: string
 declare const __ALGOLIA_API_KEY__: string
+declare const __FIREBASE_API_KEY__: string
+declare const __FIREBASE_AUTH_DOMAIN__: string
+declare const __FIREBASE_DATABASE_URL__: string
+declare const __FIREBASE_PROJECT_ID__: string
+declare const __FIREBASE_STORAGE_BUCKET__: string
+declare const __FIREBASE_MESSAGING_SENDER_ID__: string
+declare const __FIREBASE_APP_ID__: string
+declare const __FIREBASE_MEASUREMENT_ID__: string
 
 interface Navigator {
   /**
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,12 +1,27 @@
 import { FirebaseOptions } from 'firebase/app'
 
-export const FIREBASE_CONFIG: FirebaseOptions = {
-  apiKey: 'AIzaSyC2-fomLqgCjb7ELwta1I9cEarPK8ziTGs',
+const DEV_CONFIG: FirebaseOptions = {
+  apiKey: 'AIzaSyAIOcnOl01Roycc4kCiVoxyOZzlvM27O1o',
   authDomain: 'dreamboothy.firebaseapp.com',
   databaseURL: 'https://dreamboothy-default-rtdb.firebaseio.com',
   projectId: 'dreamboothy',
   storageBucket: 'dreamboothy.appspot.com',
   messagingSenderId: '357148958219',
-  appId: '1:357148958219:web:f5917f72e5f36a2015310e',
-  measurementId: 'G-3ZBD3MBTG4'
+  appId: '1:357148958219:web:062460a94961edd31531de',
+  measurementId: 'G-3RDG85K1MH'
 }
+
+const PROD_CONFIG: FirebaseOptions = {
+  apiKey: __FIREBASE_API_KEY__,
+  authDomain: __FIREBASE_AUTH_DOMAIN__,
+  databaseURL: __FIREBASE_DATABASE_URL__,
+  projectId: __FIREBASE_PROJECT_ID__,
+  storageBucket: __FIREBASE_STORAGE_BUCKET__,
+  messagingSenderId: __FIREBASE_MESSAGING_SENDER_ID__,
+  appId: __FIREBASE_APP_ID__,
+  measurementId: __FIREBASE_MEASUREMENT_ID__
+}
+
+export const FIREBASE_CONFIG: FirebaseOptions = import.meta.env.PROD
+  ? PROD_CONFIG
+  : DEV_CONFIG
