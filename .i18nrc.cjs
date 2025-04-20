b74887d5434aaea3ca8b07b2161aad7c9805a761
Chenlei Hu <huchenlei@proton.me>
2025-04-20 13:13:00 -0400

    [i18n] Use gpt-o4-mini for translation (#3529)

--- .i18nrc.cjs
+++ .i18nrc.cjs
@@ -4,7 +4,7 @@
 const { defineConfig } = require('@lobehub/i18n-cli');
 
 module.exports = defineConfig({
-  modelName: 'gpt-4',
+  modelName: 'gpt-o4-mini',
   splitToken: 1024,
   entry: 'src/locales/en',
   entryLocale: 'en',
