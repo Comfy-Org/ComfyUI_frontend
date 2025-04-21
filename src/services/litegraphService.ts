6e5930c3552384fba6d5e3f6f00ca64d2bafd428
filtered <176114999+webfiltered@users.noreply.github.com>
2025-04-22 05:24:48 +1000

    [API] Add sockets to custom widgets by default (#3548)

--- src/services/litegraphService.ts
+++ src/services/litegraphService.ts
@@ -41,7 +41,6 @@ import {
 
 import { useExtensionService } from './extensionService'
 
-const PRIMITIVE_TYPES = new Set(['INT', 'FLOAT', 'BOOLEAN', 'STRING', 'COMBO'])
 export const CONFIG = Symbol()
 export const GET_CONFIG = Symbol()
 
@@ -124,7 +123,8 @@ export const useLitegraphService = () => {
       }
 
       /**
-       * @internal Add a widget to the node. For primitive types, an input socket is also added.
+       * @internal Add a widget to the node. For both primitive types and custom widgets
+       * (unless `socketless`), an input socket is also added.
        */
       #addInputWidget(inputSpec: InputSpec) {
         const inputName = inputSpec.name
@@ -152,7 +152,7 @@ export const useLitegraphService = () => {
           })
         }
 
-        if (PRIMITIVE_TYPES.has(inputSpec.type)) {
+        if (!widget?.options?.socketless) {
           const inputSpecV1 = transformInputSpecV2ToV1(inputSpec)
           this.addInput(inputName, inputSpec.type, {
             shape: inputSpec.isOptional ? RenderShape.HollowCircle : undefined,
