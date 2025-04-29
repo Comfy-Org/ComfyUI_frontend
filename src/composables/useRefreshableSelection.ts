98d7256da47a63e91f692c843155b7fcb3626afe
Chenlei Hu <huchenlei@proton.me>
2025-04-29 19:59:49 -0400

    Remove refresh button when all nodes are selected (#3690)

--- src/composables/useRefreshableSelection.ts
+++ src/composables/useRefreshableSelection.ts
@@ -1,8 +1,6 @@
-import type { LGraphNode } from '@comfyorg/litegraph'
-import type { IWidget } from '@comfyorg/litegraph'
+import type { IWidget, LGraphNode } from '@comfyorg/litegraph'
 import { computed, ref, watchEffect } from 'vue'
 
-import { useCommandStore } from '@/stores/commandStore'
 import { useCanvasStore } from '@/stores/graphStore'
 import { isLGraphNode } from '@/utils/litegraphUtil'
 
@@ -20,14 +18,10 @@ const isRefreshableWidget = (widget: IWidget): widget is RefreshableWidget =>
  */
 export const useRefreshableSelection = () => {
   const graphStore = useCanvasStore()
-  const commandStore = useCommandStore()
   const selectedNodes = ref<LGraphNode[]>([])
-  const isAllNodesSelected = ref(false)
 
   watchEffect(() => {
     selectedNodes.value = graphStore.selectedItems.filter(isLGraphNode)
-    isAllNodesSelected.value =
-      graphStore.canvas?.graph?.nodes?.every((node) => !!node.selected) ?? false
   })
 
   const refreshableWidgets = computed(() =>
@@ -36,18 +30,12 @@ export const useRefreshableSelection = () => {
     )
   )
 
-  const isRefreshable = computed(
-    () => refreshableWidgets.value.length > 0 || isAllNodesSelected.value
-  )
+  const isRefreshable = computed(() => refreshableWidgets.value.length > 0)
 
   async function refreshSelected() {
     if (!isRefreshable.value) return
 
-    if (isAllNodesSelected.value) {
-      await commandStore.execute('Comfy.RefreshNodeDefinitions')
-    } else {
-      await Promise.all(refreshableWidgets.value.map((item) => item.refresh()))
-    }
+    await Promise.all(refreshableWidgets.value.map((item) => item.refresh()))
   }
 
   return {
