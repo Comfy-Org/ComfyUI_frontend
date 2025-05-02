8766a8e4d356636c3dcc4f15d77f830dcd08205e
filtered <176114999+webfiltered@users.noreply.github.com>
2025-05-03 06:37:26 +1000

    [Refactor] Simplify code

--- src/stores/subgraphStore.ts
+++ src/stores/subgraphStore.ts
@@ -1,7 +1,6 @@
-import { Subgraph } from '@comfyorg/litegraph'
 import { whenever } from '@vueuse/core'
 import { defineStore } from 'pinia'
-import { ref, shallowRef } from 'vue'
+import { ref } from 'vue'
 
 import { app as comfyApp } from '@/scripts/app'
 import { useWorkflowStore } from '@/stores/workflowStore'
@@ -10,19 +9,15 @@ import { isSubgraph } from '@/utils/typeGuardUtil'
 export const useSubgraphStore = defineStore('subgraph', () => {
   const workflowStore = useWorkflowStore()
 
-  const activeGraph = shallowRef<Subgraph>()
   const graphNamePath = ref<string[]>([])
   const isSubgraphActive = ref(false)
 
   const updateActiveGraph = () => {
-    activeGraph.value = comfyApp.canvas.subgraph
-    isSubgraphActive.value = isSubgraph(activeGraph.value)
-    updateGraphPaths()
-  }
+    const { subgraph } = comfyApp.canvas
+    isSubgraphActive.value = isSubgraph(subgraph)
 
-  const updateGraphPaths = () => {
-    if (activeGraph.value) {
-      const [, ...pathFromRoot] = activeGraph.value.pathToRootGraph
+    if (subgraph) {
+      const [, ...pathFromRoot] = subgraph.pathToRootGraph
 
       graphNamePath.value = pathFromRoot.map((graph) => graph.name)
     } else {
