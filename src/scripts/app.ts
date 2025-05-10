e4966f0fc94212f7e74b05bce517ca1995438b13
filtered <176114999+webfiltered@users.noreply.github.com>
2025-05-10 17:07:12 +1000

    Revert "[TMP] Add no items selected listener"
    This reverts commit 6b2c9136c36a372a57b5386bd627bc061d0c43d8.

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -764,14 +764,6 @@ export class ComfyApp {
     this.canvas.state = reactive(this.canvas.state)
     this.canvas.ds.state = reactive(this.canvas.ds.state)
 
-    canvasEl.addEventListener('no-items-selected', () => {
-      useToastStore().add({
-        severity: 'warn',
-        summary: 'No items selected',
-        life: 2000
-      })
-    })
-
     // @ts-expect-error fixme ts strict error
     this.ctx = canvasEl.getContext('2d')
 
