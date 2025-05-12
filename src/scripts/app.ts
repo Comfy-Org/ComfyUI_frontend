a27cd2d672e2bdac9c222ff0432ce06ae7e9314f
filtered <176114999+webfiltered@users.noreply.github.com>
2025-05-12 18:07:02 +1000

    Prune DOM widgets outside of current subgraph

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -717,7 +717,7 @@ export class ComfyApp {
   }
 
   #addAfterConfigureHandler() {
-    const { canvas, graph } = this
+    const { graph } = this
     const { onConfigure } = graph
     graph.onConfigure = function (...args) {
       fixLinkInputSlots(this)
@@ -734,8 +734,10 @@ export class ComfyApp {
         node.onAfterGraphConfigured?.()
       }
 
-      const nodes = canvas.subgraph?.nodes ?? graph.nodes
-      pruneWidgets(nodes)
+      graph.canvasAction((c) => {
+        const nodes = c.subgraph?.nodes ?? graph.nodes
+        pruneWidgets(nodes)
+      })
 
       return r
     }
