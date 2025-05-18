0610b33057638fcb13a7e22705581cf9993341d9
benceruleanlu <benceruleanlu@proton.me>
2025-05-18 19:40:05 -0400

    Refactor HelpButton

--- src/components/graph/selectionToolbox/HelpButton.vue
+++ src/components/graph/selectionToolbox/HelpButton.vue
@@ -1,6 +1,6 @@
 <template>
   <Button
-    v-show="hasNodeDef"
+    v-show="nodeDef"
     v-tooltip.top="{
       value: $t('g.help'),
       showDelay: 1000
@@ -20,35 +20,30 @@ import { computed } from 'vue'
 import { useNodeLibrarySidebarTab } from '@/composables/sidebarTabs/useNodeLibrarySidebarTab'
 import { useNodeHelp } from '@/composables/useNodeHelp'
 import { useCanvasStore } from '@/stores/graphStore'
+import type { ComfyNodeDefImpl } from '@/stores/nodeDefStore'
 import { useNodeDefStore } from '@/stores/nodeDefStore'
 import { useSidebarTabStore } from '@/stores/workspace/sidebarTabStore'
 import { isLGraphNode } from '@/utils/litegraphUtil'
 
 const canvasStore = useCanvasStore()
 const nodeDefStore = useNodeDefStore()
-const { openHelp } = useNodeHelp()
 const sidebarTabStore = useSidebarTabStore()
+const { openHelp } = useNodeHelp()
 const { id: nodeLibraryTabId } = useNodeLibrarySidebarTab()
 
-const selectedNode = computed(() => {
+const nodeDef = computed<ComfyNodeDefImpl | null>(() => {
   if (canvasStore.selectedItems.length !== 1) return null
   const item = canvasStore.selectedItems[0]
-  return isLGraphNode(item) ? item : null
+  if (!isLGraphNode(item)) return null
+  return nodeDefStore.fromLGraphNode(item)
 })
-
-const nodeDef = computed(() => {
-  const node = selectedNode.value
-  if (!node) return null
-  return nodeDefStore.fromLGraphNode(node)
-})
-
-const hasNodeDef = computed(() => nodeDef.value !== null)
 
 const showHelp = () => {
-  if (!hasNodeDef.value) return
+  const def = nodeDef.value
+  if (!def) return
   if (sidebarTabStore.activeSidebarTabId !== nodeLibraryTabId) {
     sidebarTabStore.toggleSidebarTab(nodeLibraryTabId)
   }
-  openHelp(nodeDef.value!)
+  openHelp(def)
 }
 </script>
