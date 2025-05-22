b0a461ad41496ec21ad0d6c432dcd996b74396e6
filtered <176114999+webfiltered@users.noreply.github.com>
2025-05-22 18:19:15 +1000

    Add convert to subgraph toolbox button

---
+++ src/components/graph/selectionToolbox/ConvertToSubgraphButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <Button
+    v-tooltip.top="{
+      value: t('commands.Comfy_Graph_ConvertToSubgraph.label'),
+      showDelay: 1000
+    }"
+    severity="secondary"
+    text
+    icon="pi pi-box"
+    @click="() => commandStore.execute('Comfy.Graph.ConvertToSubgraph')"
+  />
+</template>
+
+<script setup lang="ts">
+import Button from 'primevue/button'
+import { useI18n } from 'vue-i18n'
+
+import { useCommandStore } from '@/stores/commandStore'
+
+const { t } = useI18n()
+const commandStore = useCommandStore()
+</script>
