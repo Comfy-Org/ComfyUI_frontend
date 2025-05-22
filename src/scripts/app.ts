781565f8fdf1ef8d1bc0fd0505c84bee24a3df4c
filtered <176114999+webfiltered@users.noreply.github.com>
2025-05-22 11:25:11 -0700

    nit

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1293,7 +1293,7 @@ export class ComfyApp {
           executeWidgetsCallback(
             p.workflow.nodes
               .map((n) => this.graph.getNodeById(n.id))
-              .filter((n) => !!n) as LGraphNode[],
+              .filter((n) => !!n),
             'afterQueued'
           )
           this.canvas.draw(true, true)
