3865e5493afce8a7ef960143d69100c183a16b62
filtered <176114999+webfiltered@users.noreply.github.com>
2025-05-27 14:42:37 +1000

    1.20.6 (#3986)

--- src/components/dialog/content/signin/SignInForm.vue
+++ src/components/dialog/content/signin/SignInForm.vue
@@ -7,15 +7,12 @@
   >
     <!-- Email Field -->
     <div class="flex flex-col gap-2">
-      <label
-        class="opacity-80 text-base font-medium mb-2"
-        for="comfy-org-sign-in-email"
-      >
+      <label class="opacity-80 text-base font-medium mb-2" :for="emailInputId">
         {{ t('auth.login.emailLabel') }}
       </label>
       <InputText
-        pt:root:id="comfy-org-sign-in-email"
-        pt:root:autocomplete="email"
+        :id="emailInputId"
+        autocomplete="email"
         class="h-10"
         name="email"
         type="text"
@@ -37,8 +34,11 @@
           {{ t('auth.login.passwordLabel') }}
         </label>
         <span
-          class="text-muted text-base font-medium cursor-pointer"
-          @click="handleForgotPassword($form.email?.value)"
+          class="text-muted text-base font-medium cursor-pointer select-none"
+          :class="{
+            'text-link-disabled': !$form.email?.value || $form.email?.invalid
+          }"
+          @click="handleForgotPassword($form.email?.value, $form.email?.valid)"
         >
           {{ t('auth.login.forgotPassword') }}
         </span>
@@ -77,6 +77,7 @@ import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
 import Password from 'primevue/password'
 import ProgressSpinner from 'primevue/progressspinner'
+import { useToast } from 'primevue/usetoast'
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
@@ -87,6 +88,7 @@ import { useFirebaseAuthStore } from '@/stores/firebaseAuthStore'
 const authStore = useFirebaseAuthStore()
 const firebaseAuthActions = useFirebaseAuthActions()
 const loading = computed(() => authStore.loading)
+const toast = useToast()
 
 const { t } = useI18n()
 
@@ -94,14 +96,34 @@ const emit = defineEmits<{
   submit: [values: SignInData]
 }>()
 
+const emailInputId = 'comfy-org-sign-in-email'
+
 const onSubmit = (event: FormSubmitEvent) => {
   if (event.valid) {
     emit('submit', event.values as SignInData)
   }
 }
 
-const handleForgotPassword = async (email: string) => {
-  if (!email) return
+const handleForgotPassword = async (
+  email: string,
+  isValid: boolean | undefined
+) => {
+  if (!email || !isValid) {
+    toast.add({
+      severity: 'warn',
+      summary: t('auth.login.emailPlaceholder'),
+      life: 5_000
+    })
+    // Focus the email input
+    document.getElementById(emailInputId)?.focus?.()
+    return
+  }
   await firebaseAuthActions.sendPasswordReset(email)
 }
 </script>
+
+<style scoped>
+.text-link-disabled {
+  @apply opacity-50 cursor-not-allowed;
+}
+</style>
