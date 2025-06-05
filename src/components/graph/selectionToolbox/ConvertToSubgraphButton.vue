ceaf47c40e9a9e43b7088719fa9bc64c0ce0ad45
Blake <176114999+webfiltered@users.noreply.github.com>
2025-06-05 03:21:15 -0700

    Fix convert to subgraph shown on IO node

--- src/components/graph/selectionToolbox/ConvertToSubgraphButton.vue
+++ src/components/graph/selectionToolbox/ConvertToSubgraphButton.vue
@@ -1,5 +1,6 @@
 <template>
   <Button
+    v-show="isVisible"
     v-tooltip.top="{
       value: t('commands.Comfy_Graph_ConvertToSubgraph.label'),
       showDelay: 1000
@@ -13,10 +14,21 @@
 
 <script setup lang="ts">
 import Button from 'primevue/button'
+import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 import { useCommandStore } from '@/stores/commandStore'
+import { useCanvasStore } from '@/stores/graphStore'
 
 const { t } = useI18n()
 const commandStore = useCommandStore()
+const canvasStore = useCanvasStore()
+
+const isVisible = computed(() => {
+  return (
+    canvasStore.groupSelected ||
+    canvasStore.rerouteSelected ||
+    canvasStore.nodeSelected
+  )
+})
 </script>
