b675a9dacb33a62c71cd6a0e559c1018d80043e9
filtered <176114999+webfiltered@users.noreply.github.com>
2025-06-18 00:47:20 -0700

    [Test] Fix vitest scope overlaps playwright tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,10 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
     setupFiles: ['./vitest.setup.ts'],
-    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: [
+      'tests-ui/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+      'src/components/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'
+    ],
     coverage: {
       reporter: ['text', 'json', 'html']
     }
