346cac0889d48fbc33de97f966453e91c67d3764
bymyself <cbyrne@comfy.org>
2025-06-24 12:33:11 -0700

    [feat] Add Vue selection widgets
    - WidgetSelect: Dropdown selection with Select component
    - WidgetMultiSelect: Multiple selection with MultiSelect component
    - WidgetSelectButton: Button group selection with SelectButton component
    - WidgetTreeSelect: Hierarchical selection with TreeSelect component

---
+++ src/components/graph/vueWidgets/WidgetMultiSelect.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-col gap-1">
+    <label v-if="widget.name" class="text-sm opacity-80">{{
+      widget.name
+    }}</label>
+    <MultiSelect v-model="value" v-bind="filteredProps" :disabled="readonly" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MultiSelect from 'primevue/multiselect'
+import { computed } from 'vue'
+
+import type { SimplifiedWidget } from '@/types/simplifiedWidget'
+import {
+  PANEL_EXCLUDED_PROPS,
+  filterWidgetProps
+} from '@/utils/widgetPropFilter'
+
+const value = defineModel<any[]>({ required: true })
+
+const props = defineProps<{
+  widget: SimplifiedWidget<any[]>
+  readonly?: boolean
+}>()
+
+// MultiSelect specific excluded props include overlay styles
+const MULTISELECT_EXCLUDED_PROPS = [
+  ...PANEL_EXCLUDED_PROPS,
+  'overlayStyle'
+] as const
+
+const filteredProps = computed(() =>
+  filterWidgetProps(props.widget.options, MULTISELECT_EXCLUDED_PROPS)
+)
+</script>
