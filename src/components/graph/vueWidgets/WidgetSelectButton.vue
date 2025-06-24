199e256824c6972110a31c83767f68dc2e739f22
bymyself <cbyrne@comfy.org>
2025-06-24 12:33:11 -0700

    [feat] Add Vue selection widgets
    - WidgetSelect: Dropdown selection with Select component
    - WidgetMultiSelect: Multiple selection with MultiSelect component
    - WidgetSelectButton: Button group selection with SelectButton component
    - WidgetTreeSelect: Hierarchical selection with TreeSelect component

---
+++ src/components/graph/vueWidgets/WidgetSelectButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col gap-1">
+    <label v-if="widget.name" class="text-sm opacity-80">{{
+      widget.name
+    }}</label>
+    <SelectButton v-model="value" v-bind="filteredProps" :disabled="readonly" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import SelectButton from 'primevue/selectbutton'
+import { computed } from 'vue'
+
+import type { SimplifiedWidget } from '@/types/simplifiedWidget'
+import {
+  STANDARD_EXCLUDED_PROPS,
+  filterWidgetProps
+} from '@/utils/widgetPropFilter'
+
+const value = defineModel<any>({ required: true })
+
+const props = defineProps<{
+  widget: SimplifiedWidget<any>
+  readonly?: boolean
+}>()
+
+const filteredProps = computed(() =>
+  filterWidgetProps(props.widget.options, STANDARD_EXCLUDED_PROPS)
+)
+</script>
