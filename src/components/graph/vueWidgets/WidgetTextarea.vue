2f3b0d8db4c96706347cfc0463fb29ecd485fb9e
bymyself <cbyrne@comfy.org>
2025-06-24 12:32:54 -0700

    [feat] Add Vue input widgets
    - WidgetInputText: Single-line text input with InputText component
    - WidgetTextarea: Multi-line text input with Textarea component
    - WidgetSlider: Numeric range input with Slider component
    - WidgetToggleSwitch: Boolean toggle with ToggleSwitch component

---
+++ src/components/graph/vueWidgets/WidgetTextarea.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col gap-1">
+    <label v-if="widget.name" class="text-sm opacity-80">{{
+      widget.name
+    }}</label>
+    <Textarea v-model="value" v-bind="filteredProps" :disabled="readonly" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Textarea from 'primevue/textarea'
+import { computed } from 'vue'
+
+import type { SimplifiedWidget } from '@/types/simplifiedWidget'
+import {
+  INPUT_EXCLUDED_PROPS,
+  filterWidgetProps
+} from '@/utils/widgetPropFilter'
+
+const value = defineModel<string>({ required: true })
+
+const props = defineProps<{
+  widget: SimplifiedWidget<string>
+  readonly?: boolean
+}>()
+
+const filteredProps = computed(() =>
+  filterWidgetProps(props.widget.options, INPUT_EXCLUDED_PROPS)
+)
+</script>
