770ecea9250ed7a5da0809668df22b2ae2358c7d
ssidharth010 <ssidharth010@gmail.com>
2025-06-29 20:52:13 +0530

    add close button on login dialog

--- src/services/dialogService.ts
+++ src/services/dialogService.ts
@@ -265,7 +265,7 @@ export const useDialogService = () => {
           onSuccess: () => resolve(true)
         },
         dialogComponentProps: {
-          closable: false,
+          closable: true,
           onClose: () => resolve(false)
         }
       })
