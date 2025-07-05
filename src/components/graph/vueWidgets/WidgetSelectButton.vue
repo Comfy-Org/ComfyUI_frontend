0c6196fa3c26814067d0bcbceda45cd9beeacd1a
Benjamin Lu <benjaminlu1107@gmail.com>
2025-07-05 18:52:33 -0400

    Fix value bindings

--- src/components/graph/vueWidgets/WidgetSelectButton.vue
+++ src/components/graph/vueWidgets/WidgetSelectButton.vue
@@ -4,7 +4,7 @@
       widget.name
     }}</label>
     <SelectButton
-      v-model="value"
+      v-model="localValue"
       v-bind="filteredProps"
       :disabled="readonly"
       class="flex-grow min-w-[8em] max-w-[20em] text-xs"
@@ -13,6 +13,7 @@
           label: 'text-xs'
         }
       }"
+      @update:model-value="onChange"
     />
   </div>
 </template>
@@ -21,18 +22,30 @@
 import SelectButton from 'primevue/selectbutton'
 import { computed } from 'vue'
 
+import { useWidgetValue } from '@/composables/graph/useWidgetValue'
 import type { SimplifiedWidget } from '@/types/simplifiedWidget'
 import {
   STANDARD_EXCLUDED_PROPS,
   filterWidgetProps
 } from '@/utils/widgetPropFilter'
 
-const value = defineModel<any>({ required: true })
-
 const props = defineProps<{
   widget: SimplifiedWidget<any>
+  modelValue: any
   readonly?: boolean
 }>()
+
+const emit = defineEmits<{
+  'update:modelValue': [value: any]
+}>()
+
+// Use the composable for consistent widget value handling
+const { localValue, onChange } = useWidgetValue({
+  widget: props.widget,
+  modelValue: props.modelValue,
+  defaultValue: null,
+  emit
+})
 
 const filteredProps = computed(() =>
   filterWidgetProps(props.widget.options, STANDARD_EXCLUDED_PROPS)
