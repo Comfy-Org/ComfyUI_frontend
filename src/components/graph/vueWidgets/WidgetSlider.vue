0d2678d9518c5ea576dc44e236e36a7c01929ec4
Benjamin Lu <benjaminlu1107@gmail.com>
2025-07-05 12:16:32 -0400

    Add number to slider

--- src/components/graph/vueWidgets/WidgetSlider.vue
+++ src/components/graph/vueWidgets/WidgetSlider.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="flex items-center justify-between gap-2">
+  <div class="flex items-center gap-4">
     <label v-if="widget.name" class="text-xs opacity-80 min-w-[4em] truncate">{{
       widget.name
     }}</label>
-    <Slider
-      v-model="localValue"
-      v-bind="filteredProps"
-      :disabled="readonly"
-      class="flex-grow min-w-[8em] max-w-[20em] text-xs"
-      @update:model-value="onChange"
-    />
+    <div class="flex items-center gap-2 flex-1 justify-end">
+      <Slider
+        v-model="localValue"
+        v-bind="filteredProps"
+        :disabled="readonly"
+        class="flex-grow min-w-[8em] max-w-[20em] text-xs"
+        @update:model-value="onChange"
+      />
+      <InputText
+        :value="String(localValue)"
+        :disabled="readonly"
+        type="number"
+        :min="widget.options?.min"
+        :max="widget.options?.max"
+        :step="widget.options?.step"
+        class="w-[6em] text-center text-xs"
+        @input="handleInputChange"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import InputText from 'primevue/inputtext'
 import Slider from 'primevue/slider'
 import { computed } from 'vue'
 
@@ -44,4 +57,17 @@ const { localValue, onChange } = useNumberWidgetValue(
 const filteredProps = computed(() =>
   filterWidgetProps(props.widget.options, STANDARD_EXCLUDED_PROPS)
 )
+
+const handleInputChange = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  const value = parseFloat(target.value)
+
+  if (!isNaN(value)) {
+    const min = props.widget.options?.min ?? -Infinity
+    const max = props.widget.options?.max ?? Infinity
+    const clampedValue = Math.min(Math.max(value, min), max)
+    localValue.value = clampedValue
+    onChange(clampedValue)
+  }
+}
 </script>
