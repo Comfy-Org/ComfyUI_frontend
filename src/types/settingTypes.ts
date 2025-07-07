0f52ff0c35f09461bf26f5b49218d8e0bd2b17b4
filtered <176114999+webfiltered@users.noreply.github.com>
2025-07-07 10:04:27 +1000

    [TS] Remove explicit any type
    This appears to have simply been duplicated from defaultValue

--- src/types/settingTypes.ts
+++ src/types/settingTypes.ts
@@ -35,10 +35,7 @@ export interface Setting {
 export interface SettingParams extends FormItem {
   id: keyof Settings
   defaultValue: any | (() => any)
-  defaultsByInstallVersion?: Record<
-    `${number}.${number}.${number}`,
-    any | (() => any)
-  >
+  defaultsByInstallVersion?: Record<`${number}.${number}.${number}`, unknown>
   onChange?: (newValue: any, oldValue?: any) => void
   // By default category is id.split('.'). However, changing id to assign
   // new category has poor backward compatibility. Use this field to overwrite
