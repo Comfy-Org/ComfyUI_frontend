ec38957c367a58d36bd5e6c59caa90b4fcef251d
filtered <176114999+webfiltered@users.noreply.github.com>
2025-07-10 06:08:33 +1000

    [CodeHealth] Simplify code as follow-up to #4354 (#4400)

--- src/utils/formatUtil.ts
+++ src/utils/formatUtil.ts
@@ -386,8 +386,10 @@ export const downloadUrlToHfRepoUrl = (url: string): string => {
   }
 }
 
-export const isSemVer = (version: string) => {
-  const regex = /^(\d+)\.(\d+)\.(\d+)$/
+export const isSemVer = (
+  version: string
+): version is `${number}.${number}.${number}` => {
+  const regex = /^\d+\.\d+\.\d+$/
   return regex.test(version)
 }
 
