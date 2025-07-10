74867db361342c34e9bae28b8a66d14e40acd53d
Benjamin Lu <benjaminlu1107@gmail.com>
2025-07-10 15:34:45 -0400

    Add title watcher

--- src/components/graph/vueNodes/NodeHeader.vue
+++ src/components/graph/vueNodes/NodeHeader.vue
@@ -39,7 +39,7 @@
 <script setup lang="ts">
 import type { LGraphNode } from '@comfyorg/litegraph'
 import { LiteGraph } from '@comfyorg/litegraph'
-import { computed, onErrorCaptured, ref } from 'vue'
+import { computed, onErrorCaptured, ref, watch } from 'vue'
 
 import EditableText from '@/components/common/EditableText.vue'
 import type { VueNodeData } from '@/composables/graph/useGraphNodeManager'
@@ -78,6 +78,16 @@ const nodeInfo = computed(() => props.nodeData || props.node)
 
 // Local state for title to provide immediate feedback
 const displayTitle = ref(nodeInfo.value?.title || 'Untitled')
+
+// Watch for external changes to the node title
+watch(
+  () => nodeInfo.value?.title,
+  (newTitle) => {
+    if (newTitle && newTitle !== displayTitle.value) {
+      displayTitle.value = newTitle
+    }
+  }
+)
 
 // Compute header color based on node color property or type
 const headerColor = computed(() => {
