142a960797a2057ca738fb9cbe1ba34946a9d268
Benjamin Lu <benjaminlu1107@gmail.com>
2025-07-13 21:32:17 -0400

    [test] Add test file to trigger Danger checks

---
+++ src/test-danger.ts
@@ -0,0 +1,7 @@
+// Test file to trigger Danger checks
+export function testFunction() {
+  console.log('This is a test file to verify Danger is working');
+  console.log('Adding more lines to exceed the 3-line threshold');
+  console.log('Line 5');
+  console.log('Line 6');
+}
