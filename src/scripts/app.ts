4883c14b4201ce22f803ae066fb94323eea13212
filtered <176114999+webfiltered@users.noreply.github.com>
2025-07-24 17:54:29 +1000

    [Cleanup] Remove deprecated: node def validation (#4038)

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -905,12 +905,7 @@ export class ComfyApp {
         .join('/')
     })
 
-    return _.mapValues(
-      await api.getNodeDefs({
-        validate: useSettingStore().get('Comfy.Validation.NodeDefs')
-      }),
-      (def) => translateNodeDef(def)
-    )
+    return _.mapValues(await api.getNodeDefs(), (def) => translateNodeDef(def))
   }
 
   /**
