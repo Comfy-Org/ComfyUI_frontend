3039b4526d46fec7430d5782a12cee03e21c85ba
Terry Jia <terryjia88@gmail.com>
2025-07-26 12:43:22 -0400

    add shift + scroll to pan horizontally (#1164)

--- src/LGraphCanvas.ts
+++ src/LGraphCanvas.ts
@@ -3224,6 +3224,8 @@ export class LGraphCanvas implements CustomEventDispatcher<LGraphCanvasEventMap>
       } else if (e.ctrlKey) {
         scale *= 1 + e.deltaY * (1 - this.zoom_speed) * 0.18
         this.ds.changeScale(scale, [e.clientX, e.clientY], false)
+      } else if (e.shiftKey) {
+        this.ds.offset[0] -= e.deltaY * 1.18 * (1 / scale)
       } else {
         this.ds.offset[0] -= e.deltaX * 1.18 * (1 / scale)
         this.ds.offset[1] -= e.deltaY * 1.18 * (1 / scale)
