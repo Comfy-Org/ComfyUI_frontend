8ceb5e4bef4088714207bc52ba45f61fc58fa39f
Christian Byrne <cbyrne@comfy.org>
2025-07-30 11:26:05 -0700

    Fix errors from rebase (removed `Tag` component import and duplicated imports in api.ts) (#4608)
    Co-authored-by: github-actions <github-actions@github.com>

--- src/components/dialog/content/manager/ManagerHeader.vue
+++ src/components/dialog/content/manager/ManagerHeader.vue
@@ -4,16 +4,22 @@
       <h2 class="text-lg font-normal text-left">
         {{ $t('manager.discoverCommunityContent') }}
       </h2>
-      <Tag
-        v-tooltip.left="$t('manager.legacyManagerUIDescription')"
-        severity="info"
-        icon="pi pi-info-circle"
-        :value="$t('manager.legacyManagerUI')"
-        class="cursor-help"
-        :pt="{
-          root: { class: 'text-xs' }
-        }"
-      />
+      <div class="flex justify-end ml-auto pr-4">
+        <Tag
+          v-tooltip.left="$t('manager.legacyManagerUIDescription')"
+          severity="info"
+          icon="pi pi-info-circle"
+          :value="$t('manager.legacyManagerUI')"
+          class="cursor-help"
+          :pt="{
+            root: { class: 'text-xs' }
+          }"
+        />
+      </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import Tag from 'primevue/tag'
+</script>
