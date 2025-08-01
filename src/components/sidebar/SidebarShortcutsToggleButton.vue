10300f84cdf13fb54b16dbc7d818ef997fa14722
Johnpaul <polymenjohn1@gmail.com>
2025-08-01 03:01:11 +0100

    feat: Add shortcuts toggle button to sidebar and re order mindmap

---
+++ src/components/sidebar/SidebarShortcutsToggleButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <SidebarIcon
+    :tooltip="$t('shortcuts.keyboardShortcuts') + ' (Ctrl+Shift+K)'"
+    :selected="isShortcutsPanelVisible"
+    @click="toggleShortcutsPanel"
+  >
+    <template #icon>
+      <i-lucide:keyboard />
+    </template>
+  </SidebarIcon>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+import { useBottomPanelStore } from '@/stores/workspace/bottomPanelStore'
+
+import SidebarIcon from './SidebarIcon.vue'
+
+const bottomPanelStore = useBottomPanelStore()
+
+const isShortcutsPanelVisible = computed(
+  () => bottomPanelStore.activePanel === 'shortcuts'
+)
+
+const toggleShortcutsPanel = () => {
+  bottomPanelStore.togglePanel('shortcuts')
+}
+</script>
