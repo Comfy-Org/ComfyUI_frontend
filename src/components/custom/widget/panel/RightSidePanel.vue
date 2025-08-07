fa92e33aa0942b379b620b623d65992302ea7bb5
Jin Yi <jin12cc@gmail.com>
2025-08-07 21:59:51 +0900

    [feature] Add model selector dialog integration and custom component types

---
+++ src/components/custom/widget/panel/RightSidePanel.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-80 h-full pl-4 pr-6 pb-8 bg-white dark-theme:bg-neutral-800">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/types/custom_components/navTypes.ts
@@ -0,0 +1,9 @@
+export interface NavItemData {
+  id: string
+  label: string
+}
+
+export interface NavGroupData {
+  title: string
+  items: NavItemData[]
+}
