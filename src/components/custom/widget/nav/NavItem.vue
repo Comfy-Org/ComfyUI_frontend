30925a31aadbbfd74ad8dd751b58d52993cc0514
Jin Yi <jin12cc@gmail.com>
2025-08-10 23:33:05 +0900

    style: color modified

--- src/components/custom/widget/nav/NavItem.vue
+++ src/components/custom/widget/nav/NavItem.vue
@@ -3,8 +3,8 @@
     class="flex items-center gap-2 px-4 py-2 text-xs rounded-md transition-colors cursor-pointer"
     :class="
       active
-        ? 'bg-neutral-100 dark-theme:bg-neutral-600 text-neutral'
-        : 'text-neutral hover:bg-zinc-100 hover:dark-theme:bg-neutral-700'
+        ? 'bg-neutral-100 dark-theme:bg-zinc-700 text-neutral'
+        : 'text-neutral hover:bg-zinc-100 hover:dark-theme:bg-zinc-700/50'
     "
     role="button"
     @click="onClick"
