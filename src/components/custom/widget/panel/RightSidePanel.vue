2e6b8f2155b73f3b30eb5baa0d19c859ee861a2b
Jin Yi <jin12cc@gmail.com>
2025-08-10 23:33:05 +0900

    style: color modified

--- src/components/custom/widget/panel/RightSidePanel.vue
+++ src/components/custom/widget/panel/RightSidePanel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-full pl-4 pr-6 pb-8 bg-white dark-theme:bg-neutral-800">
+  <div class="w-full h-full pl-4 pr-6 pb-8 bg-white dark-theme:bg-zinc-800">
     <slot></slot>
   </div>
 </template>
