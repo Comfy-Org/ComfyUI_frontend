1308458202770970b43fe1362e877da7b0d84597
Johnpaul <polymenjohn1@gmail.com>
2025-08-11 23:42:47 +0100

    ui: align minimap properly

--- src/components/graph/MiniMap.vue
+++ src/components/graph/MiniMap.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-if="visible && initialized"
-    class="minimap-main-container flex absolute bottom-[20px] right-[90px] z-[1000]"
+    class="minimap-main-container flex absolute bottom-[50px] right-[100px] z-[1000]"
   >
     <MiniMapPanel
       v-if="showOptionsPanel"
