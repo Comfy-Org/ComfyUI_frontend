030ff49476e0335500bb605680e8a0cca1c040c5
Johnpaul <polymenjohn1@gmail.com>
2025-08-12 04:00:13 +0100

    styling: position minimap properly

--- src/components/graph/MiniMap.vue
+++ src/components/graph/MiniMap.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-if="visible && initialized"
-    class="minimap-main-container flex absolute bottom-[50px] right-[100px] z-[1000]"
+    class="minimap-main-container flex absolute bottom-[60px] right-[100px] z-[1000]"
   >
     <MiniMapPanel
       v-if="showOptionsPanel"
