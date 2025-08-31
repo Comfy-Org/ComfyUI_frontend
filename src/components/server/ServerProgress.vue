21c8cf8316a9bf71cd8b96fa4bc94ad7a103cbbd
filtered <176114999+webfiltered@users.noreply.github.com>
2025-08-31 22:32:01 +1000

    Adjust oval shadow size and blur for better centering

--- src/components/server/ServerProgress.vue
+++ src/components/server/ServerProgress.vue
@@ -13,7 +13,7 @@
     <!-- Large oval shadow behind content -->
     <div
       v-if="isLoading"
-      class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[500px] bg-neutral-900/95 rounded-full blur-3xl z-8"
+      class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[800px] h-[600px] bg-neutral-900 rounded-full blur-[100px] z-8"
     ></div>
 
     <!-- Main Content Layer -->
