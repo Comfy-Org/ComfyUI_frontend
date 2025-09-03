9bca4c6d9fb0e9f7aafad148b172850d02ca9736
Terry Jia <terryjia88@gmail.com>
2025-09-02 21:09:32 -0400

    remove unnecessary setup file

--- vitest.litegraph.config.ts
+++ vitest.litegraph.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
     include: ['src/lib/litegraph/test/**/*.test.ts']
   },
   resolve: {
