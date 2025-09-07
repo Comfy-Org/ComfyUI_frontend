fb94cd72211cc436a9dafe13b563d52540fa431f
filtered <176114999+webfiltered@users.noreply.github.com>
2025-09-08 04:49:43 +1000

    Move icon inside Tag component using slot

--- src/components/install/GpuPicker.vue
+++ src/components/install/GpuPicker.vue
@@ -44,10 +44,11 @@
     <div class="text-base text-neutral-300 pt-12 px-24 h-16">
       <div v-show="selected === 'mps'" class="flex items-center gap-2">
         <Tag
-          :value="$t('install.gpuPicker.recommended')"
-          class="!bg-white !text-black !rounded-full !normal-case !text-sm !font-medium !px-3"
-        />
-        <i-lucide:badge-check class="text-neutral-300 text-lg" />
+          class="!bg-white !text-black !rounded-full !normal-case !text-sm !font-medium !px-3 !flex !items-center !gap-2"
+        >
+          {{ $t('install.gpuPicker.recommended') }}
+          <i-lucide:badge-check class="text-black text-sm" />
+        </Tag>
       </div>
     </div>
 
