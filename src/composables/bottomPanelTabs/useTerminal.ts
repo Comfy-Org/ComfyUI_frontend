37ea7a3b07fff9948dad7c2035e48a894315360a
filtered <176114999+webfiltered@users.noreply.github.com>
2025-09-08 21:35:34 +1000

    Remove debug code

--- src/composables/bottomPanelTabs/useTerminal.ts
+++ src/composables/bottomPanelTabs/useTerminal.ts
@@ -9,7 +9,6 @@ export function useTerminal(element: Ref<HTMLElement | undefined>) {
   const terminal = markRaw(
     new Terminal({
       convertEol: true,
-      scrollback: 1000,
       theme: {
         background: '#171717'
       }
