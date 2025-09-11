277f37acb1dea258151c0353cc2707934a15204d
Johnpaul <polymenjohn1@gmail.com>
2025-09-11 22:07:44 +0100

    refactor: Simplify class binding in BypassButton component for improved readability

--- src/components/graph/selectionToolbox/BypassButton.vue
+++ src/components/graph/selectionToolbox/BypassButton.vue
@@ -8,12 +8,8 @@
     severity="secondary"
     text
     data-testid="bypass-button"
-    :class="{
-      'hover:dark-theme:!bg-[#262729] hover:!bg-[#E7E6E6]': true,
-      'dark-theme:[&:not(:active)]:!bg-[#262729] [&:not(:active)]:!bg-[#E7E6E6]':
-        isBypassed
-    }"
-    @click="byPass"
+    class="hover:dark-theme:bg-charcoal-300 hover:bg-[#E7E6E6]"
+    @click="toggleBypass"
   >
     <template #icon>
       <i-lucide:ban class="w-4 h-4" />
@@ -23,7 +19,6 @@
 
 <script setup lang="ts">
 import Button from 'primevue/button'
-import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 import { useSelectionState } from '@/composables/graph/useSelectionState'
@@ -32,9 +27,8 @@ import { useCommandStore } from '@/stores/commandStore'
 const { t } = useI18n()
 const commandStore = useCommandStore()
 const { hasAnySelection } = useSelectionState()
-const isBypassed = ref(false)
 
-const byPass = async () => {
+const toggleBypass = async () => {
   await commandStore.execute('Comfy.Canvas.ToggleSelectedNodes.Bypass')
 }
 </script>
