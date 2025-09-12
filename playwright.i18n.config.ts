450f156d534a7f59abd83e2330f72713cad1f437
snomiao <snomiao@gmail.com>
2025-09-12 00:44:39 +0000

    fix(knip.config.ts): revert changes
    revert changes

--- playwright.i18n.config.ts
+++ playwright.i18n.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './scripts',
+  use: {
+    baseURL: 'http://localhost:5173',
+    headless: true
+  },
   reporter: 'list',
   timeout: 60000,
   testMatch: /collect-i18n-.*\.ts/,
