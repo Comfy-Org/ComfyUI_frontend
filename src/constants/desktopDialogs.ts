b7a502c9a2f5daef3f9763d1c75431cc7944b9e4
filtered <176114999+webfiltered@users.noreply.github.com>
2025-09-16 04:58:05 +1000

    Prefer template literal for multiline string

--- src/constants/desktopDialogs.ts
+++ src/constants/desktopDialogs.ts
@@ -16,10 +16,11 @@ interface DialogData {
 export const reinstallDialog: DialogData = {
   id: 'reinstallFreshStart',
   title: 'Reinstall ComfyUI (Fresh Start)?',
-  message:
-    "Sorry, we can't launch ComfyUI because some installed packages aren't compatible.\n\n" +
-    'Click Reinstall to restore ComfyUI and get back up and running.\n\n' +
-    "Please note: if you've added custom nodes, you'll need to reinstall them after this process.",
+  message: `Sorry, we can't launch ComfyUI because some installed packages aren't compatible.
+
+Click Reinstall to restore ComfyUI and get back up and running.
+
+Please note: if you've added custom nodes, you'll need to reinstall them after this process.`,
   buttons: [
     {
       label: 'Learn More',
