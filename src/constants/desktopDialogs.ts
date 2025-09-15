c0a0467d6ca7ae884ec3f37a21d83b6ff23ca0e6
filtered <176114999+webfiltered@users.noreply.github.com>
2025-09-16 04:56:03 +1000

    Add desktop dialog data structure

---
+++ src/constants/desktopDialogs.ts
@@ -0,0 +1,37 @@
+interface DialogAction {
+  label: string
+  action: 'openUrl' | 'close'
+  url?: string
+  severity?: 'danger'
+  returnValue: string
+}
+
+interface DialogData {
+  id: string
+  title: string
+  message: string
+  buttons: DialogAction[]
+}
+
+export const reinstallDialog: DialogData = {
+  id: 'reinstallFreshStart',
+  title: 'Reinstall ComfyUI (Fresh Start)?',
+  message:
+    "Sorry, we can't launch ComfyUI because some installed packages aren't compatible.\n\n" +
+    'Click Reinstall to restore ComfyUI and get back up and running.\n\n' +
+    "Please note: if you've added custom nodes, you'll need to reinstall them after this process.",
+  buttons: [
+    {
+      label: 'Learn More',
+      action: 'openUrl',
+      url: 'https://docs.comfy.org',
+      returnValue: 'openDocs'
+    },
+    {
+      label: 'Reinstall',
+      action: 'close',
+      severity: 'danger',
+      returnValue: 'resetVenv'
+    }
+  ]
+}
