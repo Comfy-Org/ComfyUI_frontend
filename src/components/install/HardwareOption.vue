b86d76dc55eee4ad723600162e1446066938d943
filtered <176114999+webfiltered@users.noreply.github.com>
2025-09-17 11:18:36 +1000

    Move Tailwind to elements

--- src/components/install/HardwareOption.vue
+++ src/components/install/HardwareOption.vue
@@ -2,16 +2,16 @@
   <div class="relative">
     <!-- Recommended Badge -->
     <button
-      class="hardware-option p-5 flex flex-col items-center rounded-3xl transition-all duration-200 bg-neutral-900/70"
+      class="hardware-option w-[170px] h-[190px] p-5 flex flex-col items-center rounded-3xl transition-all duration-200 bg-neutral-900/70"
       :class="{
-        'selected-border': selected,
+        'border-solid border-4 border-brand-yellow': selected,
         'border-4 border-transparent': !selected
       }"
       @click="$emit('click')"
     >
       <!-- Icon/Logo Area - Rounded square container -->
       <div
-        class="icon-container shrink-0 rounded-2xl bg-neutral-800 flex items-center justify-center overflow-hidden"
+        class="icon-container w-[110px] h-[110px] shrink-0 rounded-2xl bg-neutral-800 flex items-center justify-center overflow-hidden"
       >
         <img
           v-if="imagePath"
@@ -49,19 +49,3 @@ defineProps<Props>()
 
 defineEmits<{ click: [] }>()
 </script>
-
-<style scoped>
-@reference '../../assets/css/style.css';
-
-.hardware-option {
-  @apply w-[170px] h-[190px];
-}
-
-.icon-container {
-  @apply w-[110px] h-[110px];
-}
-
-.hardware-option.selected-border {
-  @apply border-solid border-4 border-brand-yellow;
-}
-</style>
