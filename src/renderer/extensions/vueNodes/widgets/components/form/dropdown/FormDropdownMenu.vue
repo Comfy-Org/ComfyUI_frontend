839a889f4d480f97c1fdadc0c0b168f55eedf340
Rizumu Ayaka <rizumu@ayaka.moe>
2025-09-18 19:56:23 +0800

    feat: filter and actions components

--- src/renderer/extensions/vueNodes/widgets/components/form/dropdown/FormDropdownMenu.vue
+++ src/renderer/extensions/vueNodes/widgets/components/form/dropdown/FormDropdownMenu.vue
@@ -1,21 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-import { cn } from '@/utils/tailwindUtil'
-
+import FormDropdownActions from './FormDropdownActions.vue'
+import FormDropdownFilter from './FormDropdownFilter.vue'
 import FormDropdownMenuItem from './FormDropdownMenuItem.vue'
 
-const actionButtonStyle =
-  'h-8 bg-zinc-500/20 rounded-lg outline outline-1 outline-offset-[-1px] outline-sand-100 dark-theme:outline-neutral-700'
-
 const filterIndex = ref(0)
-const filterButtonStyle =
-  'px-4 py-2 rounded-md inline-flex justify-center items-center cursor-pointer hover:text-black hover:dark-theme:text-white'
-
 const layoutMode = ref<'list' | 'grid'>('grid')
-const layoutSwitchItemStyle =
-  'size-6 flex justify-center items-center rounded-sm cursor-pointer hover:scale-108 hover:text-black hover:dark-theme:text-white'
-
 const selectedIndex = ref(0)
 </script>
 
@@ -26,86 +17,9 @@ const selectedIndex = ref(0)
     class="w-103 h-[640px] pt-4 bg-white dark-theme:bg-charcoal-800 rounded-lg outline outline-offset-[-1px] outline-sand-100 dark-theme:outline-zinc-800 flex flex-col"
   >
     <!-- Filter -->
-    <div class="flex gap-1 text-zinc-400 px-4 mb-4">
-      <div
-        :class="
-          cn(
-            filterButtonStyle,
-            filterIndex === 0
-              ? '!bg-zinc-500/20 text-black dark-theme:text-white'
-              : 'bg-transparent'
-          )
-        "
-        @click="filterIndex = 0"
-      >
-        Inputs
-      </div>
-      <div
-        :class="
-          cn(
-            filterButtonStyle,
-            filterIndex === 1
-              ? '!bg-zinc-500/20 text-black dark-theme:text-white'
-              : 'bg-transparent'
-          )
-        "
-        @click="filterIndex = 1"
-      >
-        Outputs
-      </div>
-    </div>
+    <FormDropdownFilter v-model:filter-index="filterIndex" />
     <!-- Actions -->
-    <div class="flex gap-2 text-zinc-400 px-4">
-      <div
-        :class="
-          cn(
-            actionButtonStyle,
-            'flex-1 flex px-2 items-center text-base leading-none'
-          )
-        "
-      >
-        <i-lucide:search class="mr-2 size-4" /><span> Search </span>
-      </div>
-      <!-- Sort Select -->
-      <div
-        :class="cn(actionButtonStyle, 'w-8 flex justify-center items-center')"
-      >
-        <i-lucide:arrow-up-down class="size-4" />
-      </div>
-      <!-- Layout Switch -->
-      <div
-        :class="
-          cn(actionButtonStyle, 'flex justify-center items-center p-1 gap-1')
-        "
-      >
-        <div
-          :class="
-            cn(
-              layoutSwitchItemStyle,
-              layoutMode === 'list'
-                ? 'bg-neutral-500/50 text-black dark-theme:text-white'
-                : ''
-            )
-          "
-          @click="layoutMode = 'list'"
-        >
-          <i-lucide:list class="size-4" />
-        </div>
-        <div
-          :class="
-            cn(
-              layoutSwitchItemStyle,
-              layoutMode === 'grid'
-                ? 'bg-neutral-500/50 text-black dark-theme:text-white'
-                : ''
-            )
-          "
-          @click="layoutMode = 'grid'"
-        >
-          <i-lucide:layout-grid class="size-4" />
-        </div>
-      </div>
-    </div>
+    <FormDropdownActions v-model:layout-mode="layoutMode" />
     <!-- List -->
     <div class="flex overflow-hidden relative">
       <div
