963c32b1ff79318bbeb1bc567e734ff5abf21297
Alexander Brown <drjkl@comfy.org>
2025-09-17 23:29:15 -0700

    lint: add label as a checked attribute.

--- eslint.config.ts
+++ eslint.config.ts
@@ -197,8 +197,9 @@ export default defineConfig([
               'aria-placeholder',
               'aria-roledescription',
               'aria-valuetext',
-              'title',
-              'placeholder'
+              'label',
+              'placeholder',
+              'title'
             ],
             img: ['alt']
           },
