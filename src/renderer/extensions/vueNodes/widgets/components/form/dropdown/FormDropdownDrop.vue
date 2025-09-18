8ef1dd2fdd0abdb7bbfd15542ef6be55a5acaff4
Rizumu Ayaka <rizumu@ayaka.moe>
2025-09-18 17:38:51 +0800

    feat: light mode style

--- src/renderer/extensions/vueNodes/widgets/components/form/dropdown/FormDropdownDrop.vue
+++ src/renderer/extensions/vueNodes/widgets/components/form/dropdown/FormDropdownDrop.vue
@@ -8,7 +8,7 @@
   <div>
     <slot>
       <div
-        class="py-7 flex justify-center items-center rounded-lg border border-neutral-700 outline-1 outline-sold outline-offset-[-6px] outline-zinc-800 outline-dashed hover:bg-zinc-500/10 hover:border-blue-500/20"
+        class="py-7 flex justify-center items-center rounded-lg border border-neutral-500/50 outline-1 outline-sold outline-offset-[-6px] outline-zinc-500/50 outline-dashed hover:bg-zinc-500/10 hover:border-blue-500/20"
       >
         or drag and drop files here
       </div>
