ed9e8a21448586771f85b9d56497fdc67b3106c2
bymyself <cbyrne@comfy.org>
2025-09-19 21:40:44 -0700

    chore: enforce vue block order in pre-commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,19 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+const vueBlockOrderRule =
+  "vue/block-order: ['error', {'order': ['docs', 'script', 'template', 'i18n', 'style']}]"
+
 export default {
   './**/*.js': (stagedFiles) => formatAndEslint(stagedFiles),
 
-  './**/*.{ts,tsx,vue,mts}': (stagedFiles) => [
+  './**/*.{ts,tsx,mts}': (stagedFiles) => [
     ...formatAndEslint(stagedFiles),
     'pnpm typecheck'
-  ]
+  ],
+
+  './**/*.vue': (stagedFiles) => [
+    runVueBlockOrder(stagedFiles),
+    ...formatAndEslint(stagedFiles)
+  ].filter(Boolean)
 }
 
 function formatAndEslint(fileNames) {
@@ -12,4 +21,10 @@ function formatAndEslint(fileNames) {
     `pnpm exec eslint --cache --fix ${fileNames.join(' ')}`,
     `pnpm exec prettier --cache --write ${fileNames.join(' ')}`
   ]
+}
+
+function runVueBlockOrder(fileNames) {
+  if (fileNames.length === 0) return null
+  const quotedFiles = fileNames.map((file) => `"${file}"`).join(' ')
+  return `pnpm exec eslint --fix --rule "${vueBlockOrderRule}" ${quotedFiles}`
 }
