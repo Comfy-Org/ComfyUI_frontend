7291255fa4f09dd6995c2b5eed3da62dc770a308
Johnpaul <polymenjohn1@gmail.com>
2025-09-22 22:50:18 +0100

    refactor: remove unused SMALL_MODEL_SIZE_LIMIT constant from templateWorkflows

--- src/constants/templateWorkflows.ts
+++
@@ -1,4 +0,0 @@
-// Constants related to Workflow Templates functionality
-// Size threshold (in GB) for categorizing templates as "Small Models".
-// Templates with size <= this limit will appear under the Performance > Small Models category.
-export const SMALL_MODEL_SIZE_LIMIT = 3
