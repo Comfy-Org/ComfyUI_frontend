7b3baef6252f5e725c06a6c8832b8c8ab2945b30
Austin Mroz <austin@comfy.org>
2025-09-22 15:54:47 -0500

    Merge partial changes from working branch
    Wrap onConfigure instead of _internalConfigureAfterSlots
    
    Use previously set proxyWidgets property instead of calculating
    
    Fix canvas preview image support
    
    Split proxy handlers into individual functions.
    
    Add additional tests

--- src/scripts/proxyWidget.ts
+++ src/scripts/proxyWidget.ts
@@ -1,3 +1,4 @@
+import { useNodeImage } from '@/composables/node/useNodeImage'
 import type { LGraph, LGraphNode } from '@/lib/litegraph/src/litegraph'
 import { SubgraphNode } from '@/lib/litegraph/src/subgraph/SubgraphNode'
 import type { IBaseWidget } from '@/lib/litegraph/src/types/widgets.ts'
@@ -6,27 +7,25 @@ import { useCanvasStore } from '@/renderer/core/canvas/canvasStore'
 import { parseProxyWidgets } from '@/schemas/proxyWidget'
 import { DOMWidgetImpl } from '@/scripts/domWidget'
 import { useDomWidgetStore } from '@/stores/domWidgetStore'
+import { useNodeOutputStore } from '@/stores/imagePreviewStore'
+import { getNodeByExecutionId } from '@/utils/graphTraversalUtil'
 
-const originalConfigureAfterSlots =
-  SubgraphNode.prototype._internalConfigureAfterSlots
-SubgraphNode.prototype._internalConfigureAfterSlots = function () {
+const originalOnConfigure = SubgraphNode.prototype.onConfigure
+SubgraphNode.prototype.onConfigure = function (serialisedNode) {
+  if (!this.isSubgraphNode())
+    throw new Error("Can't add proxyWidgets to non-subgraphNode")
+
   const canvasStore = useCanvasStore()
   const subgraphNode = this
-  //Must give value to proxyWidgets prior to injecting or it won't serialize
+  //Must give value to proxyWidgets prior to defining or it won't serialize
   subgraphNode.properties.proxyWidgets ??= '[]'
-  const proxyWidgets = subgraphNode.properties.proxyWidgets
+  let proxyWidgets = subgraphNode.properties.proxyWidgets
 
-  //Takes no arguements, returns nothing
-  //Sometimes called multiple times on initialization, sometimes once
-  //Clobbers all widgets
-  originalConfigureAfterSlots?.bind(this)?.()
+  originalOnConfigure?.bind(this)?.(serialisedNode)
 
   Object.defineProperty(subgraphNode.properties, 'proxyWidgets', {
     get: () => {
-      const result = subgraphNode.widgets
-        .filter((w) => isProxyWidget(w))
-        .map((w) => [w._overlay.nodeId, w._overlay.widgetName])
-      return JSON.stringify(result)
+      return proxyWidgets
     },
     set: (property: string) => {
       const parsed = parseProxyWidgets(property)
@@ -51,6 +50,7 @@ SubgraphNode.prototype._internalConfigureAfterSlots = function () {
           widgetState.widget = w
         }
       }
+      proxyWidgets = property
       canvasStore.canvas?.setDirty(true, true)
       subgraphNode._setConcreteSlots()
       subgraphNode.arrange()
@@ -63,6 +63,7 @@ type Overlay = Partial<IBaseWidget> & {
   nodeId: string
   widgetName: string
   isProxyWidget: boolean
+  node?: LGraphNode
 }
 type ProxyWidget = IBaseWidget & { _overlay: Overlay }
 function isProxyWidget(w: IBaseWidget): w is ProxyWidget {
@@ -92,35 +93,87 @@ function addProxyWidget(
   }
   return addProxyFromOverlay(subgraphNode, overlay)
 }
-function resolveLinkedWidget(overlay: Overlay): IBaseWidget | undefined {
+function resolveLinkedWidget(
+  overlay: Overlay
+): [LGraphNode | undefined, IBaseWidget | undefined] {
   const { graph, nodeId, widgetName } = overlay
-  let g: LGraph | undefined = graph
-  let n: LGraphNode | SubgraphNode | undefined = undefined
-  for (const id of nodeId.split(':')) {
-    n = g?._nodes_by_id?.[id]
-    g = n?.isSubgraphNode?.() ? n.subgraph : undefined
-  }
-  if (!n) return undefined
-  return n.widgets?.find((w: IBaseWidget) => w.name === widgetName)
+  const n = getNodeByExecutionId(graph, nodeId)
+  if (!n) return [undefined, undefined]
+  return [n, n.widgets?.find((w: IBaseWidget) => w.name === widgetName)]
 }
 function addProxyFromOverlay(subgraphNode: SubgraphNode, overlay: Overlay) {
-  let linkedWidget = resolveLinkedWidget(overlay)
-  const bw = linkedWidget ?? disconnectedWidget
-  const handler = Object.fromEntries(
-    ['get', 'set', 'getPrototypeOf', 'ownKeys', 'has'].map((s) => {
-      const func = function (t: object, p: string, ...rest: object[]) {
-        if (s == 'get' && p == '_overlay') return overlay
-        let r = rest.at(-1)
-        if (overlay.hasOwnProperty(p)) r = t = overlay
-        else {
-          t = bw
-          if (p == 'value') r = t
+  let [linkedNode, linkedWidget] = resolveLinkedWidget(overlay)
+  let backingWidget = linkedWidget ?? disconnectedWidget
+  if (overlay.widgetName == '$$canvas-image-preview')
+    overlay.node = new Proxy(subgraphNode, {
+      get(_t, p) {
+        if (p == 'imgs') {
+          if (linkedNode) {
+            const images =
+              useNodeOutputStore().getNodeOutputs(linkedNode)?.images ?? []
+            if (images !== linkedNode.images) {
+              linkedNode.images = images
+              useNodeImage(linkedNode).showPreview()
+            }
+            return linkedNode.imgs
+          }
+          return []
         }
-        return (Reflect as any)[s](t, p, ...rest.slice(0, -1), r)
+        return Reflect.get(subgraphNode, p)
       }
-      return [s, func]
     })
-  )
+  /**
+   * A set of handlers which define widget interaction
+   * Many arguments are shared between function calls
+   * @param {{IBaseWidget} _t - The "target" the call is originally made on.
+   *   This argument is never used, but must be defined for typechecking
+   * @param {{string}} property - The name of the accessed value.
+   *   Checked for conditional logic, but never changed
+   * @param {{object}} receiver - The object the result is set to
+   *   and the vlaue used as 'this' if property is a get/set method
+   * @param {{unknown}} value - only used on set calls. The thing being assigned
+   */
+  const handler = {
+    get(_t: IBaseWidget, property: string, receiver: object) {
+      let redirectedTarget: object = backingWidget
+      let redirectedReceiver = receiver
+      if (property == '_overlay') return overlay
+      else if (property == 'value') redirectedReceiver = backingWidget
+      if (overlay.hasOwnProperty(property)) {
+        redirectedTarget = overlay
+        redirectedReceiver = overlay
+      }
+      return Reflect.get(redirectedTarget, property, redirectedReceiver)
+    },
+    set(_t: IBaseWidget, property: string, value: unknown, receiver: object) {
+      let redirectedTarget: object = backingWidget
+      let redirectedReceiver = receiver
+      if (property == 'value') redirectedReceiver = backingWidget
+      else if (property == 'computedHeight') {
+        //update linkage regularly, but no more than once per frame
+        ;[linkedNode, linkedWidget] = resolveLinkedWidget(overlay)
+        backingWidget = linkedWidget ?? disconnectedWidget
+      }
+      if (overlay.hasOwnProperty(property)) {
+        redirectedTarget = overlay
+        redirectedReceiver = overlay
+      }
+      return Reflect.set(redirectedTarget, property, value, redirectedReceiver)
+    },
+    getPrototypeOf() {
+      return Reflect.getPrototypeOf(backingWidget)
+    },
+    ownKeys() {
+      return Reflect.ownKeys(backingWidget)
+    },
+    has(_t: IBaseWidget, property: string) {
+      let redirectedTarget: object = backingWidget
+      if (overlay.hasOwnProperty(property)) {
+        redirectedTarget = overlay
+      }
+      return Reflect.has(redirectedTarget, property)
+    }
+  }
   const w = new Proxy(disconnectedWidget, handler)
   subgraphNode.widgets.push(w)
   return w
