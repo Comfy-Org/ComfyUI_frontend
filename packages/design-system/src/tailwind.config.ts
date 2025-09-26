6362d54a7731ccfe9c24d18a7d4ca832c01dd07b
filtered <176114999+webfiltered@users.noreply.github.com>
2025-09-26 12:29:38 +1000

    Restore icon configuration in main app, keep design-system clean

--- packages/design-system/src/tailwind.config.ts
+++ packages/design-system/src/tailwind.config.ts
@@ -1,25 +1,4 @@
-import lucide from '@iconify-json/lucide/icons.json' with { type: 'json' }
-import { addDynamicIconSelectors } from '@iconify/tailwind'
-
 export default {
   content: [],
-
-  safelist: [
-    'icon-[lucide--folder]',
-    'icon-[lucide--package]',
-    'icon-[lucide--image]',
-    'icon-[lucide--video]',
-    'icon-[lucide--box]',
-    'icon-[lucide--audio-waveform]',
-    'icon-[lucide--message-circle]'
-  ],
-
-  plugins: [
-    addDynamicIconSelectors({
-      iconSets: {
-        lucide
-      },
-      prefix: 'icon'
-    })
-  ]
+  plugins: []
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,28 @@
-import { default as config } from '@comfyorg/design-system/tailwind-config'
+import lucide from '@iconify-json/lucide/icons.json' with { type: 'json' }
+import { addDynamicIconSelectors } from '@iconify/tailwind'
+
+import { iconCollection } from './build/customIconCollection'
 
 export default {
-  ...config,
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}']
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+
+  safelist: [
+    'icon-[lucide--folder]',
+    'icon-[lucide--package]',
+    'icon-[lucide--image]',
+    'icon-[lucide--video]',
+    'icon-[lucide--box]',
+    'icon-[lucide--audio-waveform]',
+    'icon-[lucide--message-circle]'
+  ],
+
+  plugins: [
+    addDynamicIconSelectors({
+      iconSets: {
+        comfy: iconCollection,
+        lucide
+      },
+      prefix: 'icon'
+    })
+  ]
 }
