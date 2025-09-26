78703eb0f5273c6395ff3538ef0ba582c6812818
filtered <176114999+webfiltered@users.noreply.github.com>
2025-09-26 14:36:33 +1000

    Add Tailwind icon configuration to design-system

--- packages/design-system/src/tailwind.config.ts
+++ packages/design-system/src/tailwind.config.ts
@@ -1,4 +1,23 @@
+import lucide from '@iconify-json/lucide/icons.json' with { type: 'json' }
+import { addDynamicIconSelectors } from '@iconify/tailwind'
+
 export default {
   content: [],
-  plugins: []
+  safelist: [
+    'icon-[lucide--folder]',
+    'icon-[lucide--package]',
+    'icon-[lucide--image]',
+    'icon-[lucide--video]',
+    'icon-[lucide--box]',
+    'icon-[lucide--audio-waveform]',
+    'icon-[lucide--message-circle]'
+  ],
+  plugins: [
+    addDynamicIconSelectors({
+      iconSets: {
+        lucide
+      },
+      prefix: 'icon'
+    })
+  ]
 }
