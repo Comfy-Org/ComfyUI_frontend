c1498008ab2593d57a16957014aceac93edcb501
filtered <176114999+webfiltered@users.noreply.github.com>
2025-10-04 17:07:34 +1000

    Remove unused const - only used in installer

--- src/constants/uvMirrors.ts
+++ src/constants/uvMirrors.ts
@@ -26,9 +26,3 @@ export const PYTHON_MIRROR: UVMirror = {
   validationPathSuffix:
     '/20250115/cpython-3.10.16+20250115-aarch64-apple-darwin-debug-full.tar.zst.sha256'
 }
-
-export const PYPI_MIRROR: UVMirror = {
-  settingId: 'Comfy-Desktop.UV.PypiInstallMirror',
-  mirror: 'https://pypi.org/simple/',
-  fallbackMirror: 'https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple'
-}
