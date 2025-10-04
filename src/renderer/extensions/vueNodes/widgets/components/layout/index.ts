d9606ef20d794c2e0d0b3a554cfc1cd3c693d740
DrJKL <DrJKL0424@gmail.com>
2025-10-03 18:47:02 -0700

    WIP: More property mappings

--- src/renderer/extensions/vueNodes/widgets/components/layout/index.ts
+++ src/renderer/extensions/vueNodes/widgets/components/layout/index.ts
@@ -2,15 +2,13 @@ import { cn } from '@/utils/tailwindUtil'
 
 export const WidgetInputBaseClass = cn([
   // Background
-  'bg-node-component-widget-input-surface/10',
+  'bg-node-component-widget-input-surface',
+  'text-node-component-widget-input',
   // Outline
   'border-none',
-  'outline',
-  'outline-1',
-  'outline-offset-[-1px]',
-  'outline-zinc-300/10',
+  'outline outline-offset-[-1px] outline-zinc-300/10',
   // Rounded
-  '!rounded-lg',
+  'rounded-lg',
   // Hover
   'hover:outline-blue-500/80'
 ])
