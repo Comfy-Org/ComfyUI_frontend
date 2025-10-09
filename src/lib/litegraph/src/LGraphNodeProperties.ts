2b35ab2c3f8bfabcad74722f6224c135ba3f2e2a
DrJKL <DrJKL0424@gmail.com>
2025-10-08 18:27:19 -0700

    fix: Allow force-updates to properties by setting to the same value

--- src/lib/litegraph/src/LGraphNodeProperties.ts
+++ src/lib/litegraph/src/LGraphNodeProperties.ts
@@ -132,14 +132,12 @@ export class LGraphNodeProperties {
     oldValue: any,
     newValue: any
   ): void {
-    // if (oldValue !== newValue && this.node.graph) {
     this.node.graph?.trigger('node:property:changed', {
       nodeId: this.node.id,
       property: propertyPath,
       oldValue,
       newValue
     })
-    // }
   }
 
   /**
