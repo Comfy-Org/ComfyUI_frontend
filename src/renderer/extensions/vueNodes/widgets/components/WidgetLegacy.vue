86f286e2bcd4f9682058eeba20a9a8eb2e3bed78
Austin Mroz <austin@comfy.org>
2025-10-10 16:55:18 -0700

    Revert to older, more functional mouse code
    Remove unneeded draw code - Code search showed no custom nodes using the
    newer class-based widget draw/mouse commands. They have been removed to
    simplify the code

--- src/renderer/extensions/vueNodes/widgets/components/WidgetLegacy.vue
+++ src/renderer/extensions/vueNodes/widgets/components/WidgetLegacy.vue
@@ -1,33 +1,32 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
+
 import type { LGraphNode } from '@/lib/litegraph/src/LGraphNode'
-import {  LiteGraph } from '@/lib/litegraph/src/litegraph'
+import { LiteGraph } from '@/lib/litegraph/src/litegraph'
 import type { IBaseWidget } from '@/lib/litegraph/src/types/widgets'
-import type { CanvasPointerEvent } from 'lib/litegrpah/src/types/events'
 import { BaseWidget } from '@/lib/litegraph/src/widgets/BaseWidget'
-import type { SimplifiedWidget } from '@/types/simplifiedWidget'
 import { useCanvasStore } from '@/renderer/core/canvas/canvasStore'
+import type { SimplifiedWidget } from '@/types/simplifiedWidget'
 
 const props = defineProps<{
   widget: SimplifiedWidget<void>
   readonly?: boolean
 }>()
 
-
 const canvasEl = ref()
-console.log(props.widget)
-
 
 const { canvas } = useCanvasStore()
-let node: LGraphNode|undefined
-let widget: IBaseWidget|undefined
+let node: LGraphNode | undefined
+let widgetInstance: IBaseWidget | undefined
 onMounted(() => {
-  node = canvas?.graph?.getNodeById(canvasEl.value.attributes['node-id'].value) ?? undefined
+  node =
+    canvas?.graph?.getNodeById(canvasEl.value.attributes['node-id'].value) ??
+    undefined
   if (!node) return
-  widget = node.widgets?.find((w) => w.name === props.widget.name)
-  if (!widget) return
-  const originalCallback = widget.callback
-  widget.callback = function(...args) {
+  widgetInstance = node.widgets?.find((w) => w.name === props.widget.name)
+  if (!widgetInstance) return
+  const originalCallback = widgetInstance.callback
+  widgetInstance.callback = function (...args) {
     const ret = originalCallback?.apply(this, args)
     draw()
     return ret
@@ -35,22 +34,22 @@ onMounted(() => {
   draw()
 })
 function draw() {
-  if (!widget || !node) return
+  if (!widgetInstance || !node) return
   const ctx = canvasEl.value?.getContext('2d')
   if (!ctx) return
   const bgcolor = LiteGraph.WIDGET_BGCOLOR
   try {
     //zinc-500/10
-    LiteGraph.WIDGET_BGCOLOR = "#71717A1A"
+    LiteGraph.WIDGET_BGCOLOR = '#71717A1A'
     const { width } = canvasEl.value
-    const height = widget.computeSize ? widget.computeSize(width)[1] : 20
-    widget.y = 0
+    const height = widgetInstance.computeSize
+      ? widgetInstance.computeSize(width)[1]
+      : 20
+    widgetInstance.y = 0
     canvasEl.value.height = height
-    ctx.fill
-    if (widget instanceof BaseWidget)
-      widget.drawWidget(ctx, { width, showText: true })
-    else
-      widget.draw?.(ctx, node, width, 0, height)
+    if (widgetInstance instanceof BaseWidget)
+      widgetInstance.drawWidget(ctx, { width, showText: true })
+    else widgetInstance.draw?.(ctx, node, width, 0, height)
   } finally {
     LiteGraph.WIDGET_BGCOLOR = bgcolor
   }
@@ -58,36 +57,24 @@ function draw() {
 //TODO: some nodes use onPointerDown
 let interactingMouse: boolean = false
 function handleMouse(e: PointerEvent) {
-  if (!node || !widget || !canvas) return
-  if (e.type == 'pointerup')
-    interactingMouse = false
-  else if (e.type == 'pointerdown')
-    interactingMouse = true
-  else if (e.type == 'pointermove' && !interactingMouse)
-    return
+  if (!node || !widgetInstance || !canvas) return
+  if (e.type == 'pointerup') interactingMouse = false
+  else if (e.type == 'pointerdown') interactingMouse = true
+  else if (e.type == 'pointermove' && !interactingMouse) return
   const x = e.offsetX
   const y = e.offsetY
-  const canvasPointerEvent: CanvasPointerEvent = {
-    ...e,
-    canvasX: x + node.pos[0],
-    canvasY: y + node.pos[1]
+  if (widgetInstance.mouse) {
+    // @ts-expect-error - event is missing properties
+    widgetInstance.mouse(e, [x, y], node)
   }
-
-  if (widget.mouse) {
-    widget.mouse(canvasPointerEvent, [x, y], node)
-    return
-  }
-  if (!(widget instanceof BaseWidget)) return
-  if (widget.onClick && e.type == 'pointerup')
-    widget.onClick({e: canvasPointerEvent, node, canvas})
-  else if (widget.onDrag && e.type =='pointermove' && interactingMouse)
-    widget.onDrag({e: canvasPointerEvent, node, canvas})
 }
 </script>
 <template>
-  <canvas ref="canvasEl" class="ml-[-24px] mr-[-16px] cursor-crosshair"
+  <canvas
+    ref="canvasEl"
+    class="ml-[-24px] mr-[-16px] cursor-crosshair"
     @pointerdown="handleMouse"
     @pointermove="handleMouse"
     @pointerup="handleMouse"
-    />
+  />
 </template>
