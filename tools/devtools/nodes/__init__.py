6ff0c19a0456472c027bbf5e3826b742d27c77d0
bymyself <cbyrne@comfy.org>
2025-10-11 12:14:55 -0700

    refactor and reorganize dev tool nodes

--- tools/devtools/nodes/__init__.py
+++ tools/devtools/nodes/__init__.py
@@ -1,36 +1,62 @@
 from __future__ import annotations
 
-from .nodes import (
+from .errors import (
     DeprecatedNode,
-    DummyPatch,
     ErrorRaiseNode,
     ErrorRaiseNodeWithMessage,
     ExperimentalNode,
-    LoadAnimatedImageTest,
+    NODE_CLASS_MAPPINGS as errors_class_mappings,
+    NODE_DISPLAY_NAME_MAPPINGS as errors_display_name_mappings,
+)
+from .inputs import (
     LongComboDropdown,
-    MultiSelectNode,
     NodeWithBooleanInput,
     NodeWithDefaultInput,
     NodeWithForceInput,
     NodeWithOptionalComboInput,
     NodeWithOptionalInput,
     NodeWithOnlyOptionalInput,
-    NodeWithOutputCombo,
     NodeWithOutputList,
     NodeWithSeedInput,
     NodeWithStringInput,
     NodeWithUnionInput,
     NodeWithValidation,
+    SimpleSlider,
+    NODE_CLASS_MAPPINGS as inputs_class_mappings,
+    NODE_DISPLAY_NAME_MAPPINGS as inputs_display_name_mappings,
+)
+from .models import (
+    DummyPatch,
+    LoadAnimatedImageTest,
     ObjectPatchNode,
+    NODE_CLASS_MAPPINGS as models_class_mappings,
+    NODE_DISPLAY_NAME_MAPPINGS as models_display_name_mappings,
+)
+from .remote import (
+    MultiSelectNode,
+    NodeWithOutputCombo,
     RemoteWidgetNode,
     RemoteWidgetNodeWithControlAfterRefresh,
     RemoteWidgetNodeWithParams,
     RemoteWidgetNodeWithRefresh,
     RemoteWidgetNodeWithRefreshButton,
-    SimpleSlider,
-    NODE_CLASS_MAPPINGS,
-    NODE_DISPLAY_NAME_MAPPINGS,
+    NODE_CLASS_MAPPINGS as remote_class_mappings,
+    NODE_DISPLAY_NAME_MAPPINGS as remote_display_name_mappings,
 )
+
+NODE_CLASS_MAPPINGS = {
+    **errors_class_mappings,
+    **inputs_class_mappings,
+    **remote_class_mappings,
+    **models_class_mappings,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    **errors_display_name_mappings,
+    **inputs_display_name_mappings,
+    **remote_display_name_mappings,
+    **models_display_name_mappings,
+}
 
 __all__ = [
     "DeprecatedNode",
