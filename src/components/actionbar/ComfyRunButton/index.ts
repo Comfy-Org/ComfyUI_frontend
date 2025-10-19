1f5191847a05a6f48e6fec787684c76c477cdb39
Christian Byrne <cbyrne@comfy.org>
2025-10-19 10:30:14 -0700

    make "require subscription" toggleable in build (#6144)
    ## Summary
    
    Adds build time feature flags system starting with a flag that indicates
    whether subscription is required to use the app. This is only used on
    cloud.
    
    ┆Issue is synchronized with this [Notion
    page](https://www.notion.so/PR-6144-make-require-subscription-toggleable-in-build-2916d73d3650813bb140c5e96bcce1ce)
    by [Unito](https://www.unito.io)

--- src/components/actionbar/ComfyRunButton/index.ts
+++ src/components/actionbar/ComfyRunButton/index.ts
@@ -2,6 +2,6 @@ import { defineAsyncComponent } from 'vue'
 
 import { isCloud } from '@/platform/distribution/types'
 
-export default isCloud
+export default isCloud && __BUILD_FLAGS__.REQUIRE_SUBSCRIPTION
   ? defineAsyncComponent(() => import('./CloudRunButtonWrapper.vue'))
   : defineAsyncComponent(() => import('./ComfyQueueButton.vue'))
