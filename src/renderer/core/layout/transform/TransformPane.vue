77c9a9db6b08b594bbabd591c2bc719c9740e4c9
DrJKL <DrJKL0424@gmail.com>
2025-10-22 14:39:17 -0700

    wip: Try removing the events/culling

--- src/renderer/core/layout/transform/TransformPane.vue
+++ src/renderer/core/layout/transform/TransformPane.vue
@@ -4,7 +4,7 @@
     :class="
       cn(
         'absolute inset-0 w-full h-full pointer-events-none',
-        isInteracting ? 'transform-pane--interacting' : 'will-change-auto',
+        isInteracting ? 'will-change-transform' : 'will-change-auto',
         isLOD && 'isLOD'
       )
     "
@@ -40,24 +40,13 @@ const { isTransforming: isInteracting } = useTransformSettling(canvasElement, {
   settleDelay: 512
 })
 
-const emit = defineEmits<{
-  transformUpdate: []
-}>()
-
 useRafFn(
   () => {
     if (!props.canvas) {
       return
     }
     syncWithCanvas(props.canvas)
-    emit('transformUpdate')
   },
   { immediate: true }
 )
 </script>
-
-<style scoped>
-.transform-pane--interacting {
-  will-change: transform;
-}
-</style>
