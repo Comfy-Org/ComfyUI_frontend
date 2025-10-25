aedac8f35bd38128e908691cb6a43bfbca3091a8
snomiao <snomiao@gmail.com>
2025-10-25 13:37:07 +0000

    [bugfix] Use main branch screenshots for E2E tests
    Fixes #6197

--- src/utils/searchAndReplace.ts
+++ src/utils/searchAndReplace.ts
@@ -47,7 +47,7 @@ export function applyTextReplacements(
       console.warn('Unable to find widget', split[1], 'on node', split[0], node)
       return match
     }
-
+    // eslint-disable-next-line no-control-regex
     return ((widget.value ?? '') + '').replaceAll(
       /[/?<>\\:*|"\x00-\x1F\x7F]/g,
       '_'
