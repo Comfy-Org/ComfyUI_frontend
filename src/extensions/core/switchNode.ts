d29264823ff84219c57a65e04efd8e7f67c25d8c
Austin Mroz <austin@comfy.org>
2025-11-04 09:25:12 -0800

    WIP link cleanup

--- src/extensions/core/switchNode.ts
+++ src/extensions/core/switchNode.ts
@@ -33,43 +33,44 @@ app.registerExtension({
         // This node is purely frontend and does not impact the resulting prompt so should not be serialized
         this.isVirtualNode = true
       }
-      changeType(new_type: ISlotType) {
+      combinedType(newType: ISlotType, slot: number): ISlotType | undefined {
+        const otherType = this.inputs[slot === 0 ? 1 : 0].type
+        if (typeof newType !== 'string' || typeof otherType !== 'string')
+          return undefined
+        if (newType === '*') return otherType
+        if (otherType === '*') return newType
+        const newSet = new Set(newType.split(','))
+        // @ts-expect-error intersection doesn't exist?
+        const combinedSet = newSet.intersection(new Set(otherType.split(',')))
+        if (combinedSet.size == 0) return undefined
+        return [...combinedSet].join(',')
+      }
+      changeType(newType: ISlotType, slot: number, combinedType: ISlotType) {
         this.linkTimeout = setTimeout(() => {
+          if (!this.graph) return
           this.linkTimeout = undefined
-          if (this.outputs[0].type != new_type) {
-            this.outputs[0].type = new_type
-            this.inputs[0].type = new_type
-            this.inputs[1].type = new_type
+          this.inputs[slot].type = newType
+          if (this.outputs[0].type != combinedType) {
+            this.outputs[0].type = combinedType
 
             //check and potentially remove links
-            if (!this.outputs[0].links) {
-              return
-            }
-            let removed_links = []
-            for (let link_id of this.outputs[0].links) {
-              let link = app.graph.links[link_id]
-              let target_node = app.graph.getNodeById(link.target_id)
-              if (!target_node) continue
-              let target_input = target_node.inputs[link.target_slot]
-              let keep = LiteGraph.isValidConnection(
-                new_type,
-                target_input.type
+            for (let link_id of this.outputs[0].links ?? []) {
+              let link = this.graph.links[link_id]
+              if (!link) continue
+              const { input, inputNode, subgraphOutput } = link.resolve(
+                this.graph
               )
-              if (!keep) {
-                link.disconnect(app.graph, 'input')
-                removed_links.push(link_id)
-              }
-              target_node.onConnectionsChange?.(
-                LiteGraph.INPUT,
-                link.target_slot,
-                keep,
-                link,
-                target_input
+              const inputType = (input ?? subgraphOutput)?.type
+              if (
+                !inputType ||
+                LiteGraph.isValidConnection(combinedType, inputType)
               )
+                continue
+              if (inputNode) inputNode.disconnectInput(link.target_slot)
+              else
+                // @ts-expect-error Does exist, but fails to cleanup output links
+                subgraphOutput?.disconnect()
             }
-            this.outputs[0].links = this.outputs[0].links.filter(
-              (v) => !removed_links.includes(v)
-            )
           }
         }, 50)
       }
@@ -79,19 +80,27 @@ app.registerExtension({
         iscon: boolean,
         linf: LLink | null | undefined
       ) {
-        if (app.configuringGraph) return
+        if (app.configuringGraph || !this.graph) return
         if (contype == LiteGraph.INPUT && (slot == 0 || slot == 1)) {
-          let new_type: ISlotType = '*'
+          let newType: ISlotType | undefined = '*'
           if (iscon && linf) {
-            const origin_node = app.graph.getNodeById(linf.origin_id)
-            if (!origin_node) return
-            new_type = origin_node.outputs[linf.origin_slot].type
+            const { output, subgraphInput } = linf.resolve(this.graph)
+            newType = (output ?? subgraphInput)?.type
+            if (!newType) return
           }
           if (this.linkTimeout) {
             clearTimeout(this.linkTimeout)
           }
-          this.changeType(new_type)
+          const combinedType = this.combinedType(newType, slot)
+          //should be blocked by onConnectInput
+          if (!combinedType) throw new Error('Invalid connection')
+          this.changeType(newType, slot, combinedType)
         }
+      }
+      override onConnectInput(targetSlot: number, type: unknown): boolean {
+        if (app.configuringGraph) return true
+        if (!(typeof type === 'string')) return false
+        return !!this.combinedType(type, targetSlot)
       }
       override getInputLink(): LLink | null {
         return super.getInputLink(this.widgets?.[0]?.value ? 0 : 1)
