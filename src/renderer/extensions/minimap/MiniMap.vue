47688fe363ac239652b95af71619aafa12935100
Christian Byrne <cbyrne@comfy.org>
2025-11-04 14:18:30 -0800

    fix minimap navigation on touch devices (#6580)
    Fixes minimap navigation (dragging the viewport box on the minimap) on
    touch devices.
    
    ┆Issue is synchronized with this [Notion
    page](https://www.notion.so/PR-6580-fix-minimap-navigation-on-touch-devices-2a16d73d36508195b070da2b8e4b908a)
    by [Unito](https://www.unito.io)
    
    ---------
    
    Co-authored-by: Alexander Brown <drjkl@comfy.org>

--- src/renderer/extensions/minimap/MiniMap.vue
+++ src/renderer/extensions/minimap/MiniMap.vue
@@ -61,11 +61,12 @@
       <div class="minimap-viewport" :style="viewportStyles" />
 
       <div
-        class="absolute inset-0"
+        class="absolute inset-0 touch-none"
         @pointerdown="handlePointerDown"
         @pointermove="handlePointerMove"
         @pointerup="handlePointerUp"
         @pointerleave="handlePointerUp"
+        @pointercancel="handlePointerCancel"
         @wheel="handleWheel"
       />
     </div>
@@ -105,6 +106,7 @@ const {
   handlePointerDown,
   handlePointerMove,
   handlePointerUp,
+  handlePointerCancel,
   handleWheel,
   setMinimapRef
 } = useMinimap()
