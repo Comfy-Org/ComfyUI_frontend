32cf57c2d2a92f42266d10249a917098b9b03391
Austin Mroz <austin@comfy.org>
2025-11-04 19:27:41 -0800

    Adopt V3 schema implementation

--- src/extensions/core/switchNode.ts
+++ src/extensions/core/switchNode.ts
@@ -1,109 +1,127 @@
 import { LGraphNode, LiteGraph } from '@/lib/litegraph/src/litegraph'
 import type { LLink } from '@/lib/litegraph/src/LLink'
 import type { ISlotType } from '@/lib/litegraph/src/interfaces'
+import { useChainCallback } from '@/composables/functional/useChainCallback'
 
 import { app } from '@/scripts/app'
 
 app.registerExtension({
   name: 'Comfy.SwitchNode',
-  registerCustomNodes() {
-    class SwitchNode extends LGraphNode {
-      static override category: string | undefined
-      static defaultVisibility = false
-      linkTimeout: undefined | ReturnType<typeof setTimeout> = undefined
-
-      constructor(title?: string) {
-        super(title ?? 'switch')
-        if (!this.properties) {
-          this.properties = {}
-        }
-        this.addInput('true', '*')
-        this.addInput('false', '*')
-        this.addWidget('toggle', 'toggle', true, () => {}, {})
-        this.addOutput('output', '*')
-
-        // This node is purely frontend and does not impact the resulting prompt so should not be serialized
-        this.isVirtualNode = true
-      }
-      combinedType(newType: ISlotType, slot: number): ISlotType | undefined {
-        const otherType = this.inputs[slot].type
-        if (typeof newType !== 'string' || typeof otherType !== 'string')
-          return undefined
-        if (newType === '*') return otherType
-        if (otherType === '*') return newType
-        const newSet = new Set(newType.split(','))
-        const combinedSet = newSet.intersection(new Set(otherType.split(',')))
-        if (combinedSet.size == 0) return undefined
-        return [...combinedSet].join(',')
-      }
-      changeOutputType(combinedType: ISlotType) {
-        this.linkTimeout = setTimeout(() => {
-          if (!this.graph) return
-          this.linkTimeout = undefined
-          if (this.outputs[0].type != combinedType) {
-            this.outputs[0].type = combinedType
-
-            //check and potentially remove links
-            for (let link_id of this.outputs[0].links ?? []) {
-              let link = this.graph.links[link_id]
-              if (!link) continue
-              const { input, inputNode, subgraphOutput } = link.resolve(
-                this.graph
-              )
-              const inputType = (input ?? subgraphOutput)?.type
-              if (!inputType) continue
-              const keep = LiteGraph.isValidConnection(combinedType, inputType)
-              if (!keep && subgraphOutput)
-                // @ts-expect-error exists, but bad. need to fix
-                subgraphOutput.disconnect()
-              else if (!keep && inputNode)
-                inputNode.disconnectInput(link.target_slot)
-              if (input && inputNode?.onConnectionsChange)
-                inputNode.onConnectionsChange(
-                  LiteGraph.INPUT,
-                  link.target_slot,
-                  keep,
-                  link,
-                  input
-                )
-            }
-            app.canvas.setDirty(true, true)
-          }
-        }, 50)
-      }
-      override onConnectionsChange(
-        contype: ISlotType,
-        slot: number,
-        iscon: boolean,
-        linf: LLink | null | undefined
-      ) {
-        if (contype == LiteGraph.INPUT && (slot == 0 || slot == 1)) {
-          if (!this.graph) return
-          let newType: ISlotType | undefined = '*'
-          if (iscon && linf) {
-            const { output, subgraphInput } = linf.resolve(this.graph)
-            newType = (output ?? subgraphInput)?.type
-            if (!newType) return
-          }
-          if (this.linkTimeout) {
-            clearTimeout(this.linkTimeout)
-          }
-          const combinedType = this.combinedType(newType, slot)
-          //should be blocked by onConnectInput
-          if (!combinedType) throw new Error('Invalid connection')
-          this.inputs[slot ? 0 : 1].type = newType
-          this.changeOutputType(combinedType)
-        }
-      }
-      override getInputLink(): LLink | null {
-        return super.getInputLink(this.widgets?.[0]?.value ? 0 : 1)
-      }
+  beforeRegisterNodeDef(nodeType, nodeData) {
+    const inputs = {
+      ...nodeData.input?.required,
+      ...nodeData.input?.optional
     }
-    LiteGraph.registerNodeType(
-      'Switch',
-      Object.assign(SwitchNode, {
-        title: 'Switch'
-      })
+    if (!Object.values(inputs).some((w) => w[0] === 'COMFY_MATCHTYPE_V3'))
+      return
+    nodeType.prototype.onNodeCreated = useChainCallback(
+      nodeType.prototype.onNodeCreated,
+      function (this: LGraphNode) {
+        const connectionGroups: Record<string, [string, ISlotType][]> = {}
+        for (const input of this.inputs) {
+          if (input.type !== 'COMFY_MATCHTYPE_V3') continue
+          // @ts-expect-error type not yet defined
+          const { template = { tempalte_id: 'switch', allowed_types: '*' } } =
+            inputs[input.name][1]
+          input.type = template.allowed_types
+          connectionGroups[template.template_id] ??= []
+          connectionGroups[template.template_id].push([input.name, input.type])
+        }
+        for (const connectionGroup of Object.values(connectionGroups)) {
+          addConnectionGroup(this, connectionGroup)
+        }
+      }
     )
   }
 })
+function addConnectionGroup(node: LGraphNode, slots: [string, ISlotType][]) {
+  node.onConnectionsChange = useChainCallback(
+    node.onConnectionsChange,
+    function (
+      this: LGraphNode,
+      contype: ISlotType,
+      slot: number,
+      iscon: boolean,
+      linf: LLink | null | undefined
+    ) {
+      const input = this.inputs[slot]
+      if (contype !== LiteGraph.INPUT || !this.graph || !input) return
+      const slotPair = slots.find(([name]) => name === input.name)
+      if (!slotPair) return
+      //TODO: Generalize for >2 inputs
+      let newType: ISlotType | undefined = slotPair[1]
+      if (iscon && linf) {
+        const { output, subgraphInput } = linf.resolve(this.graph)
+        newType = (output ?? subgraphInput)?.type
+        if (!newType) return
+      }
+      const combinedType = combineTypes(newType, input.type)
+      //should be blocked by onConnectInput
+      if (!combinedType) throw new Error('Invalid connection')
+      //restrict the type of all OTHER inputs to combinedType
+      for (const [name] of slots) {
+        if (name == input.name) continue
+        const inp = this.inputs.find((i) => i.name === name)
+        if (!inp) continue
+        inp.type = newType
+      }
+      changeOutputType(this, combinedType)
+    }
+  )
+}
+
+function changeOutputType(node: LGraphNode, combinedType: ISlotType) {
+  // @ts-expect-error linkTimeout doesn't exist
+  if (node.linkTimeout) {
+    // @ts-expect-error linkTimeout doesn't exist
+    clearTimeout(node.linkTimeout)
+  }
+  // @ts-expect-error linkTimeout doesn't exist
+  node.linkTimeout = setTimeout(() => {
+    if (!node.graph) return
+    // @ts-expect-error linkTimeout doesn't exist
+    node.linkTimeout = undefined
+    if (node.outputs[0].type != combinedType) {
+      node.outputs[0].type = combinedType
+
+      //check and potentially remove links
+      for (let link_id of node.outputs[0].links ?? []) {
+        let link = node.graph.links[link_id]
+        if (!link) continue
+        const { input, inputNode, subgraphOutput } = link.resolve(node.graph)
+        const inputType = (input ?? subgraphOutput)?.type
+        if (!inputType) continue
+        const keep = LiteGraph.isValidConnection(combinedType, inputType)
+        if (!keep && subgraphOutput)
+          // @ts-expect-error exists, but bad. need to fix
+          subgraphOutput.disconnect()
+        else if (!keep && inputNode) inputNode.disconnectInput(link.target_slot)
+        if (input && inputNode?.onConnectionsChange)
+          inputNode.onConnectionsChange(
+            LiteGraph.INPUT,
+            link.target_slot,
+            keep,
+            link,
+            input
+          )
+      }
+      app.canvas.setDirty(true, true)
+    }
+  }, 50)
+}
+
+function isStrings(types: ISlotType[]): types is string[] {
+  return !types.some((t) => typeof t !== 'string')
+}
+
+function combineTypes(...types: ISlotType[]): ISlotType | undefined {
+  if (!isStrings(types)) return undefined
+  const filteredTypes = types.filter((t) => t !== '*')
+  if (!filteredTypes.length) return '*'
+  const combinedSet = filteredTypes.reduce(
+    (combined, partial) => combined.intersection(new Set(partial.split(','))),
+    new Set(filteredTypes[0].split(','))
+  )
+  if (combinedSet.size == 0) return undefined
+  return [...combinedSet].join(',')
+}
