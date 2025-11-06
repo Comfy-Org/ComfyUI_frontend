93b66efe052aab3d20869624f9ffdb813b2f8a90
bymyself <cbyrne@comfy.org>
2025-11-06 15:03:58 -0700

    add sync devtools to globalsetup

--- browser_tests/globalSetup.ts
+++ browser_tests/globalSetup.ts
@@ -1,17 +1,20 @@
 import type { FullConfig } from '@playwright/test'
-import dotenv from 'dotenv'
+import { config as loadEnv } from 'dotenv'
 
 import { backupPath } from './utils/backupUtils'
+import { syncDevtools } from './utils/devtoolsSync'
 
-dotenv.config()
+loadEnv()
 
-export default function globalSetup(config: FullConfig) {
+export default function globalSetup(_: FullConfig) {
   if (!process.env.CI) {
     if (process.env.TEST_COMFYUI_DIR) {
       backupPath([process.env.TEST_COMFYUI_DIR, 'user'])
       backupPath([process.env.TEST_COMFYUI_DIR, 'models'], {
         renameAndReplaceWithScaffolding: true
       })
+
+      syncDevtools(process.env.TEST_COMFYUI_DIR)
     } else {
       console.warn(
         'Set TEST_COMFYUI_DIR in .env to prevent user data (settings, workflows, etc.) from being overwritten'
