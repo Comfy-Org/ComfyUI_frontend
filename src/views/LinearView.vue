e6d734da5978587566c02e922f8e135c9fd38df0
Austin Mroz <austin@comfy.org>
2025-11-18 18:04:07 -0800

    Fix right side panel sizing.
    Run button is always on screen, text boxes scale up to a reasonable max

--- src/views/LinearView.vue
+++ src/views/LinearView.vue
@@ -149,9 +149,11 @@ async function runButtonClick(e: Event) {
           <LoginButton v-else-if="isDesktop" />
         </div>
         <div
-          class="overflow-y-auto rounded-lg border p-2 gap-2 h-full border-[var(--interface-stroke)] bg-comfy-menu-bg flex flex-col"
+          class="rounded-lg border p-2 gap-2 h-full border-[var(--interface-stroke)] bg-comfy-menu-bg flex flex-col"
         >
-          <div class="grow-1 flex justify-start flex-col">
+          <div
+            class="grow-1 flex justify-start flex-col overflow-y-auto contain-size *:max-h-100"
+          >
             <NodeWidgets
               v-for="nodeData of nodeDatas"
               :key="nodeData.id"
