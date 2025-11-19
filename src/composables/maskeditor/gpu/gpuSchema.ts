6c7bdd97262de868f7988229c7c0d58c8149fc95
trsommer <tristan@bundd.de>
2025-11-19 05:03:21 +0100

    removed unused exports

--- src/composables/maskeditor/gpu/gpuSchema.ts
+++ src/composables/maskeditor/gpu/gpuSchema.ts
@@ -14,12 +14,3 @@ export const StrokePoint = d.struct({
   size: d.location(1, d.f32), // Diameter
   pressure: d.location(2, d.f32) // 0.0 - 1.0
 })
-
-// 3. Vertex Shader Output
-export const VertexOutput = d.struct({
-  position: d.builtin.position,
-  localUV: d.location(0, d.vec2f),
-  color: d.location(1, d.vec3f),
-  opacity: d.location(2, d.f32),
-  hardness: d.location(3, d.f32)
-})
