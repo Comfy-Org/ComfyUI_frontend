0cc4b03902dd4b3cf0ea69a0aee3739060831288
DrJKL <DrJKL0424@gmail.com>
2025-11-26 14:35:44 -0800

    style: non-functional buttons and menu

--- src/platform/assets/components/AssetCard.vue
+++ src/platform/assets/components/AssetCard.vue
@@ -1,30 +1,62 @@
 <template>
-  <component
-    :is="interactive ? 'button' : 'div'"
+  <div
     data-component-id="AssetCard"
     :data-asset-id="asset.id"
-    v-bind="elementProps"
+    :aria-labelledby="titleId"
+    :aria-describedby="descId"
+    tabindex="1"
     :class="
       cn(
         'rounded-2xl overflow-hidden transition-all duration-200 bg-modal-card-background p-2',
         interactive &&
-          'group appearance-none bg-transparent m-0 outline-none cursor-pointer text-left hover:bg-secondary-background focus:bg-secondary-background border-none focus:outline-solid outline-base-foreground outline-4'
+          'group appearance-none bg-transparent m-0 outline-none text-left hover:bg-secondary-background focus:bg-secondary-background border-none focus:outline-solid outline-base-foreground outline-4'
       )
     "
-    @click="interactive && $emit('select', asset)"
     @keydown.enter="interactive && $emit('select', asset)"
   >
     <div class="relative aspect-square w-full overflow-hidden rounded-xl">
-      <img
-        v-if="shouldShowImage"
-        :src="asset.preview_url"
-        class="size-full object-contain"
-      />
       <div
+        v-if="isLoading || error"
+        class="flex size-full cursor-pointer items-center justify-center bg-gradient-to-br from-smoke-400 via-smoke-800 to-charcoal-400"
+        role="button"
+        @click="interactive && $emit('select', asset)"
+      />
+      <img
         v-else
-        class="flex size-full items-center justify-center bg-gradient-to-br from-smoke-400 via-smoke-800 to-charcoal-400"
-      ></div>
+        :src="asset.preview_url"
+        class="size-full object-contain cursor-pointer"
+        role="button"
+        @click="interactive && $emit('select', asset)"
+      />
+
       <AssetBadgeGroup :badges="asset.badges" />
+      <IconGroup
+        ref="card-buttons"
+        :class="
+          cn(
+            'absolute top-2 right-2 invisible group-hover:visible',
+            dropdownMenuButton?.isOpen && 'visible'
+          )
+        "
+      >
+        <IconButton>
+          <i class="icon-[lucide--file-text]" />
+        </IconButton>
+        <MoreButton ref="dropdown-menu-button" size="sm">
+          <template #default="{}">
+            <IconTextButton :label="$t('g.rename')" type="secondary" size="md">
+              <template #icon>
+                <i class="icon-[lucide--pencil]" />
+              </template>
+            </IconTextButton>
+            <IconTextButton :label="$t('g.delete')" type="secondary" size="md">
+              <template #icon>
+                <i class="icon-[lucide--trash-2]" />
+              </template>
+            </IconTextButton>
+          </template>
+        </MoreButton>
+      </IconGroup>
     </div>
     <div :class="cn('p-4 h-32 flex flex-col justify-between')">
       <div>
@@ -68,13 +100,17 @@
         </span>
       </div>
     </div>
-  </component>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useImage } from '@vueuse/core'
-import { computed, useId } from 'vue'
+import { computed, useId, useTemplateRef } from 'vue'
 
+import IconButton from '@/components/button/IconButton.vue'
+import IconGroup from '@/components/button/IconGroup.vue'
+import IconTextButton from '@/components/button/IconTextButton.vue'
+import MoreButton from '@/components/button/MoreButton.vue'
 import AssetBadgeGroup from '@/platform/assets/components/AssetBadgeGroup.vue'
 import type { AssetDisplayItem } from '@/platform/assets/composables/useAssetBrowser'
 import { useSettingStore } from '@/platform/settings/settingStore'
@@ -91,6 +127,10 @@ defineEmits<{
 
 const settingStore = useSettingStore()
 
+const dropdownMenuButton = useTemplateRef<typeof MoreButton>(
+  'dropdown-menu-button'
+)
+
 const titleId = useId()
 const descId = useId()
 
@@ -98,23 +138,8 @@ const tooltipDelay = computed<number>(() =>
   settingStore.get('LiteGraph.Node.TooltipDelay')
 )
 
-const { error } = useImage({
+const { isLoading, error } = useImage({
   src: asset.preview_url ?? '',
   alt: asset.name
 })
-
-const shouldShowImage = computed(() => asset.preview_url && !error.value)
-
-const elementProps = computed(() =>
-  interactive
-    ? {
-        type: 'button',
-        'aria-labelledby': titleId,
-        'aria-describedby': descId
-      }
-    : {
-        'aria-labelledby': titleId,
-        'aria-describedby': descId
-      }
-)
 </script>
