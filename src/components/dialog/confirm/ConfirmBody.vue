287fc2b203a757a23e0ce756ca93735eda6d9c89
DrJKL <DrJKL0424@gmail.com>
2025-11-26 18:13:55 -0800

    WIP: Deleting assets, UI only, also not wired up

---
+++ src/components/dialog/confirm/ConfirmBody.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="flex flex-col px-4 py-2 text-sm text-muted-foreground border-t border-border-default"
+  >
+    <p v-if="confirmationText">
+      {{ confirmationText }}
+    </p>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps<{
+  confirmationText?: string
+}>()
+</script>
