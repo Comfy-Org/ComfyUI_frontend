a5bd37ef56044c9e8b610970af8ba85686350606
DrJKL <DrJKL0424@gmail.com>
2025-12-01 12:00:00 -0800

    Properly resolve MaybeRef

--- src/components/dialog/confirm/ConfirmBody.vue
+++ src/components/dialog/confirm/ConfirmBody.vue
@@ -2,15 +2,18 @@
   <div
     class="flex flex-col px-4 py-2 text-sm text-muted-foreground border-t border-border-default"
   >
-    <p v-if="promptText">
-      {{ promptText }}
+    <p v-if="promptTextReal">
+      {{ promptTextReal }}
     </p>
   </div>
 </template>
 <script setup lang="ts">
+import { computed, toValue } from 'vue'
 import type { MaybeRefOrGetter } from 'vue'
 
-defineProps<{
+const { promptText } = defineProps<{
   promptText?: MaybeRefOrGetter<string>
 }>()
+
+const promptTextReal = computed(() => toValue(promptText))
 </script>
