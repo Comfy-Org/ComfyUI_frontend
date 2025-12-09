d3e9e15f07be99b794a114514fbbb26e4dd3955d
Christian Byrne <cbyrne@comfy.org>
2025-12-09 01:41:32 -0800

    change credits icons and tooltips (conditional on feature flag) (#7276)
    This PR changes the credits icons and tooltips based on state of the
    `subscription_tiers_enabled` feature flag.
    
    When the flag is enabled (or undefined -- for local), the dollar icon is
    replaced with the lucide-component icon in UserCredit and node price
    badges (Partner Nodes), and a new tooltip row appears in
    CurrentUserPopover displaying "Credits have been unified" with a
    detailed hover tooltip explaining the credit unification across Partner
    Nodes and Cloud workflows.
    
    <img width="539" height="535" alt="image"
    src="https://github.com/user-attachments/assets/7e952f9b-0abb-4979-85b7-0eecdeaf808c"
    />
    
    Related:
    
    - https://github.com/Comfy-Org/ComfyUI_frontend/pull/6115 (borrows badge
    implementation from this PR)
    
    ┆Issue is synchronized with this [Notion
    page](https://www.notion.so/PR-7276-change-credits-icons-and-tooltips-conditional-on-feature-flag-2c46d73d365081809a6afd5861018a15)
    by [Unito](https://www.unito.io)

--- src/lib/litegraph/src/LGraphIcon.ts
+++ src/lib/litegraph/src/LGraphIcon.ts
@@ -1,20 +1,24 @@
 export interface LGraphIconOptions {
-  unicode: string
+  unicode?: string
   fontFamily?: string
+  image?: HTMLImageElement
   color?: string
   bgColor?: string
   fontSize?: number
+  size?: number
   circlePadding?: number
   xOffset?: number
   yOffset?: number
 }
 
 export class LGraphIcon {
-  unicode: string
+  unicode?: string
   fontFamily: string
+  image?: HTMLImageElement
   color: string
   bgColor?: string
   fontSize: number
+  size: number
   circlePadding: number
   xOffset: number
   yOffset: number
@@ -22,18 +26,22 @@ export class LGraphIcon {
   constructor({
     unicode,
     fontFamily = 'PrimeIcons',
+    image,
     color = '#e6c200',
     bgColor,
     fontSize = 16,
+    size,
     circlePadding = 2,
     xOffset = 0,
     yOffset = 0
   }: LGraphIconOptions) {
     this.unicode = unicode
     this.fontFamily = fontFamily
+    this.image = image
     this.color = color
     this.bgColor = bgColor
     this.fontSize = fontSize
+    this.size = size ?? fontSize
     this.circlePadding = circlePadding
     this.xOffset = xOffset
     this.yOffset = yOffset
@@ -43,26 +51,44 @@ export class LGraphIcon {
     x += this.xOffset
     y += this.yOffset
 
-    const { font, textBaseline, textAlign, fillStyle } = ctx
+    if (this.image) {
+      const iconSize = this.size
+      const iconRadius = iconSize / 2 + this.circlePadding
 
-    ctx.font = `${this.fontSize}px '${this.fontFamily}'`
-    ctx.textBaseline = 'middle'
-    ctx.textAlign = 'center'
-    const iconRadius = this.fontSize / 2 + this.circlePadding
-    // Draw icon background circle if bgColor is set
-    if (this.bgColor) {
-      ctx.beginPath()
-      ctx.arc(x + iconRadius, y, iconRadius, 0, 2 * Math.PI)
-      ctx.fillStyle = this.bgColor
-      ctx.fill()
+      if (this.bgColor) {
+        const { fillStyle } = ctx
+        ctx.beginPath()
+        ctx.arc(x + iconRadius, y, iconRadius, 0, 2 * Math.PI)
+        ctx.fillStyle = this.bgColor
+        ctx.fill()
+        ctx.fillStyle = fillStyle
+      }
+
+      const imageX = x + this.circlePadding
+      const imageY = y - iconSize / 2
+      ctx.drawImage(this.image, imageX, imageY, iconSize, iconSize)
+    } else if (this.unicode) {
+      const { font, textBaseline, textAlign, fillStyle } = ctx
+
+      ctx.font = `${this.fontSize}px '${this.fontFamily}'`
+      ctx.textBaseline = 'middle'
+      ctx.textAlign = 'center'
+      const iconRadius = this.fontSize / 2 + this.circlePadding
+
+      if (this.bgColor) {
+        ctx.beginPath()
+        ctx.arc(x + iconRadius, y, iconRadius, 0, 2 * Math.PI)
+        ctx.fillStyle = this.bgColor
+        ctx.fill()
+      }
+
+      ctx.fillStyle = this.color
+      ctx.fillText(this.unicode, x + iconRadius, y)
+
+      ctx.font = font
+      ctx.textBaseline = textBaseline
+      ctx.textAlign = textAlign
+      ctx.fillStyle = fillStyle
     }
-    // Draw icon
-    ctx.fillStyle = this.color
-    ctx.fillText(this.unicode, x + iconRadius, y)
-
-    ctx.font = font
-    ctx.textBaseline = textBaseline
-    ctx.textAlign = textAlign
-    ctx.fillStyle = fillStyle
   }
 }
