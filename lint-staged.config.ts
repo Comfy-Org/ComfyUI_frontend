63221b1cd5d5f10bff23abdf7695577488f68b48
DrJKL <DrJKL0424@gmail.com>
2025-12-10 15:56:36 -0800

    Typescriptify lint-staged config

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,15 +1,15 @@
 import path from 'node:path'
 
 export default {
-  './**/*.js': (stagedFiles) => formatAndEslint(stagedFiles),
+  './**/*.js': (stagedFiles: string[]) => formatAndEslint(stagedFiles),
 
-  './**/*.{ts,tsx,vue,mts}': (stagedFiles) => [
+  './**/*.{ts,tsx,vue,mts}': (stagedFiles: string[]) => [
     ...formatAndEslint(stagedFiles),
     'pnpm typecheck'
   ]
 }
 
-function formatAndEslint(fileNames) {
+function formatAndEslint(fileNames: string[]) {
   // Convert absolute paths to relative paths for better ESLint resolution
   const relativePaths = fileNames.map((f) => path.relative(process.cwd(), f))
   const joinedPaths = relativePaths.map((p) => `"${p}"`).join(' ')
