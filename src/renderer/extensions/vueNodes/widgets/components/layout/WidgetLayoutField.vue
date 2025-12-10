b990033070482f7ad558ade2badac063c78db71b
Austin Mroz <austin@comfy.org>
2025-12-10 13:42:00 -0800

    Add slight padding between label and border

--- src/renderer/extensions/vueNodes/widgets/components/layout/WidgetLayoutField.vue
+++ src/renderer/extensions/vueNodes/widgets/components/layout/WidgetLayoutField.vue
@@ -13,7 +13,7 @@ const hideLayoutField = inject<boolean>('hideLayoutField', false)
 
 <template>
   <div
-    class="grid grid-cols-subgrid grid-rows-[30px] min-w-0 items-center justify-between gap-1"
+    class="grid grid-cols-subgrid grid-rows-[30px] min-w-0 items-center justify-between gap-1 -ml-1 pl-1"
   >
     <div
       v-if="!hideLayoutField"
