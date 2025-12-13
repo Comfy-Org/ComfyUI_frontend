076acf1b31ee4331d5032b708cf56917fe9557ae
bymyself <cbyrne@comfy.org>
2025-12-13 03:32:48 -0800

    graph state store impl

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -47,12 +47,14 @@ import { useSubscription } from '@/platform/cloud/subscription/composables/useSu
 import { useExtensionService } from '@/services/extensionService'
 import { useLitegraphService } from '@/services/litegraphService'
 import { useSubgraphService } from '@/services/subgraphService'
+import { useGraphErrorState } from '@/core/graph/state/useGraphErrorState'
 import { useApiKeyAuthStore } from '@/stores/apiKeyAuthStore'
 import { useCommandStore } from '@/stores/commandStore'
 import { useDomWidgetStore } from '@/stores/domWidgetStore'
 import { useExecutionStore } from '@/stores/executionStore'
 import { useExtensionStore } from '@/stores/extensionStore'
 import { useFirebaseAuthStore } from '@/stores/firebaseAuthStore'
+import { useGraphStateStore } from '@/core/graph/state/graphStateStore'
 import { useNodeOutputStore } from '@/stores/imagePreviewStore'
 import { KeyComboImpl, useKeybindingStore } from '@/stores/keybindingStore'
 import { useModelStore } from '@/stores/modelStore'
@@ -78,6 +80,7 @@ import {
   findLegacyRerouteNodes,
   noNativeReroutes
 } from '@/utils/migration/migrateReroute'
+import { collectMissingNodes } from '@/workbench/extensions/manager/utils/graphHasMissingNodes'
 import { getSelectedModelsMetadata } from '@/workbench/utils/modelMetadataUtil'
 import { deserialiseAndCreate } from '@/utils/vintageClipboard'
 
@@ -764,6 +767,8 @@ export class ComfyApp {
     void useSubgraphStore().fetchSubgraphs()
     await useExtensionService().loadExtensions()
 
+    useGraphErrorState()
+
     this.addProcessKeyHandler()
     this.addConfigureHandler()
     this.addApiUpdateHandlers()
@@ -1230,6 +1235,23 @@ export class ComfyApp {
             useLitegraphService().fitView()
           })
         }
+      }
+
+      const graphStateStore = useGraphStateStore()
+      const missingNodes = collectMissingNodes(
+        this.rootGraph,
+        useNodeDefStore().nodeDefsByName
+      )
+      for (const node of missingNodes) {
+        const locatorId = node.graph?.isRootGraph
+          ? String(node.id)
+          : `${node.graph?.id}:${node.id}`
+        graphStateStore.execute({
+          type: 'SetNodeError',
+          version: 1,
+          nodeId: locatorId,
+          hasError: true
+        })
       }
     } catch (error) {
       useDialogService().showErrorDialog(error, {
