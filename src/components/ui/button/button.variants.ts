d9bde9becfd8dd0d9811df69ed77d28753e00685
DrJKL <DrJKL0424@gmail.com>
2025-12-15 13:58:12 -0800

    style: Align variants with Figma designs (excluding outlined)

--- src/components/ui/button/button.variants.ts
+++ src/components/ui/button/button.variants.ts
@@ -5,29 +5,27 @@ export const buttonVariants = cva({
   base: 'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   variants: {
     variant: {
-      default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
-      destructive:
-        'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
-      outline:
-        'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
       secondary:
-        'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
-      ghost: 'hover:bg-accent hover:text-accent-foreground',
-      link: 'text-primary underline-offset-4 hover:underline'
+        'bg-secondary-background text-secondary-foreground hover:bg-secondary-background-hover',
+      primary:
+        'bg-primary-background text-base-foreground hover:bg-primary-background-hover',
+      inverted:
+        'bg-base-foreground text-base-background hover:bg-base-foreground/80',
+      destructive:
+        'bg-destructive-background text-base-foreground hover:bg-destructive-background-hover',
+      'destructive-subtle':
+        'bg-secondary-background text-base-foreground hover:bg-destructive-background-hover'
     },
     size: {
-      default: 'h-9 px-4 py-2',
-      xs: 'h-7 rounded px-2',
-      sm: 'h-8 rounded-md px-3 text-xs',
-      lg: 'h-10 rounded-md px-8',
-      icon: 'h-9 w-9',
-      'icon-sm': 'size-8',
-      'icon-lg': 'size-10'
+      sm: 'h-6 rounded-sm px-2 py-1 text-xs',
+      md: 'h-8 rounded-lg p-2 text-xs',
+      lg: 'h-10 rounded-lg px-4 py-2 text-sm',
+      icon: 'size-9'
     }
   },
   defaultVariants: {
-    variant: 'default',
-    size: 'default'
+    variant: 'secondary',
+    size: 'md'
   }
 })
 
