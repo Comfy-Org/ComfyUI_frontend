517fade36947aab56bc9ff47ef2686a39b5a76a9
DrJKL <DrJKL0424@gmail.com>
2025-12-16 10:19:46 -0800

    fix: Still need to figure out why the body font doesn't apply sometimes.

--- src/components/ui/button/button.variants.ts
+++ src/components/ui/button/button.variants.ts
@@ -2,7 +2,7 @@ import type { VariantProps } from 'cva'
 import { cva } from 'cva'
 
 export const buttonVariants = cva({
-  base: 'inline-flex items-center justify-center gap-2 whitespace-nowrap appearance-none border-none rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  base: 'inline-flex items-center justify-center gap-2 cursor-pointer whitespace-nowrap appearance-none border-none rounded-md text-sm font-medium font-inter transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   variants: {
     variant: {
       unset: 'text-base-foreground hover:bg-secondary-background-hover',
