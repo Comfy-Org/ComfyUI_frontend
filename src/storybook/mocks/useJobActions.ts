d8e57c60bfa609aa95ff94b746864ef54aa44e3a
Benjamin Lu <benjaminlu1107@gmail.com>
2025-12-23 13:57:12 -0800

    Add stories for list view and general job card

---
+++ src/storybook/mocks/useJobActions.ts
@@ -0,0 +1,25 @@
+import { ref } from 'vue'
+
+import type { JobAction } from '../../composables/queue/useJobActions'
+import type { JobListItem } from '../../composables/queue/useJobList'
+
+const actionsByJobId = ref<Record<string, JobAction[]>>({})
+
+export function setMockJobActions(actions: Record<string, JobAction[]>) {
+  actionsByJobId.value = actions
+}
+
+export function useJobActions() {
+  function getJobActions(job: JobListItem) {
+    return actionsByJobId.value[job.id] ?? []
+  }
+
+  async function runJobAction() {
+    return undefined
+  }
+
+  return {
+    getJobActions,
+    runJobAction
+  }
+}
