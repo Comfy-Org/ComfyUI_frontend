f2980b19ccaa5818e3e815d20f134b4ffc95f2df
Austin Mroz <austin@comfy.org>
2025-12-30 14:01:56 -0800

    Test nits

--- tests-ui/tests/composables/graph/useGraphNodeManager.test.ts
+++ tests-ui/tests/composables/graph/useGraphNodeManager.test.ts
@@ -9,37 +9,38 @@ import { NodeSlotType } from '@/lib/litegraph/src/types/globalEnums'
 
 setActivePinia(createTestingPinia())
 
+function createTestGraph() {
+  const graph = new LGraph()
+  const node = new LGraphNode('test')
+  node.addInput('input', 'INT')
+  node.addWidget('number', 'testnum', 2, () => undefined, {})
+  graph.add(node)
+
+  const { vueNodeData } = useGraphNodeManager(graph)
+  const onReactivityUpdate = vi.fn()
+  watch(vueNodeData, onReactivityUpdate)
+
+  return [node, graph, onReactivityUpdate] as const
+}
+
 describe('Node Reactivity', () => {
   it('should trigger on callback', async () => {
-    const graph = new LGraph()
-    const node = new LGraphNode('test')
-    node.addInput('input', 'INT')
-    node.addWidget('number', 'testnum', 2, () => undefined, {})
-    graph.add(node)
-
-    const { vueNodeData } = useGraphNodeManager(graph)
-    const onReactivityUpdate = vi.fn()
-    watch(vueNodeData, onReactivityUpdate)
-    expect(onReactivityUpdate).toHaveBeenCalledTimes(0)
+    const [node, , onReactivityUpdate] = createTestGraph()
 
     node.widgets![0].callback!(2)
     await nextTick()
     expect(onReactivityUpdate).toHaveBeenCalledTimes(1)
   })
-  it('should remain reactive after a connection is made', async () => {
-    const graph = new LGraph()
-    const node = new LGraphNode('test')
-    node.addInput('input', 'INT')
-    node.addWidget('number', 'testnum', 2, () => undefined, {})
-    graph.add(node)
 
-    const { vueNodeData } = useGraphNodeManager(graph)
-    const onReactivityUpdate = vi.fn()
+  it('should remain reactive after a connection is made', async () => {
+    const [node, graph, onReactivityUpdate] = createTestGraph()
+
     graph.trigger('node:slot-links:changed', {
       nodeId: '1',
       slotType: NodeSlotType.INPUT
     })
-    watch(vueNodeData, onReactivityUpdate)
+    await nextTick()
+    onReactivityUpdate.mockClear()
 
     node.widgets![0].callback!(2)
     await nextTick()
