a780efb5ff91e5c118fe785b98191d72b46f69b8
Austin Mroz <austin@comfy.org>
2025-12-31 17:56:14 -0800

    Add linear indicator to workflow tab

--- src/components/topbar/WorkflowTab.vue
+++ src/components/topbar/WorkflowTab.vue
@@ -7,6 +7,10 @@
     @mouseleave="handleMouseLeave"
     @click="handleClick"
   >
+    <i
+      v-if="workflowOption.workflow.activeState?.extra?.linearMode"
+      class="icon-[lucide--panels-top-left] bg-azure-600"
+    />
     <span class="workflow-label inline-block max-w-[150px] truncate text-sm">
       {{ workflowOption.workflow.filename }}
     </span>
