e968078edef021fc57bb01e3f95b688f6d15bc80
Brian Jemilo II <jemiloii@gmail.com>
2026-01-07 00:32:47 -0600

    Dragging non-workflow image will create a load image node

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -96,6 +96,7 @@ import { type ComfyWidgetConstructor } from './widgets'
 import { ensureCorrectLayoutScale } from '@/renderer/extensions/vueNodes/layout/ensureCorrectLayoutScale'
 import { extractFileFromDragEvent } from '@/utils/eventUtils'
 import { getWorkflowDataFromFile } from '@/scripts/metadata/parser'
+import { pasteImageNode } from '@/composables/usePaste'
 
 export const ANIM_PREVIEW_WIDGET = '$$comfy_animation_preview'
 
@@ -1439,9 +1440,15 @@ export class ComfyApp {
    */
   async handleFile(file: File, openSource?: WorkflowOpenSource) {
     const fileName = file.name.replace(/\.\w+$/, '') // Strip file extension
+    console.log('DROPPED!')
     const workflowData = await getWorkflowDataFromFile(file)
-    if (!workflowData) {
+    if (!workflowData && !file.type.startsWith('image')) {
       this.showErrorOnFileLoad(file)
+      return
+    } else if (file.type.startsWith('image')) {
+      const transfer = new DataTransfer();
+      transfer.items.add(file);
+      pasteImageNode(this.canvas, transfer.items)
       return
     }
 
