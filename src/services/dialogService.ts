1bf5b5397dd1fdc177bd8a801500e66a9cc549a7
Simula_r <18093452+simula-r@users.noreply.github.com>
2026-01-08 19:22:50 -0800

    Feat(cloud)/new top up dialog (#7899)
    ## Summary
    
    - Implement the new add credits (top up) dialog. 
    - Refactor the subscription dialog to make different credit types easier
    to understand
    
    ## Changes
    
    - **What**: TopUpCreditsDialogContent.vue, SubscriptionPanel.vue,
    /en/main.json
    - **Breaking**: <!-- Any breaking changes (if none, remove this line)
    -->
    - **Dependencies**: <!-- New dependencies (if none, remove this line)
    -->
    
    ## Review Focus
    
    <!-- Critical design decisions or edge cases that need attention -->
    
    <!-- If this PR fixes an issue, uncomment and update the line below -->
    <!-- Fixes #ISSUE_NUMBER -->
    
    
    https://github.com/user-attachments/assets/a6454651-e195-4430-bfcc-0f2a8c1dc80b
    
    Relevant notion links:
    
    https://www.notion.so/comfy-org/Implement-New-Top-Up-Dialog-with-Custom-Amount-Input-2df6d73d36508142b901fc0edb0d1fc1?source=copy_link
    
    https://www.notion.so/comfy-org/Implement-Update-confusing-credits-remaining-this-month-message-2df6d73d36508168b7e5ed46754cec60?source=copy_link

--- src/services/dialogService.ts
+++ src/services/dialogService.ts
@@ -368,7 +368,8 @@ export const useDialogService = () => {
         headless: true,
         pt: {
           header: { class: 'p-0! hidden' },
-          content: { class: 'p-0! m-0!' }
+          content: { class: 'p-0! m-0! rounded-2xl' },
+          root: { class: 'rounded-2xl' }
         }
       }
     })
