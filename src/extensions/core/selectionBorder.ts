2ac2c2f9256649757c09eced8bf9eedd6496179e
Johnpaul <polymenjohn1@gmail.com>
2026-01-12 19:08:17 +0100

    fix(extensions): type selectionBorder onDrawForeground visibleArea as Rectangle

--- src/extensions/core/selectionBorder.ts
+++ src/extensions/core/selectionBorder.ts
@@ -1,4 +1,8 @@
-import { type LGraphCanvas, createBounds } from '@/lib/litegraph/src/litegraph'
+import {
+  type LGraphCanvas,
+  Rectangle,
+  createBounds
+} from '@/lib/litegraph/src/litegraph'
 import { app } from '@/scripts/app'
 
 /**
@@ -56,7 +60,7 @@ const ext = {
 
     app.canvas.onDrawForeground = function (
       ctx: CanvasRenderingContext2D,
-      visibleArea: any
+      visibleArea: Rectangle
     ) {
       // Call original if it exists
       originalDrawForeground?.call(this, ctx, visibleArea)
