9f59573848d6f42103a2623f257b7f5835bf1e16
Alexander Brown <448862+DrJKL@users.noreply.github.com>
2026-01-17 13:06:57 -0800

    Non-muted text for editable Display Name

--- src/platform/assets/components/modelInfo/ModelInfoPanel.vue
+++ src/platform/assets/components/modelInfo/ModelInfoPanel.vue
@@ -13,7 +13,7 @@
         <EditableText
           :model-value="displayName"
           :is-editing="isEditingDisplayName"
-          class="break-all"
+          class="break-all text-base-foreground"
           @dblclick="isEditingDisplayName = !isImmutable"
           @edit="handleDisplayNameEdit"
           @cancel="isEditingDisplayName = false"
