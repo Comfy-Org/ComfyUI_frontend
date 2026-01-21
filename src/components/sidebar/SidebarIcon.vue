f7ac0aa39ed1e9b48910151972a232a107494967
Jin Yi <jin12cc@gmail.com>
2026-01-21 10:49:36 +0900

    [feat] Add queue badge to assets sidebar tab (#8170)

--- src/components/sidebar/SidebarIcon.vue
+++ src/components/sidebar/SidebarIcon.vue
@@ -17,22 +17,28 @@
   >
     <div class="side-bar-button-content">
       <slot name="icon">
-        <OverlayBadge v-if="shouldShowBadge" :value="overlayValue">
+        <div class="sidebar-icon-wrapper relative">
           <i
             v-if="typeof icon === 'string'"
             :class="icon + ' side-bar-button-icon'"
           />
-          <component :is="icon" v-else class="side-bar-button-icon" />
-        </OverlayBadge>
-        <i
-          v-else-if="typeof icon === 'string'"
-          :class="icon + ' side-bar-button-icon'"
-        />
-        <component
-          :is="icon"
-          v-else-if="typeof icon === 'object'"
-          class="side-bar-button-icon"
-        />
+          <component
+            :is="icon"
+            v-else-if="typeof icon === 'object'"
+            class="side-bar-button-icon"
+          />
+          <span
+            v-if="shouldShowBadge"
+            :class="
+              cn(
+                'sidebar-icon-badge absolute min-w-[16px] rounded-full bg-primary-background py-0.25 text-[10px] font-medium leading-[14px] text-base-foreground',
+                badgeClass || '-top-1 -right-1'
+              )
+            "
+          >
+            {{ overlayValue }}
+          </span>
+        </div>
       </slot>
       <span v-if="label && !isSmall" class="side-bar-button-label">{{
         t(label)
@@ -42,7 +48,6 @@
 </template>
 
 <script setup lang="ts">
-import OverlayBadge from 'primevue/overlaybadge'
 import { computed } from 'vue'
 import type { Component } from 'vue'
 import { useI18n } from 'vue-i18n'
@@ -57,6 +62,7 @@ const {
   tooltip = '',
   tooltipSuffix = '',
   iconBadge = '',
+  badgeClass = '',
   label = '',
   isSmall = false
 } = defineProps<{
@@ -65,6 +71,7 @@ const {
   tooltip?: string
   tooltipSuffix?: string
   iconBadge?: string | (() => string | null)
+  badgeClass?: string
   label?: string
   isSmall?: boolean
 }>()
