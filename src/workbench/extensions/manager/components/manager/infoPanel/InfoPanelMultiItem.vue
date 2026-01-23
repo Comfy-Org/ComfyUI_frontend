5d158e1487c3f28617cdcc05520e5574c3e649f9
Jin Yi <jin12cc@gmail.com>
2026-01-23 15:09:08 +0900

    manager: design improved related to infopanel & delete unused files

--- src/workbench/extensions/manager/components/manager/infoPanel/InfoPanelMultiItem.vue
+++ src/workbench/extensions/manager/components/manager/infoPanel/InfoPanelMultiItem.vue
@@ -9,7 +9,7 @@
           {{ t('manager.actions') }}
         </span>
       </template>
-      <div class="flex justify-center gap-2 px-4 py-2">
+      <div class="flex flex-col gap-1 px-4">
         <!-- Mixed: Don't show any button -->
         <div v-if="isMixed" class="text-sm text-neutral-500">
           {{ $t('manager.mixedSelectionMessage') }}
