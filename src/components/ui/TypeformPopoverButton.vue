c845308fd4d0925da4b50ede54530be1f279c24e
Austin Mroz <austin@comfy.org>
2026-01-27 23:01:10 -0800

    Make typeform button raw link on mobile.
    The popover component fails to autosize properly on mobile. I'm not been
    able to replicate this behavior with artificially tiny desktop window
    sizes, but I'm not alone in experiencing this.
    
    Rather than relying on the previous workaroound (description is link),
    the button will now functions as a simple link when on mobile

--- src/components/ui/TypeformPopoverButton.vue
+++ src/components/ui/TypeformPopoverButton.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { whenever } from '@vueuse/core'
+import { breakpointsTailwind, useBreakpoints, whenever } from '@vueuse/core'
 import { useTemplateRef } from 'vue'
 
 import Popover from '@/components/ui/Popover.vue'
@@ -10,6 +10,7 @@ defineProps<{
 }>()
 
 const feedbackRef = useTemplateRef('feedbackRef')
+const isMobile = useBreakpoints(breakpointsTailwind).smaller('md')
 
 whenever(feedbackRef, () => {
   const scriptEl = document.createElement('script')
@@ -18,7 +19,12 @@ whenever(feedbackRef, () => {
 })
 </script>
 <template>
-  <Popover>
+  <a v-if="isMobile" :href="`https://form.typeform.com/to/${dataTfWidget}`">
+    <Button variant="inverted" class="rounded-full size-12">
+      <i class="icon-[lucide--circle-question-mark] size-6" />
+    </Button>
+  </a>
+  <Popover v-else>
     <template #button>
       <Button variant="inverted" class="rounded-full size-12">
         <i class="icon-[lucide--circle-question-mark] size-6" />
