2f3612310abb2fd430530c729985e287132843c8
pythongosssss <125205205+pythongosssss@users.noreply.github.com>
2026-02-17 11:57:40 -0800

    update store

--- src/stores/appModeStore.ts
+++ src/stores/appModeStore.ts
@@ -6,12 +6,17 @@ export type AppMode = 'graph' | 'app' | 'builder:select' | 'builder:arrange'
 export const useAppModeStore = defineStore('appMode', () => {
   const mode = ref<AppMode>('graph')
   const builderSaving = ref(false)
+  const hasOutputs = ref(true)
 
   const isBuilderMode = computed(
     () => mode.value === 'builder:select' || mode.value === 'builder:arrange'
   )
-  const isAppMode = computed(() => mode.value === 'app')
-  const isGraphMode = computed(() => mode.value === 'graph')
+  const isAppMode = computed(
+    () => mode.value === 'app' || mode.value === 'builder:arrange'
+  )
+  const isGraphMode = computed(
+    () => mode.value === 'graph' || mode.value === 'builder:select'
+  )
   const isBuilderSaving = computed(
     () => builderSaving.value && isBuilderMode.value
   )
@@ -22,6 +27,11 @@ export const useAppModeStore = defineStore('appMode', () => {
     isAppMode,
     isGraphMode,
     isBuilderSaving,
+    hasOutputs,
+    setBuilderSaving: (newBuilderSaving: boolean) => {
+      if (!isBuilderMode.value) return
+      builderSaving.value = newBuilderSaving
+    },
     setMode: (newMode: AppMode) => {
       if (newMode === mode.value) return
       mode.value = newMode
