163fd0e5abf3852067d7c551d484073aabed2ed5
Austin Mroz <austin@comfy.org>
2026-03-04 16:26:35 -0800

    Fix output z-index

--- src/renderer/extensions/linearMode/LinearPreview.vue
+++ src/renderer/extensions/linearMode/LinearPreview.vue
@@ -159,7 +159,7 @@ async function rerun(e: Event) {
     />
     <OutputHistory
       v-if="!isBuilderMode"
-      class="min-w-0"
+      class="min-w-0 z-10"
       @update-selection="handleSelection"
     />
     <LinearFeedback
