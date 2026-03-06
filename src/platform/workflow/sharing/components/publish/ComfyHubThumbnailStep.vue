8c5d32885c3e12058637c2f317fe5e62c3947edb
GitHub Action <action@github.com>
2026-03-06 02:02:45 +0000

    [automated] Apply ESLint and Oxfmt fixes

--- src/platform/workflow/sharing/components/publish/ComfyHubThumbnailStep.vue
+++ src/platform/workflow/sharing/components/publish/ComfyHubThumbnailStep.vue
@@ -50,12 +50,12 @@
             <img
               :src="comparisonPreviewUrls.after!"
               :alt="$t('comfyHubPublish.uploadComparisonAfterPrompt')"
-              class="h-full w-full object-contain"
+              class="size-full object-contain"
             />
             <img
               :src="comparisonPreviewUrls.before!"
               :alt="$t('comfyHubPublish.uploadComparisonBeforePrompt')"
-              class="absolute inset-0 h-full w-full object-contain"
+              class="absolute inset-0 size-full object-contain"
               :style="{
                 clipPath: `inset(0 ${100 - previewSliderPosition}% 0 0)`
               }"
