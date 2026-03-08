98ad2a96721945a37ac60eb83f938b0325095d9a
Christian Byrne <cbyrne@comfy.org>
2026-03-07 18:19:54 -0800

    [backport core/1.41] Yet further app fixes (#9523) (#9566)
    Backport of #9523 to core/1.41.
    
    Cherry-pick of merge commit 83ffaf30 with minor conflict in
    `src/locales/en/main.json` (added new i18n keys). Resolved by accepting
    incoming keys.
    
    **Original PR:** https://github.com/Comfy-Org/ComfyUI_frontend/pull/9523
    **Pipeline ticket:** 15e1f241-efaa-4fe5-88ca-4ccc7bfb3345
    
    ┆Issue is synchronized with this [Notion
    page](https://www.notion.so/PR-9566-backport-core-1-41-Yet-further-app-fixes-9523-31d6d73d3650815c9c06eee813f0e72e)
    by [Unito](https://www.unito.io)
    
    Co-authored-by: AustinMroz <austin@comfy.org>

--- src/components/builder/AppBuilder.vue
+++ src/components/builder/AppBuilder.vue
@@ -26,7 +26,7 @@ import { app } from '@/scripts/app'
 import { DOMWidgetImpl } from '@/scripts/domWidget'
 import { promptRenameWidget } from '@/utils/widgetUtil'
 import { useAppMode } from '@/composables/useAppMode'
-import { useAppModeStore } from '@/stores/appModeStore'
+import { nodeTypeValidForApp, useAppModeStore } from '@/stores/appModeStore'
 import { resolveNode } from '@/utils/litegraphUtil'
 import { cn } from '@/utils/tailwindUtil'
 import { HideLayoutFieldKey } from '@/types/widgetTypes'
@@ -144,7 +144,11 @@ function handleDown(e: MouseEvent) {
 }
 function handleClick(e: MouseEvent) {
   const [node, widget] = getHovered(e) ?? []
-  if (node?.mode !== LGraphEventMode.ALWAYS)
+  if (
+    node?.mode !== LGraphEventMode.ALWAYS ||
+    !nodeTypeValidForApp(node.type) ||
+    node.has_errors
+  )
     return canvasInteractions.forwardEventToCanvas(e)
 
   if (!widget) {
@@ -180,7 +184,9 @@ const renderedOutputs = computed(() => {
   return canvas
     .graph!.nodes.filter(
       (n) =>
-        n.constructor.nodeData?.output_node && n.mode === LGraphEventMode.ALWAYS
+        n.constructor.nodeData?.output_node &&
+        n.mode === LGraphEventMode.ALWAYS &&
+        !n.has_errors
     )
     .map(nodeToDisplayTuple)
 })
