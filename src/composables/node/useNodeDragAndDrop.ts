9c11397474a28ebe7e3011002c4b95d6beaa6f45
Austin Mroz <austin@comfy.org>
2026-03-16 14:19:21 -0700

    Fix drag and drop from assets onto node

--- src/composables/node/useNodeDragAndDrop.ts
+++ src/composables/node/useNodeDragAndDrop.ts
@@ -21,27 +21,51 @@ export const useNodeDragAndDrop = <T>(
   const hasFiles = (items: DataTransferItemList) =>
     !!Array.from(items).find((f) => f.kind === 'file')
 
-  const filterFiles = (files: FileList) => Array.from(files).filter(fileFilter)
+  const filterFiles = (files: FileList | File[]) =>
+    Array.from(files).filter(fileFilter)
 
   const hasValidFiles = (files: FileList) => filterFiles(files).length > 0
 
   const isDraggingFiles = (e: DragEvent | undefined) => {
     if (!e?.dataTransfer?.items) return false
-    return onDragOver?.(e) ?? hasFiles(e.dataTransfer.items)
+    return (
+      onDragOver?.(e) ??
+      (hasFiles(e.dataTransfer.items) ||
+        e?.dataTransfer?.types?.includes('text/uri-list'))
+    )
   }
 
   const isDraggingValidFiles = (e: DragEvent | undefined) => {
-    if (!e?.dataTransfer?.files) return false
-    return hasValidFiles(e.dataTransfer.files)
+    if (e?.dataTransfer?.files?.length)
+      return hasValidFiles(e.dataTransfer.files)
+
+    return e?.dataTransfer?.getData('text/uri-list')
   }
 
   node.onDragOver = isDraggingFiles
 
-  node.onDragDrop = function (e: DragEvent) {
+  node.onDragDrop = async function (e: DragEvent) {
     if (!isDraggingValidFiles(e)) return false
 
     const files = filterFiles(e.dataTransfer!.files)
-    void onDrop(files)
+    if (files.length) {
+      await onDrop(files)
+      return true
+    }
+
+    const uri = e?.dataTransfer?.getData('text/uri-list')
+    if (!uri) return false
+
+    const resp = await fetch(uri)
+    const fileName = URL.parse(uri)?.searchParams?.get('filename')
+    if (!fileName) return false
+
+    const blob = await resp.blob()
+    const file = new File([blob], fileName, { type: blob.type })
+    const uriFiles = filterFiles([file])
+    if (!uriFiles.length) return false
+
+    await onDrop([file])
     return true
   }
 }
