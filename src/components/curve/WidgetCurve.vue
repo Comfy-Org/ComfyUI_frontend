2169e66ac289d0c2d220850071a57a4592025931
bymyself <cbyrne@comfy.org>
2026-03-17 09:21:18 +0000

    feat: add linear interpolation type to CURVE widget (#10118)
    Cherry-pick of 46d8567f10c6ae68b710f823a41c37c22cb78052 from main.
    Backport of https://github.com/Comfy-Org/ComfyUI_frontend/pull/10118
    
    Adapted for cloud/1.41: omits disabled/upstream features from #9896
    that were not included in the cloud/1.41 backport of that PR.

--- src/components/curve/WidgetCurve.vue
+++ src/components/curve/WidgetCurve.vue
@@ -1,16 +1,60 @@
 <template>
-  <CurveEditor v-model="modelValue" />
+  <div class="flex flex-col gap-1">
+    <Select
+      :model-value="modelValue.interpolation"
+      @update:model-value="onInterpolationChange"
+    >
+      <SelectTrigger size="md">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem
+          v-for="interp in CURVE_INTERPOLATIONS"
+          :key="interp"
+          :value="interp"
+        >
+          {{ $t(`curveWidget.${interp}`) }}
+        </SelectItem>
+      </SelectContent>
+    </Select>
+    <CurveEditor
+      :model-value="modelValue.points"
+      :interpolation="modelValue.interpolation"
+      @update:model-value="onPointsChange"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-import type { CurvePoint } from './types'
+import Select from '@/components/ui/select/Select.vue'
+import SelectContent from '@/components/ui/select/SelectContent.vue'
+import SelectItem from '@/components/ui/select/SelectItem.vue'
+import SelectTrigger from '@/components/ui/select/SelectTrigger.vue'
+import SelectValue from '@/components/ui/select/SelectValue.vue'
 
 import CurveEditor from './CurveEditor.vue'
+import { CURVE_INTERPOLATIONS } from './types'
+import type { CurveData, CurveInterpolation, CurvePoint } from './types'
 
-const modelValue = defineModel<CurvePoint[]>({
-  default: () => [
-    [0, 0],
-    [1, 1]
-  ]
+const modelValue = defineModel<CurveData>({
+  default: () => ({
+    points: [
+      [0, 0],
+      [1, 1]
+    ],
+    interpolation: 'monotone_cubic'
+  })
 })
+
+function onPointsChange(points: CurvePoint[]) {
+  modelValue.value = { ...modelValue.value, points }
+}
+
+function onInterpolationChange(value: unknown) {
+  if (typeof value !== 'string') return
+  modelValue.value = {
+    ...modelValue.value,
+    interpolation: value as CurveInterpolation
+  }
+}
 </script>
