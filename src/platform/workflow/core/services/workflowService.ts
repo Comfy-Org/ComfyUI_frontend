d8856f3703e5486979d11303168ff44bbba475ba
jaeone94 <89377375+jaeone94@users.noreply.github.com>
2026-03-31 05:03:29 +0900

    [backport cloud/1.42] fix: prevent saving active workflow content to inactive tab on close (#10756)
    Backport of #10745 to cloud/1.42.
    
    ┆Issue is synchronized with this [Notion
    page](https://www.notion.so/PR-10756-backport-cloud-1-42-fix-prevent-saving-active-workflow-content-to-inactive-tab-on-clo-3336d73d365081b386c2c3f9f52920c8)
    by [Unito](https://www.unito.io)

--- src/platform/workflow/core/services/workflowService.ts
+++ src/platform/workflow/core/services/workflowService.ts
@@ -136,7 +136,7 @@ export const useWorkflowService = () => {
       }
     }
 
-    workflow.changeTracker?.checkState()
+    if (workflowStore.isActive(workflow)) workflow.changeTracker?.checkState()
 
     if (isSelfOverwrite) {
       await saveWorkflow(workflow)
@@ -161,7 +161,7 @@ export const useWorkflowService = () => {
     if (workflow.isTemporary) {
       await saveWorkflowAs(workflow)
     } else {
-      workflow.changeTracker?.checkState()
+      if (workflowStore.isActive(workflow)) workflow.changeTracker?.checkState()
 
       const isApp = workflow.initialMode === 'app'
       const expectedPath =
