bb7a8245dc5c030294c8f46b0d688cf41a07c0ba
GitHub Action <action@github.com>
2026-04-22 14:47:50 +0000

    [automated] Apply ESLint and Oxfmt fixes

--- src/components/boundingbox/WidgetBoundingBox.test.ts
+++ src/components/boundingbox/WidgetBoundingBox.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable vue/one-component-per-file */
 import { render, screen } from '@testing-library/vue'
 import userEvent from '@testing-library/user-event'
 import { describe, expect, it } from 'vitest'
