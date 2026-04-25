4b579aab096e6ff2392d7fcb802c32fb49a47cb4
Benjamin Lu <benjaminlu1107@gmail.com>
2026-04-25 01:47:13 -0700

    test: rename asset scenario mocks

--- browser_tests/fixtures/assetScenarioFixture.ts
+++ browser_tests/fixtures/assetScenarioFixture.ts
@@ -1,11 +1,11 @@
-import { jobsBackendFixture } from '@e2e/fixtures/jobsBackendFixture'
+import { jobsApiMockFixture } from '@e2e/fixtures/jobsApiMockFixture'
 import { AssetScenarioHelper } from '@e2e/fixtures/helpers/AssetScenarioHelper'
 
-export const assetScenarioFixture = jobsBackendFixture.extend<{
+export const assetScenarioFixture = jobsApiMockFixture.extend<{
   assetScenario: AssetScenarioHelper
 }>({
-  assetScenario: async ({ page, jobsBackend }, use) => {
-    const assetScenario = new AssetScenarioHelper(page, jobsBackend)
+  assetScenario: async ({ page, jobsApi }, use) => {
+    const assetScenario = new AssetScenarioHelper(page, jobsApi)
 
     await use(assetScenario)
 
