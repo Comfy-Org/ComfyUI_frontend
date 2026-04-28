e994c2f630aaf7876c3a37cfa463a08937b3e7c1
dante01yoon <bunggl@naver.com>
2026-04-28 16:23:08 +0900

    Revert "fix: update knip package entrypoints"
    This reverts commit 711fa84e3cc96ab93b1c6fd2cfeaf9733aa88aca.

--- knip.config.ts
+++ knip.config.ts
@@ -24,16 +24,12 @@ const config: KnipConfig = {
       project: ['src/**/*.{js,ts}']
     },
     'packages/shared-frontend-utils': {
-      entry: ['src/formatUtil.ts', 'src/networkUtil.ts'],
       project: ['src/**/*.{js,ts}']
     },
     'packages/registry-types': {
-      entry: ['src/comfyRegistryTypes.ts'],
       project: ['src/**/*.{js,ts}']
     },
     'packages/ingest-types': {
-      entry: ['src/index.ts'],
-      ignoreDependencies: ['zod'],
       project: ['src/**/*.{js,ts}']
     },
     'apps/website': {
