e9466fd19aafb5366e715a367485f3bbe264d9c2
Austin <austin@comfy.org>
2026-04-30 09:54:34 -0700

    Remove tests for cleaner backportRemove tests for cleaner backport

--- tools/devtools/nodes/inputs.py
+++ tools/devtools/nodes/inputs.py
@@ -302,21 +302,6 @@ class NodeWithV2ComboInput:
     def node_with_v2_combo_input(self, combo_input: str):
         return (combo_input,)
 
-class NodeWithLegacyWidget:
-    @classmethod
-    def INPUT_TYPES(cls):
-        return {
-            "required": { "legacy_widget": ("INT", { "widgetType":  "DEVTOOLSLEGACYWIDGET" }) }
-        }
-
-    RETURN_TYPES = ()
-    FUNCTION = "node_with_legacy_widget"
-    CATEGORY = "DevTools"
-    DESCRIPTION = ("A node with a legacy widget")
-
-    def node_with_legacy_widget(self):
-        return ()
-
 
 NODE_CLASS_MAPPINGS = {
     "DevToolsLongComboDropdown": LongComboDropdown,
@@ -333,7 +318,6 @@ NODE_CLASS_MAPPINGS = {
     "DevToolsNodeWithSeedInput": NodeWithSeedInput,
     "DevToolsNodeWithValidation": NodeWithValidation,
     "DevToolsNodeWithV2ComboInput": NodeWithV2ComboInput,
-    "DevToolsNodeWithLegacyWidget": NodeWithLegacyWidget,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -351,7 +335,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "DevToolsNodeWithSeedInput": "Node With Seed Input",
     "DevToolsNodeWithValidation": "Node With Validation",
     "DevToolsNodeWithV2ComboInput": "Node With V2 Combo Input",
-    "DevToolsNodeWithLegacyWidget": "Node With Legacy Widget",
 }
 
 __all__ = [
