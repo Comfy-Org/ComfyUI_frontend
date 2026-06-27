8172788e2720bd6ce59a2126986fcb2f8a8faed0
Terry Jia <terryjia88@gmail.com>
2026-06-27 04:31:39 -0400

    [backport cloud/1.45] FE-1150 feat(rightSidePanel): hide 3D viewport widgets from panel (#13206) (#13208)
    Backport of https://github.com/Comfy-Org/ComfyUI_frontend/pull/13206 to
    cloud/1.45

--- src/extensions/core/load3d.ts
+++ src/extensions/core/load3d.ts
@@ -339,7 +339,7 @@ useExtensionService().registerExtension({
           name: inputName,
           component: Load3D,
           inputSpec: { ...inputSpecLoad3D, name: inputName },
-          options: {}
+          options: { hideInPanel: true }
         })
 
         widget.type = 'load3D'
@@ -573,7 +573,7 @@ useExtensionService().registerExtension({
           name: inputSpecPreview3D.name,
           component: Load3D,
           inputSpec: inputSpecPreview3D,
-          options: {}
+          options: { hideInPanel: true }
         })
 
         widget.type = 'load3D'
