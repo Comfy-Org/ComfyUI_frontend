ba6f22ac109af0366106a8069d734601433f5e8b
Terry Jia <terryjia88@gmail.com>
2026-07-02 23:55:55 -0400

    feat(assets): support text file outputs in media assets

--- src/extensions/core/saveText.ts
+++ src/extensions/core/saveText.ts
@@ -1,6 +1,7 @@
-import { t } from '@/i18n'
-import { app } from '@/scripts/app'
-import { ComfyWidgets } from '@/scripts/widgets'
+import {
+  addTextPreviewWidgets,
+  updateTextPreviewWidgets
+} from '@/extensions/core/textPreviewWidgets'
 import { useExtensionService } from '@/services/extensionService'
 
 useExtensionService().registerExtension({
@@ -11,31 +12,13 @@ useExtensionService().registerExtension({
     const onNodeCreated = nodeType.prototype.onNodeCreated
     nodeType.prototype.onNodeCreated = function () {
       onNodeCreated?.apply(this, [])
-
-      const previewWidget = ComfyWidgets['STRING'](
-        this,
-        'preview_text',
-        ['STRING', { multiline: true }],
-        app
-      ).widget
-
-      previewWidget.label = t('saveText.preview')
-      previewWidget.hidden = false
-      previewWidget.options.hidden = false
-      previewWidget.options.read_only = true
-      previewWidget.options.serialize = false
-      previewWidget.serialize = false
+      addTextPreviewWidgets(this)
     }
 
     const onExecuted = nodeType.prototype.onExecuted
     nodeType.prototype.onExecuted = function (message) {
       onExecuted?.apply(this, [message])
-
-      const text = message?.text ?? ''
-      const preview = this.widgets?.find((w) => w.name === 'preview_text')
-      if (preview) {
-        preview.value = Array.isArray(text) ? text.join('\n\n') : text
-      }
+      updateTextPreviewWidgets(this, message)
     }
   }
 })
