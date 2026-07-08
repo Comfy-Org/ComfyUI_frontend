04d11e8c6cd8ec41b08841ddcebdd7e385b666fe
huang47 <157390+huang47@users.noreply.github.com>
2026-07-08 14:05:37 -0700

    fix: update desktop ambient typings

--- apps/desktop-ui/.storybook/preview.ts
+++ apps/desktop-ui/.storybook/preview.ts
@@ -77,7 +77,6 @@ const preview: Preview = {
           { value: 'light', icon: 'sun', title: 'Light' },
           { value: 'dark', icon: 'moon', title: 'Dark' }
         ],
-        showName: true,
         dynamicTitle: true
       }
     }
