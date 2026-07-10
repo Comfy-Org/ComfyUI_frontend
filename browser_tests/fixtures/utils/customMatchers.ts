f35ea2c14ee7fef3a4ecdc571718ba3abec78640
Austin Mroz <austin@comfy.org>
2026-07-10 15:39:24 -0700

    Test cleanup
    I wanted to add a test for resize undo. How hard can it be?

--- browser_tests/fixtures/utils/customMatchers.ts
+++ browser_tests/fixtures/utils/customMatchers.ts
@@ -2,6 +2,9 @@ import type { ExpectMatcherState, Locator } from '@playwright/test'
 import { expect } from '@playwright/test'
 
 import type { NodeReference } from '@e2e/fixtures/utils/litegraphUtils'
+import type { Position, Size } from '@e2e/fixtures/types'
+
+type Bounds = Position & Size
 
 function makeMatcher<T>(
   getValue: (node: NodeReference) => Promise<T> | T,
@@ -46,5 +49,37 @@ export const comfyExpect = expect.extend({
       pass: isFocused,
       message: () => `Expected element to ${isFocused ? 'not ' : ''}be focused.`
     }
+  },
+  toBeCloseToArray(
+    target: number[],
+    expected: number[],
+    options?: { numDigits?: number }
+  ) {
+    expect(target).toHaveLength(expected.length)
+    const closeTo: (a: number, b: number) => void = this.isNot
+      ? (a, b) => expect(a).not.toBeCloseTo(b, options?.numDigits)
+      : (a, b) => expect(a).toBeCloseTo(b, options?.numDigits)
+    target.forEach((a, i) => closeTo(a, expected[i]))
+    const message = () =>
+      `${JSON.stringify(target)} is ${this.isNot ? 'not ' : ''}close to ${JSON.stringify(expected)}`
+    return { pass: !this.isNot, message }
+  },
+  async toHaveBounds(
+    locator: Locator,
+    expected: Bounds,
+    { numDigits }: { numDigits?: number } = {}
+  ) {
+    await expect(async () => {
+      const box = (await locator.boundingBox())!
+      const closeTo: (k: keyof Bounds) => void = this.isNot
+        ? (k) => expect(box[k], k).not.toBeCloseTo(expected[k], numDigits)
+        : (k) => expect(box[k], k).toBeCloseTo(expected[k], numDigits)
+      for (const key of ['x', 'y', 'width', 'height'] as const) closeTo(key)
+    }).toPass({ timeout: 5000 })
+
+    const box = (await locator.boundingBox())!
+    const message = () =>
+      `Bounds ${box} ${this.isNot ? 'does not match' : 'matches'} ${JSON.stringify(expected)}`
+    return { pass: !this.isNot, message }
   }
 })
