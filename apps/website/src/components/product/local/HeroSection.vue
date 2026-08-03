03e36106b355a2100e171b356a5c4b0bd409a4e1
Michael B <michael@imick.io>
2026-08-03 17:00:08 -0400

    fix(website): center hero text block on mid-width viewports

--- apps/website/src/components/product/local/HeroSection.vue
+++ apps/website/src/components/product/local/HeroSection.vue
@@ -282,7 +282,7 @@ onUnmounted(() => {
 
     <!-- Text -->
     <div
-      class="relative z-10 mt-17 w-full px-4 pb-16 lg:mt-0 lg:min-w-160 lg:flex-1 lg:translate-x-[10%] lg:px-20 lg:py-14"
+      class="relative z-10 mx-auto mt-17 w-full max-w-xl px-4 pb-16 lg:mx-0 lg:mt-0 lg:max-w-none lg:min-w-160 lg:flex-1 lg:translate-x-[10%] lg:px-20 lg:py-14"
     >
       <ProductHeroBadge text="DESKTOP" />
 
