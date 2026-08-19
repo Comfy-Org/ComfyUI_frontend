d166154ed3dd10eb25efd7847e241e3bb7cfe49b
another-chi <another_chi@pm.me>
2026-08-19 19:46:10 +0200

    fix: keep app mode input panel visible on the right side

--- src/views/LinearView.vue
+++ src/views/LinearView.vue
@@ -123,7 +123,7 @@ function dragDrop(e: DragEvent) {
       />
       <Splitter
         :key="splitterKey"
-        class="h-full flex-1 border-none bg-secondary-background"
+        class="h-full min-w-0 flex-1 border-none bg-secondary-background"
         @resizestart="$event.originalEvent.preventDefault()"
         @resizeend="onResizeEnd"
       >
