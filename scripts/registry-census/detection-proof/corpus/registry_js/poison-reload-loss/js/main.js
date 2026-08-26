8447c9b729765b94c589ccaa52a8a93e21f9cad7
christian-byrne <cbyrne@comfy.org>
2026-08-26 15:55:30 -0700

    test: tie reload poison to restored store state
    Addresses review feedback:
    https://github.com/Comfy-Org/ComfyUI_frontend/pull/15760#discussion_r3867151962

--- scripts/registry-census/detection-proof/corpus/registry_js/poison-reload-loss/js/main.js
+++ scripts/registry-census/detection-proof/corpus/registry_js/poison-reload-loss/js/main.js
@@ -2,14 +2,25 @@ import { app } from '../../scripts/app.js'
 
 app.registerExtension({
   name: 'poison.reload.loss',
-  beforeRegisterNodeDef(nodeType) {
-    let sourceGraph
-    const orig = nodeType.prototype.onSerialize
+  beforeRegisterNodeDef(nodeType, nodeData) {
+    if (nodeData.name !== 'CLIPTextEncode') return
+
+    let sourceSerialized = false
+    const origConfigure = nodeType.prototype.onConfigure
+    const origSerialize = nodeType.prototype.onSerialize
+    nodeType.prototype.onConfigure = function (nodeData) {
+      origConfigure?.call(this, nodeData)
+      const widget = this.widgets?.find((widget) => widget.name === 'text')
+      if (sourceSerialized && widget && !widget._state.poisonReloadSource) {
+        widget.value = ''
+      }
+    }
     nodeType.prototype.onSerialize = function (nodeData) {
-      orig?.call(this, nodeData)
-      sourceGraph ??= this.graph
-      if (this.graph !== sourceGraph) {
-        nodeData.widgets_values_named = {}
+      origSerialize?.call(this, nodeData)
+      const widget = this.widgets?.find((widget) => widget.name === 'text')
+      if (widget) {
+        widget._state.poisonReloadSource = true
+        sourceSerialized = true
       }
     }
   }
