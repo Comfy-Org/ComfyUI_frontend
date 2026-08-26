f9f9eb2421c01f47278fee07c70debdb9792e56a
christian-byrne <cbyrne@comfy.org>
2026-08-25 19:26:48 -0700

    fix: share widget type accessors
    Addresses review feedback:
    https://github.com/Comfy-Org/ComfyUI_frontend/pull/15766#discussion_r3856770020
    https://github.com/Comfy-Org/ComfyUI_frontend/pull/15766#discussion_r3856770028

--- src/lib/litegraph/src/widgets/BaseWidget.ts
+++ src/lib/litegraph/src/widgets/BaseWidget.ts
@@ -52,6 +52,17 @@ export interface WidgetEventOptions {
   canvas: LGraphCanvas
 }
 
+const storeBackedType: PropertyDescriptor = {
+  configurable: true,
+  enumerable: true,
+  get(this: BaseWidget) {
+    return this['_state'].type
+  },
+  set(this: BaseWidget, value: string) {
+    this['_state'].type = value
+  }
+}
+
 export abstract class BaseWidget<TWidget extends IBaseWidget = IBaseWidget>
   implements IBaseWidget, NodeBindable
 {
@@ -213,14 +224,7 @@ export abstract class BaseWidget<TWidget extends IBaseWidget = IBaseWidget>
     )
     if (registered) {
       this._state = registered
-      Object.defineProperty(this, 'type', {
-        configurable: true,
-        enumerable: true,
-        get: () => this._state.type as TWidget['type'],
-        set: (value: TWidget['type']) => {
-          this._state.type = value as TWidgetType
-        }
-      })
+      Object.defineProperty(this, 'type', storeBackedType)
     }
   }
 
