a1fc3f2143cc57cc91956f89f0b01ebd1420ccc2
benjcooley <benjcooley@gmail.com>
2026-08-25 18:19:08 -0700

    test: expose undefined registry sentinel

--- src/services/comfyRegistryService.test.ts
+++ src/services/comfyRegistryService.test.ts
@@ -15,7 +15,7 @@ vi.mock('axios', () => ({
 import { useComfyRegistryService } from '@/services/comfyRegistryService'
 
 describe('useComfyRegistryService', () => {
-  it.for([undefined, ''])(
+  it.for([null, undefined, '', 'undefined'])(
     'does not query for node name %s',
     async (nodeName) => {
       const result = await Reflect.apply(
