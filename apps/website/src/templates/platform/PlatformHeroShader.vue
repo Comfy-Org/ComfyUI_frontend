7fa5725bdcefd3095461fe52914e86fb62e3fdd4
bertfy <bert@comfy.org>
2026-08-28 16:08:22 -0700

    feat: focus platform hero shader

--- apps/website/src/templates/platform/PlatformHeroShader.vue
+++ apps/website/src/templates/platform/PlatformHeroShader.vue
@@ -15,7 +15,7 @@ onMounted(() => {
 <template>
   <div
     aria-hidden="true"
-    class="pointer-events-none absolute inset-0 z-0 overflow-hidden mask-[radial-gradient(ellipse_92%_84%_at_72%_50%,black_0%,black_58%,transparent_100%)] opacity-35 filter-[grayscale(1)_contrast(1.15)]"
+    class="pointer-events-none absolute inset-0 z-0 overflow-hidden mask-[radial-gradient(ellipse_92%_78%_at_70%_52%,black_0%,black_46%,transparent_100%)] opacity-35 filter-[grayscale(1)_contrast(1.15)] md:mask-[radial-gradient(ellipse_40%_82%_at_78%_48%,black_0%,black_44%,rgba(0,0,0,0.7)_62%,transparent_82%)]"
   >
     <ShaderClient v-if="isMounted" class="size-full" />
   </div>
