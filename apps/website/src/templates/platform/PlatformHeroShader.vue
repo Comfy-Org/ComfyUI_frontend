0637b08664e78a1e939242a5ecb9ae306fc1100f
bertfy <bert@comfy.org>
2026-08-28 20:48:18 -0700

    feat: expand platform hero shader

--- apps/website/src/templates/platform/PlatformHeroShader.vue
+++ apps/website/src/templates/platform/PlatformHeroShader.vue
@@ -15,7 +15,7 @@ onMounted(() => {
 <template>
   <div
     aria-hidden="true"
-    class="pointer-events-none absolute inset-0 z-0 overflow-hidden mask-[radial-gradient(ellipse_92%_78%_at_70%_52%,black_0%,black_46%,transparent_100%)] opacity-40 filter-[grayscale(1)_contrast(1.15)] md:left-1/2 md:mask-[radial-gradient(ellipse_58%_82%_at_50%_48%,black_0%,black_48%,rgba(0,0,0,0.72)_66%,transparent_90%)]"
+    class="pointer-events-none absolute inset-0 z-0 overflow-hidden mask-[radial-gradient(ellipse_96%_84%_at_70%_52%,black_0%,black_54%,transparent_100%)] opacity-50 filter-[grayscale(1)_contrast(1.15)] md:left-[42%] md:mask-[radial-gradient(ellipse_74%_92%_at_50%_48%,black_0%,black_58%,rgba(0,0,0,0.82)_78%,transparent_100%)]"
   >
     <ShaderClient v-if="isMounted" class="size-full" />
   </div>
