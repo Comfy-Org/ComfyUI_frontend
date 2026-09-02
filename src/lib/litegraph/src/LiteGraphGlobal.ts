6b196f7d26590b87ee0e73e72490ca7f5a55a23f
bymyself <cbyrne@comfy.org>
2026-09-02 12:55:20 +0000

    refactor: use Reflect.get instead of double assertion for onPropertyChange check
    Addresses review feedback:
    https://github.com/Comfy-Org/ComfyUI_frontend/pull/12235#discussion_r3913650159

--- src/lib/litegraph/src/LiteGraphGlobal.ts
+++ src/lib/litegraph/src/LiteGraphGlobal.ts
@@ -437,10 +437,7 @@ export class LiteGraphGlobal {
     if (prev) this.onNodeTypeReplaced?.(type, base_class, prev)
 
     // warnings
-    if (
-      (base_class.prototype as unknown as Record<string, unknown>)
-        .onPropertyChange
-    )
+    if (Reflect.get(base_class.prototype, 'onPropertyChange'))
       console.warn(
         `LiteGraph node class ${type} has onPropertyChange method, it must be called onPropertyChanged with d at the end`
       )
