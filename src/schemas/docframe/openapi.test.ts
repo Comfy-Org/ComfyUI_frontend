80696063661a29aad2c89ce9306542c85fee881f
Christian Byrne <cbyrne@comfy.org>
2026-09-04 08:41:57 +0000

    fix(types): bound doc update operation ids

--- src/schemas/docframe/openapi.test.ts
+++ src/schemas/docframe/openapi.test.ts
@@ -26,20 +26,41 @@ const validDocUpdateFixtures = [
     seq: 2,
     update_b64: 'AQ==',
     op_ids: ['op-1', 'op-2']
+  },
+  {
+    v: 1,
+    workflow_id: 'wf-1',
+    seq: 3,
+    update_b64: 'AQ==',
+    op_ids: Array.from({ length: 256 }, () => 'x'.repeat(128))
   }
 ]
 
 const invalidDocUpdateFixtures = [
-  { workflow_id: 'wf-1', seq: 3, update_b64: 'AQ==' },
+  { workflow_id: 'wf-1', seq: 4, update_b64: 'AQ==' },
   {
     v: 1,
     workflow_id: 'wf-1',
-    seq: 4,
+    seq: 5,
     update_b64: 'AQ==',
     unexpected: true
   },
-  { v: 1, workflow_id: 'wf-1', seq: 5, update_b64: 'AQ==', op_ids: [''] },
-  { v: 1, workflow_id: 'wf-1', seq: 6, update_b64: 'AQ==', op_ids: [42] }
+  { v: 1, workflow_id: 'wf-1', seq: 6, update_b64: 'AQ==', op_ids: [''] },
+  { v: 1, workflow_id: 'wf-1', seq: 7, update_b64: 'AQ==', op_ids: [42] },
+  {
+    v: 1,
+    workflow_id: 'wf-1',
+    seq: 8,
+    update_b64: 'AQ==',
+    op_ids: Array.from({ length: 257 }, () => 'op')
+  },
+  {
+    v: 1,
+    workflow_id: 'wf-1',
+    seq: 9,
+    update_b64: 'AQ==',
+    op_ids: ['x'.repeat(129)]
+  }
 ]
 
 describe('docframe OpenAPI schema', () => {
