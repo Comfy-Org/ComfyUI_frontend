22c42d0f291f7c110cc13da6b75f5398380ac332
Simon Pinfold <synap5e@users.noreply.github.com>
2026-09-07 21:06:56 -0700

    chore(settings): remove Comfy.Assets.UseAssetAPI
    AustinMroz's review comment 3951993926 requested removing the setting entirely, superseding the deprecate-don't-delete convention for this case.

--- src/platform/settings/constants/coreSettings.ts
+++ src/platform/settings/constants/coreSettings.ts
@@ -1212,14 +1212,6 @@ export const CORE_SETTINGS: SettingParams[] = [
     defaultValue: false
   },
   {
-    id: 'Comfy.Assets.UseAssetAPI',
-    name: 'Use Asset API for model library (deprecated)',
-    type: 'hidden',
-    tooltip: 'Use new Asset API for model browsing',
-    defaultValue: isCloud ? true : false,
-    deprecated: true
-  },
-  {
     id: 'Comfy.ModelLibrary.UseAssetBrowser',
     name: 'Use the asset browser for the model library',
     type: 'hidden',
