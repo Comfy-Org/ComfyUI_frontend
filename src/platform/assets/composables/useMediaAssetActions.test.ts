46ec7f292ce58824db07cd0fc1e2741378d043ff
Christian Byrne <cbyrne@comfy.org>
2026-09-21 02:31:48 -0700

    test(assets): remove redundant loader mock reset
    Addresses review feedback:
    https://github.com/Comfy-Org/ComfyUI_frontend/pull/16252#discussion_r3938359489

--- src/platform/assets/composables/useMediaAssetActions.test.ts
+++ src/platform/assets/composables/useMediaAssetActions.test.ts
@@ -319,10 +319,6 @@ describe('useMediaAssetActions', () => {
     )
     litegraphServiceMock.addNodeOnGraph.mockImplementation(createLoadImageNode)
     litegraphServiceMock.getCanvasCenter.mockReturnValue([100, 100])
-    mockDetectNodeTypeFromFilename.mockReturnValue({
-      nodeType: 'LoadImage',
-      widgetName: 'image'
-    })
     mockGetOutputAssetMetadata.mockReturnValue(null)
     mockGetAssetType.mockReturnValue('input')
     mockResolveOutputAssetItems.mockResolvedValue([])
