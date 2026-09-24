05d7e94de9e9702af014baea9b98fc6b84b6b865
june-bump <june@comfy.org>
2026-09-24 14:34:42 -0700

    fix(website): play nav featured video once instead of adding a pause button

--- apps/website/src/components/common/HeaderMain/NavFeaturedCard.vue
+++ apps/website/src/components/common/HeaderMain/NavFeaturedCard.vue
@@ -1,26 +1,14 @@
 <script setup lang="ts">
-import { useMediaControls } from '@vueuse/core'
-import { useTemplateRef } from 'vue'
-
 import ButtonPill from '@/components/ui/button-pill/ButtonPill.vue'
 
 import { prefersReducedMotion } from '../../../composables/useReducedMotion'
 import type { NavFeatured } from '../../../data/mainNavigation'
-import type { Locale } from '../../../i18n/translations'
-import { t } from '../../../i18n/translations'
-import PlayPauseButton from '../PlayPauseButton.vue'
 
-const { featured, locale = 'en' } = defineProps<{
-  featured: NavFeatured
-  locale?: Locale
-}>()
-
-const videoEl = useTemplateRef<HTMLVideoElement>('videoEl')
-const { playing } = useMediaControls(videoEl)
+defineProps<{ featured: NavFeatured }>()
 </script>
 
 <template>
-  <li class="relative w-62 shrink-0">
+  <li class="shrink-0">
     <a
       :href="featured.cta.href"
       :aria-label="featured.cta.ariaLabel"
@@ -28,7 +16,6 @@ const { playing } = useMediaControls(videoEl)
     >
       <video
         v-if="featured.videoSrc"
-        ref="videoEl"
         class="aspect-4/3 w-62 max-w-none rounded-xl object-cover"
         :src="featured.videoSrc"
         :poster="featured.imageSrc"
@@ -36,7 +23,6 @@ const { playing } = useMediaControls(videoEl)
         width="744"
         height="558"
         :autoplay="!prefersReducedMotion()"
-        loop
         muted
         playsinline
       />
@@ -59,15 +45,5 @@ const { playing } = useMediaControls(videoEl)
         </ButtonPill>
       </div>
     </a>
-    <PlayPauseButton
-      v-if="featured.videoSrc"
-      :playing
-      size="sm"
-      class="absolute top-2 right-2"
-      :aria-label="
-        playing ? t('player.pause', locale) : t('player.play', locale)
-      "
-      @click="playing = !playing"
-    />
   </li>
 </template>
